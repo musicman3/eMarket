fa8475536798d548142143f692a3c8aa5a13ee03
musicman3 <micromail@mail.ru>
2018-03-10 22:19:36 +0200

    правки

--- controller/pages/new_project.php
+++ controller/pages/new_project.php
@@ -10,28 +10,6 @@
 	require_once($_SERVER['DOCUMENT_ROOT'].'/model/connect_page_start.php');
 	/****** Copyright © 2018 eMarket ******* *************/
 
-	$table = $PDO->getColRow("SELECT * FROM csd_license", array());
-
-	//SELECT HASH-METHOD
-	if (HASH_METHOD == 'gost'){
-		$hash = $lang['hash_gost'];
-	}
-	if (HASH_METHOD == 'sha256'){
-		$hash = $lang['hash_sha256'];
-	}
-
-	//SELECT CRYPT-METHOD
-	if (CRYPT_METHOD == 'gost'){
-		$crypt = $lang['crypt_gost'];
-	}
-	if (CRYPT_METHOD == 'blowfish'){
-		$crypt = $lang['crypt_blowfish'];
-	}
-	if (CRYPT_METHOD == 'rijndael-256'){
-		$crypt = $lang['crypt_rijndael-256'];
-	}
-
-
 	/************  CONNECT PAGE END  ************/
 	require_once($_SERVER['DOCUMENT_ROOT'].'/model/connect_page_end.php');
 	/****** Copyright © 2018 eMarket ******* *************/
