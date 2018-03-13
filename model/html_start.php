3ff0a606324dc5020e794fb6b63784a7e0919c5a
Prizraki <lw12@yandex.ru>
2018-03-13 17:10:04 +0300

    Изменены meta

--- model/html_start.php
+++ model/html_start.php
@@ -5,12 +5,12 @@
 ***************************************/
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!doctype html>
+<html dir="ltr" lang="<?php echo $lang['meta-language'] ?>">
 <head>
 <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-<meta http-equiv="content-language" content="<?php echo $lang['meta-language'] ?>" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="robots" content="noindex,nofollow" />
 <meta name="generator" content="HippoEDIT, Notepad++" />
 <meta name="classification" content="software" />
