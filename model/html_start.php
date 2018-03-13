4b486082c6011713f25b67afebdb643ada3586a3
musicman3 <micromail@mail.ru>
2018-03-13 03:41:12 +0200

    Создание хэдера и автогенерация префикса title

--- model/html_start.php
+++ model/html_start.php
@@ -6,19 +6,27 @@
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<meta http-equiv="content-language" content="<?php echo $lang['meta-language'] ?>" />
-		<meta name="robots" content="noindex,nofollow" />
-		<meta name="generator" content="HippoEDIT, Notepad++" />
-		<meta name="classification" content="software" />
-		<meta name="author" content="eMarket" />
-		<meta name="owner" content="eMarket" />
-		<meta name="copyright" content="Copyright�2011 by eMerket. All right reserved." />
-	 <link rel="stylesheet" type="text/css" href="/view/default/<?php echo $patch ?>/style.css" media="screen" />
-	 <script type="text/javascript" src="/ext/jquery/jquery.min.js"></script>
-	 <?php if ($patch == 'admin'){ ?> <script type="text/javascript" src="/view/default/admin/js/jscookmenu/JSCookMenu.js"></script>
-	 <script type="text/javascript" src="/view/default/admin/js/jscookmenu/ThemeOffice/theme.js"></script>
-	 <link rel="stylesheet" href="/view/default/admin/js/jscookmenu/ThemeOffice/theme.css" type="text/css"> <?php } ?>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<meta http-equiv="content-language" content="<?php echo $lang['meta-language'] ?>" />
+<meta name="robots" content="noindex,nofollow" />
+<meta name="generator" content="HippoEDIT, Notepad++" />
+<meta name="classification" content="software" />
+<meta name="author" content="eMarket" />
+<meta name="owner" content="eMarket" />
+<meta name="copyright" content="Copyright©2018 by eMarket. All right reserved." />
+<?php $title_prefix = basename(($_SERVER['REQUEST_URI']), '.php'); // автогенерация префикса title по названию файла. Пример: для index.php = index ?>
+<title><?php echo $lang['title_'.$title_prefix] ?></title>
+<link rel="stylesheet" type="text/css" href="/view/default/<?php echo $patch ?>/style.css" media="screen" />
+<script type="text/javascript" src="/ext/jquery/jquery.min.js"></script>
+<?php //вывод только в админке
+	if ($patch == 'admin'){ ?>
+		<script type="text/javascript" src="/view/default/admin/js/jscookmenu/JSCookMenu.js"></script>
+		<script type="text/javascript" src="/view/default/admin/js/jscookmenu/ThemeOffice/theme.js"></script>
+		<link rel="stylesheet" href="/view/default/admin/js/jscookmenu/ThemeOffice/theme.css" type="text/css">
+		<?php 
+		require_once($_SERVER['DOCUMENT_ROOT'].'/view/default/admin/header.php');
+	} // конец вывода только в админке
+	?>
---
+++ view/default/admin/header.php
@@ -0,0 +1,76 @@
+<?php
+/****** Copyright © 2018 eMarket ******* 
+*   GNU GENERAL PUBLIC LICENSE v.3.0   *    
+* https://github.com/musicman3/eMarket *
+***************************************/
+?>
+
+</head>
+<body>
+<table border="0" width="100%" cellspacing="0" cellpadding="0">
+	<tr>
+		<td style="padding: 0 5px 0 5px;"><img src="/view/default/admin/images/img.png"></td>
+		<td width="330" align="right" style="padding-right: 5px;" >
+			<span style="color: #000000;"><b>SSL</b></span>
+		</td>
+	</tr>
+</table>
+
+<div id="administrationMenu" class="ThemeOfficeMainItem">
+	<ul style="visibility: hidden">
+		<li>
+			<span></span><span>Раздел</span>
+			<ul>
+				<li>
+					<span><img src="/view/default/admin/images/icons/" /></span><span>Меню 1</span>
+					<ul>
+						<li>
+							<span><img src="/view/default/admin/images/icons/" /></span><a href="http://#">Подменю меню 1</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<span><img src="/view/default/admin/images/icons/" /></span><span>Меню 2</span>
+					<ul>
+						<li>
+							<span><img src="/view/default/admin/images/icons/" /></span><a href="http://#">Подменю меню 2</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<span><img src="/view/default/admin/images/icons/"/></span><a href="#">Меню 3</a>
+				</li>
+			</ul>
+		</li>
+
+		<li>
+			<span></span><span>Раздел</span>
+			<ul>
+				<li>
+					<span><img src="/view/default/admin/images/icons/" /></span><span>Меню 1</span>
+					<ul>
+						<li>
+							<span><img src="/view/default/admin/images/icons/" /></span><a href="http://#">Подменю меню 1</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<span><img src="/view/default/admin/images/icons/" /></span><span>Меню 2</span>
+					<ul>
+						<li>
+							<span><img src="/view/default/admin/images/icons/" /></span><a href="http://#">Подменю меню 2</a>
+						</li>
+					</ul>
+				</li>
+				<li>
+					<span><img src="/view/default/admin/images/icons/"/></span><a href="#">Меню 3</a>
+				</li>
+			</ul>
+		</li>
+	
+	</ul>
+</div>
+
+<script type="text/javascript"><!--
+	cmDrawFromText('administrationMenu', 'hbr', cmThemeOffice, 'ThemeOffice');
+//--></script>
