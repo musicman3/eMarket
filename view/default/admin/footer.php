419bfa71934cbff20271e1291612527362b07942
Prizraki <lw12@yandex.ru>
2018-03-14 23:23:53 +0300

    Footer для админки

---
+++ view/default/admin/footer.php
@@ -0,0 +1,10 @@
+<?php
+/****** Copyright © 2018 eMarket ******* 
+*   GNU GENERAL PUBLIC LICENSE v.3.0   *    
+* https://github.com/musicman3/eMarket *
+***************************************/
+?>
+
+<div class="footer">
+Пишем сюда одну строку
+</div>
