8c1907b2576cf9a8528a009449979c4ba9cbccaa
musicman3 <micromail@mail.ru>
2018-03-16 00:44:34 +0200

    правка

--- view/default/admin/pages/error_log/index.php
+++ view/default/admin/pages/error_log/index.php
@@ -11,7 +11,7 @@
 <table  align="center" width="85%">
 	<tr align="center">
 		<td align="center">
-			<textarea readonly style="width:100%; height:400px;"><?php foreach ($lines as $line) { echo $line; } ?></textarea>
+			<textarea readonly style="width:100%; height:400px;"><?php foreach ($lines as $error_line) { echo $error_line; } ?></textarea>
 		</td>
 	</tr>
 </table>
