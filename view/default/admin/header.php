b7e9939fbf0eda4ab44937dc05f125a398f07475
Prizraki <lw12@yandex.ru>
2018-03-16 16:17:35 +0300

    Добавлен правый бар, вдруг пригодится

--- view/default/admin/header.php
+++ view/default/admin/header.php
@@ -24,6 +24,7 @@
 
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav navbar-right"><li> </li></ul>
       <ul class="nav navbar-nav">
 			<?php	for ($i = 0; $i < count($level); $i++) { ?>
 			<li>
