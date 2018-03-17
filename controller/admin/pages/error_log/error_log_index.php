646a39b7c55b3d719aed60b7d037c5e88ba5bec3
musicman3 <micromail@mail.ru>
2018-03-17 02:41:52 +0200

    Правка ошибки при навигации в логе ошибок + языковая переменная

--- controller/admin/pages/error_log/error_log_index.php
+++ controller/admin/pages/error_log/error_log_index.php
@@ -12,9 +12,13 @@
 	/**************************************/
 	
 	$lines = array();
-	// если начальная страница
+	// задаем количество строк на странице вывода
 	$lines_p = 20;
+	$lines_page = 20;
+	// устанавливаем страницу в ноль при заходе
 	$i = 0;
+	
+	// Если файл открыт, то
 	if (file_exists($_SERVER['DOCUMENT_ROOT'].'/model/work/errors.log')) {
 		// Получаем содержимое файла в виде массива
 		$lines = file($_SERVER['DOCUMENT_ROOT'].'/model/work/errors.log');
@@ -22,36 +26,35 @@
 		$lines = array_reverse($lines);
 		// считаем количество строк
 		$counter = count($lines);
-		$counter_max = $counter - 21;
-
+		
+		// Если нажали на кнопку вперед
 		if(isset($_POST['lines_p']) && isset($_POST['i'])){
 			// назначаем количество строк на странице
-			$lines_p = $_POST['lines_p'] + 20;
+			$lines_p = $_POST['lines_p'] + $lines_page;
 			//задаем значение счетчика
-			$i = $_POST['i'] + 20;
-
+			$i = $_POST['i'] + $lines_page;
+			if ($i > $counter-$lines_page) {
+				$i = $counter-$lines_page;
+			}
+			if ($lines_p > $counter) {
+				$lines_p = $counter;
+			}
 		}
+		
+		// Если нажали на кнопку назад
 		if(isset($_POST['lines_p2']) && isset($_POST['i2'])){
 			// назначаем количество строк на странице
-			$lines_p = $_POST['lines_p2'] - 20;
+			$lines_p = $_POST['lines_p2'] - $lines_page;
 			//задаем значение счетчика
-			$i = $_POST['i2'] - 20;
+			$i = $_POST['i2'] - $lines_page;
 			if ($i <0) {
 				$i = 0;
 			}
-			if ($lines_p <20) {
-				$lines_p = 20;
+			if ($lines_p <$lines_page) {
+				$lines_p = $lines_page;
 			}
-			if ($i >= $counter_max) {
-				$i = $counter_max-20;
-			}
-			if ($lines_p >= $counter) {
-				$lines_p = $counter-20;
-			}
-
 		}
-		
-
+				
 	}
 
 	/*********  CONNECT PAGE END  *********/
