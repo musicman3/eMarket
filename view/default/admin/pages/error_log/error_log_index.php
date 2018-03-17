a8d40e95a0bf10b237d2e225c7cc971300736c11
Prizraki <lw12@yandex.ru>
2018-03-17 10:11:40 +0300

    Оформление логов

--- view/default/admin/pages/error_log/error_log_index.php
+++ view/default/admin/pages/error_log/error_log_index.php
@@ -5,34 +5,76 @@
 ***************************************/
 ?>
 
-</head>
-<body>
-<br><br><br><br><br>
+<div id="log" class="container">
+	<div class="row">
+		<div class="panel panel-default">
+		
+			<div class="panel-heading">
+				<h3 class="panel-title">
+				      <div class="pull-left"><?php echo $lang['menu_error_log'] ?></div>
+				      <div class="clearfix"></div>
+				</h3>
+			</div>
+
+		<div class="panel-body">
+
+<table class="table">
+	<thead>
+		<tr>
+			<th>
+			<?php if ($counter > 0) { ?><b>c <?php echo $lines_p-19 ?> по <?php echo $lines_p ?> ( из <?php echo $counter; ?> )</b><?php } ?>
+			</th>
+			<th>
+<?php if ($counter > 0) { ?>	
   <form>
    <input hidden name="i" value="<?php echo $i ?>">
    <input hidden name="lines_p" value="<?php echo $lines_p ?>">
-   <p><input type="submit" value=">" action="/controller/admin/pages/error_log/error_log_index.php" formmethod="post"></p>
+   <div style="display: inline-block;float: right; padding-left:5px;"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/error_log/error_log_index.php" formmethod="post"><span class="glyphicon glyphicon-chevron-right"></span></button></div>
   </form>
+		
+			
   <form>
    <input hidden name="i2" value="<?php echo $i ?>">
    <input hidden name="lines_p2" value="<?php echo $lines_p ?>">
-   <p><input type="submit" value="<" action="/controller/admin/pages/error_log/error_log_index.php" formmethod="post"></p>
+   <div style="display: inline-block;float: right; padding-right:5px;"><button type="submit" class="btn btn-primary btn-xs"  action="/controller/admin/pages/error_log/error_log_index.php" formmethod="post"><span class="glyphicon glyphicon-chevron-left"></span></button></div>
   </form>
-<table  align="center" width="85%">
-
+<?php } ?>
+			</th>
+		</tr>
+	</thead>
+<tfoot>
+    <tr>
+      <th colspan="2">
+<?php if ($counter > 0) { ?>
+      <div style="display: inline-block;float: right;"><button type="submit" class="btn btn-primary btn-sm" action="#" formmethod="post">Удалить</button></div>
+<?php } ?>
+      </th>
+    </tr>
+</tfoot>
+	<tbody>
+			
 <?php	for ($i; $i < $lines_p; $i++) { 
 
-		if (strrpos ($lines[$i], 'PHP Notice:') == true ){ ?><tr><font color="green""><?php echo $lines[$i].'<br>'; ?></font><?php }elseif
+		if (strrpos ($lines[$i], 'PHP Notice:') == true ){ ?><tr class="success"><td colspan="2"><?php echo $lines[$i].'<br>'; ?><?php }elseif
 
-		(strrpos ($lines[$i], 'PHP Warning:') == true ){ ?><tr><font color="red""><?php echo $lines[$i].'<br>'; ?></font><?php }elseif
+		(strrpos ($lines[$i], 'PHP Warning:') == true ){ ?><tr class="danger"><td colspan="2"><?php echo $lines[$i].'<br>'; ?><?php }elseif
 
-		(strrpos ($lines[$i], 'PHP Warning2:') == true ){ ?><tr><font color="yellow""><?php echo $lines[$i].'<br>'; ?></font><?php }elseif
+		(strrpos ($lines[$i], 'PHP Warning2:') == true ){ ?><tr class="warning"><td colspan="2"><?php echo $lines[$i].'<br>'; ?><?php }elseif
 			
-			(strrpos ($lines[$i], 'PHP Parse error:') == true ){ ?><tr><font color="blue""><?php echo $lines[$i].'<br>'; ?></font><?php }else
+			(strrpos ($lines[$i], 'PHP Parse error:') == true ){ ?><tr class="info"><td colspan="2"><?php echo $lines[$i].'<br>'; ?><?php }elseif
+			
+			($counter > 0){
 
 				echo $lines[$i].'<br>';
 			?>
 
-		</tr><?php } ?>
+		</td></tr><?php }} ?>
+
+	</tbody>
 	
 </table>
+
+			</div>
+		</div>
+	</div>
+</div>
