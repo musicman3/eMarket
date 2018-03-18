81a42654c232914f5b93471526a91352abedee8a
Prizraki <lw12@yandex.ru>
2018-03-18 11:47:00 +0300

    Горизонтальная прокрутка таблиц на мобильных устройствах

--- view/default/admin/pages/error_log/error_log_index.php
+++ view/default/admin/pages/error_log/error_log_index.php
@@ -17,7 +17,7 @@
 			</div>
 <?php if (file_exists($_SERVER['DOCUMENT_ROOT'].'/model/work/errors.log') == true) { ?>
 		<div class="panel-body">
-
+<div class="table-responsive">
 <table class="table">
 	<thead>
 		<tr>
@@ -73,7 +73,7 @@
 	</tbody>
 	
 </table>
-
+</div>
 			</div>
 <?php } else { ?>
 		<div class="panel-body"><?php echo $lang['no_log'] ?></div>
