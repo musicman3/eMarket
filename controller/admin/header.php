7b8f20f8be8ccd9d39689d5f930339ae72cd57aa
musicman3 <micromail@mail.ru>
2018-03-19 02:47:44 +0200

    Набросок структуры меню

--- controller/admin/header.php
+++ controller/admin/header.php
@@ -10,41 +10,59 @@
 	$level = array();
 	$menu = array();
 	$submenu = array();
-	
-	//LEVEL 0
-	$level[0] = '<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span></span> Раздел 0 <b class="caret"></b></a>';
-	
-	$menu[0][0] = '<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span><img src="/view/default/admin/images/icons/" /></span> Меню 0 <b class="caret"></b></a>';
-	$submenu[0][0][0] = '<a href="http://www.mail.ru"><span><img src="/view/default/admin/images/icons/" /></span> Подменю 0</a>';
-	$submenu[0][0][1] = '<a href="http://www.mail.ru"><span><img src="/view/default/admin/images/icons/" /></span> Подменю 1</a>';
-	
-	$menu[0][1] = '<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span><img src="/view/default/admin/images/icons/" /></span> Меню 1 <b class="caret"></b></a>';
-	$submenu[0][1][0] = '<a href="http://www.mail.ru"><span><img src="/view/default/admin/images/icons/" /></span> Подменю 0</a>';
-	$submenu[0][1][1] = '<a href="http://www.mail.ru"><span><img src="/view/default/admin/images/icons/" /></span> Подменю 1</a>';
-	$submenu[0][1][2] = '<a href="http://www.mail.ru"><span><img src="/view/default/admin/images/icons/" /></span> Подменю 2</a>';
-	$submenu[0][1][3] = '<a href="http://www.mail.ru"><span><img src="/view/default/admin/images/icons/" /></span> Подменю 3</a>';
-	
-	$menu[0][2] = '<a href="http://www.mail.ru"><span><img src="/view/default/admin/images/icons/" /></span> Меню 2</a>';
-	
-	//LEVEL 1
-	$level[1] = '<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span></span> '.$lang['menu_tools'].' <b class="caret"></b></a>';
-	
-	$menu[1][0] = '<a href="/controller/admin/pages/error_log/error_log_index.php"><span><img src="/view/default/admin/images/icons/16x16/error.png" /></span> '.$lang['menu_error_log'].'</a>';
 
-	//LEVEL 2
-	$level[2] = '<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span></span> '.$lang['menu_help'].' <b class="caret"></b></a>';
+	//СОРТИРОВКА
+	$menu_market = '0'; //МАГАЗИН
+	$menu_sales = '1'; // ПРОДАЖИ
+	$menu_counterparty = '2'; //КОНТРАГЕНТЫ
+	$menu_tools = '3'; //ИНСТРУМЕНТЫ
+	$menu_help = '4'; //ПОМОЩЬ
+	$menu_exit = '5'; //ВЫХОД
 	
-	$menu[2][0] = '<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span><img src="/view/default/admin/images/icons/16x16/emarket.png" /></span> '.$lang['menu_extra'].' <b class="caret"></b></a>';
-	$submenu[2][0][0] = '<a href="http://#"><span><img src="/view/default/admin/images/icons/16x16/wrench_orange.png" /></span> '.$lang['menu_support'].'</a>';
 	
-	$menu[2][1] = '<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span><img src="/view/default/admin/images/icons/16x16/locale.png" /></span> '.$lang['menu_languages'].' <b class="caret"></b></a>';
-	//НУЖНО СДЕЛАТЬ ПАРСИНГ СПИСКА ЯЗЫКОВ И ВЫВОД В МЕНЮ
-	$submenu[2][1][0] = '<a href="http://#"><span><img src="/view/default/admin/images/worldflags/ru.png" /></span> '.$lang['menu_language'].'</a>';
-	$submenu[2][1][1] = '<a href="http://#"><span><img src="/view/default/admin/images/worldflags/gb.png" /></span> English</a>';
+	//МАГАЗИН
+	$level[$menu_market] = '<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span></span>Магазин<b class="caret"></b></a>';
 	
-	$menu[2][2] = '<a target="_blank" href="/controller/catalog/index.php"><span><img src="/view/default/admin/images/icons/16x16/home.png" /></span> '.$lang['menu_catalog'].'</a>';
+	$menu[$menu_market][0] = '<a href="#"><span><img src="/view/default/admin/images/icons/16x16/folder.png" /></span> Категории </a>';
+	$menu[$menu_market][1] = '<a href="#"><span><img src="/view/default/admin/images/icons/16x16/products.png" /></span> Товары </a>';
+	$menu[$menu_market][2] = '<a href="#"><span><img src="/view/default/admin/images/icons/16x16/manufacturers.png" /></span> Производители </a>';
+	$menu[$menu_market][3] = '<a href="#"><span><img src="/view/default/admin/images/icons/16x16/attach.png" /></span> Варианты </a>';
+	$menu[$menu_market][4] = '<a href="#"><span><img src="/view/default/admin/images/icons/16x16/specials.png" /></span> Распродажи </a>';
+	$menu[$menu_market][5] = '<a href="#"><span><img src="/view/default/admin/images/icons/16x16/feature.png" /></span> Рекомендуемые </a>';
+	$menu[$menu_market][6] = '<a href="#"><span><img src="/view/default/admin/images/icons/16x16/date.png" /></span> Ожидаемые </a>';
+	$menu[$menu_market][7] = '<a href="#"><span><img src="/view/default/admin/images/icons/16x16/reviews.png" /></span> Отзывы </a>';
+	$menu[$menu_market][8] = '<a href="#"><span><img src="/view/default/admin/images/icons/16x16/article.png" /></span> Статьи </a>';
 
-	//LEVEL 3
-	$level[3] = '<span></span><a href="/controller/admin/verify/logout.php">'.$lang['menu_exit'].'</a>';
+	//ПРОДАЖИ
+	$level[$menu_sales] = '<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span></span>Продажи<b class="caret"></b></a>';
+	
+	$menu[$menu_sales][0] = '<a href="#"><span><img src="/view/default/admin/images/icons/16x16/orders.png" /></span> Заказы </a>';
+
+	//КОНТРАГЕНТЫ
+	$level[$menu_counterparty] = '<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span></span>Контрагенты<b class="caret"></b></a>';
+	
+	$menu[$menu_counterparty][0] = '<a href="#"><span><img src="/view/default/admin/images/icons/16x16/building.png" /></span> Юридические лица </a>';
+	$menu[$menu_counterparty][1] = '<a href="#"><span><img src="/view/default/admin/images/icons/16x16/people.png" /></span> Физические лица </a>';
+	
+	//ИНСТРУМЕНТЫ
+	$level[$menu_tools] = '<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span></span> '.$lang['menu_tools'].' <b class="caret"></b></a>';
+	
+	$menu[$menu_tools][0] = '<a href="/controller/admin/pages/error_log/error_log_index.php"><span><img src="/view/default/admin/images/icons/16x16/error.png" /></span> '.$lang['menu_error_log'].'</a>';
+
+	//ПОМОЩЬ
+	$level[$menu_help] = '<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span></span> '.$lang['menu_help'].' <b class="caret"></b></a>';
+	
+	$menu[$menu_help][0] = '<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span><img src="/view/default/admin/images/icons/16x16/folder_wrench.png" /></span> '.$lang['menu_extra'].' <b class="caret"></b></a>';
+	$submenu[$menu_help][0][0] = '<a href="http://#"><span><img src="/view/default/admin/images/icons/16x16/wrench_orange.png" /></span> '.$lang['menu_support'].'</a>';
+	
+	$menu[$menu_help][1] = '<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span><img src="/view/default/admin/images/icons/16x16/locale.png" /></span> '.$lang['menu_languages'].' <b class="caret"></b></a>';
+
+	$submenu[$menu_help][1][0] = '<a href="http://#"><span><img src="/view/default/admin/images/worldflags/ru.png" /></span> '.$lang['menu_language'].'</a>';
+	$submenu[$menu_help][1][1] = '<a href="http://#"><span><img src="/view/default/admin/images/worldflags/gb.png" /></span> English</a>';
+	
+	$menu[$menu_help][2] = '<a target="_blank" href="/controller/catalog/index.php"><span><img src="/view/default/admin/images/icons/16x16/home.png" /></span> '.$lang['menu_catalog'].'</a>';
+
+	//ВЫХОД
+	$level[$menu_exit] = '<span></span><a href="/controller/admin/verify/logout.php">'.$lang['menu_exit'].'</a>';
 
 ?>
--- controller/admin/pages/error_log/error_log_index.php
+++ controller/admin/pages/error_log/error_log_index.php
@@ -12,25 +12,25 @@
 	
 	// Если нажали на кнопку Удалить
 	if(isset($_POST['log_delete']) == 'delete'){
-		// назначаем количество строк на странице
+		// удаляем лог
 		unlink($_SERVER['DOCUMENT_ROOT'].'/model/work/errors.log');
 	}
 
-	//КНОПКИ НАВИГАЦИИ ВПЕРЕД-НАЗАД
+	//КНОПКИ НАВИГАЦИИ НАЗАД-ВПЕРЕД И ПОСТРОЧНЫЙ ВЫВОД ТАБЛИЦЫ
 	$lines = array();
-	$lines_p = 20;// задаем количество строк на странице вывода
-	$lines_page = 20;// задаем количество строк на странице вывода
+	$lines_page = 20; // задаем количество строк на странице вывода
 	$i = 0;	// устанавливаем страницу в ноль при заходе
+	$lines_p = $lines_page;
 	// Если файл открыт, то
-	if (file_exists($_SERVER['DOCUMENT_ROOT'].'/model/work/errors.log')) {
-		$lines = file($_SERVER['DOCUMENT_ROOT'].'/model/work/errors.log'); // Получаем содержимое файла в виде массива
-		$lines = array_reverse($lines);// сортируем в обратном порядке
+	if (file_exists($_SERVER['DOCUMENT_ROOT'].'/model/work/errors.log')) { // Если файл существует, то
+		$lines = file($_SERVER['DOCUMENT_ROOT'].'/model/work/errors.log'); // получаем содержимое файла в виде массива
+		$lines = array_reverse($lines); // сортируем в обратном порядке
 		$counter = count($lines); // считаем количество строк
 		
 		// Если нажали на кнопку вперед
 		if(isset($_POST['lines_p']) && isset($_POST['i'])){
-			$lines_p = $_POST['lines_p'] + $lines_page; // назначаем количество строк на странице
-			$i = $_POST['i'] + $lines_page; //задаем значение счетчика
+			$lines_p = $_POST['lines_p'] + $lines_page; // пересчитываем количество строк на странице
+			$i = $_POST['i'] + $lines_page; // задаем значение счетчика
 			if ($i >= $counter) {
 				$i = $_POST['i'];
 			}
@@ -40,17 +40,17 @@
 		}
 		// Если нажали на кнопку назад
 		if(isset($_POST['lines_p2']) && isset($_POST['i2'])){
-			$lines_p = $_POST['i2']; // назначаем количество строк на странице
-			$i = $_POST['i2'] - $lines_page; //задаем значение счетчика
-			if ($i <0) {
+			$lines_p = $_POST['i2']; // пересчитываем количество строк на странице
+			$i = $_POST['i2'] - $lines_page; // задаем значение счетчика
+			if ($i < 0) {
 				$i = 0;
 			}
-			if ($lines_p <$lines_page) {
+			if ($lines_p < $lines_page) {
 				$lines_p = $lines_page;
 			}
 		}
 	}
-	//КОНЕЦ->КНОПКИ НАВИГАЦИИ ВПЕРЕД-НАЗАД 
+	//КНОПКИ НАВИГАЦИИ НАЗАД-ВПЕРЕД И ПОСТРОЧНЫЙ ВЫВОД ТАБЛИЦЫ
 
 	/*********  CONNECT PAGE END  *********/
 	require_once($_SERVER['DOCUMENT_ROOT'].'/model/connect_page_end.php');
