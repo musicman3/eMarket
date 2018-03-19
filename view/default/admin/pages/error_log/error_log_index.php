7fe609c65f0d9fd35edb21ddace380e4e7579bf5
Prizraki <lw12@yandex.ru>
2018-03-19 10:50:13 +0300

    Кнопка удалить и меню изменено

--- view/default/admin/pages/error_log/error_log_index.php
+++ view/default/admin/pages/error_log/error_log_index.php
@@ -22,40 +22,49 @@
 	<thead>
 		<tr>
 			<th>
-			<b><?php echo $lang['s'] ?> <?php echo $i+1 ?> <?php echo $lang['po'] ?> <?php echo $lines_p ?> ( <?php echo $lang['iz'] ?> <?php echo $counter; ?> )</b>
+			<div class="log-page"><?php echo $lang['s'] ?> <?php echo $i+1 ?> <?php echo $lang['po'] ?> <?php echo $lines_p ?> ( <?php echo $lang['iz'] ?> <?php echo $counter; ?> )<div>
 			</th>
+			
 			<th>
   <form>
    <input hidden name="i" value="<?php echo $i ?>">
    <input hidden name="lines_p" value="<?php echo $lines_p ?>">
    <div class="log-right"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/error_log/error_log_index.php" formmethod="post"><span class="glyphicon glyphicon-chevron-right"></span></button></div>
   </form>
+		
+			
   <form>
    <input hidden name="i2" value="<?php echo $i ?>">
    <input hidden name="lines_p2" value="<?php echo $lines_p ?>">
    <div class="log-left"><button type="submit" class="btn btn-primary btn-xs"  action="/controller/admin/pages/error_log/error_log_index.php" formmethod="post"><span class="glyphicon glyphicon-chevron-left"></span></button></div>
   </form>
+  
+  	<form>
+	  	<input hidden name="log_delete" value="delete">
+		<div class="log-del"><button type="submit" name="log_delete" class="btn btn-primary btn-xs" action="/controller/admin/pages/error_log/error_log_index.php" formmethod="post"><?php echo $lang['button_delete'] ?></button></div>
+	</form>
 			</th>
 		</tr>
 	</thead>
-<tfoot>
-    <tr>
-      <th colspan="2">
-	  <form>
-	  	<input hidden name="log_delete" value="delete">
-      <div class="log-del"><button type="submit" name="log_delete" class="btn btn-primary btn-xs" action="/controller/admin/pages/error_log/error_log_index.php" formmethod="post"><?php echo $lang['button_delete'] ?></button></div>
-	  </form>
-      </th>
-    </tr>
-</tfoot>
+
 	<tbody>
+			
 <?php	for ($i; $i < $lines_p; $i++) { 
+
 		if (strrpos ($lines[$i], 'PHP Notice:') == true ){ ?><tr class="success"><td colspan="2"><?php echo $lines[$i].'</td></tr>'; ?><?php }elseif
+
 		(strrpos ($lines[$i], 'PHP Warning:') == true ){ ?><tr class="danger"><td colspan="2"><?php echo $lines[$i].'</td></tr>'; ?><?php }elseif
+
 		(strrpos ($lines[$i], 'PHP Fatal error:') == true ){ ?><tr class="warning"><td colspan="2"><?php echo $lines[$i].'</td></tr>'; ?><?php }elseif
-		(strrpos ($lines[$i], 'PHP Parse error:') == true ){ ?><tr class="info"><td colspan="2"><?php echo $lines[$i].'</td></tr>'; ?><?php }else
-			{ ?><tr><td colspan="2"><?php echo $lines[$i] ?></td></tr><?php }} ?>
+			
+			(strrpos ($lines[$i], 'PHP Parse error:') == true ){ ?><tr class="info"><td colspan="2"><?php echo $lines[$i].'</td></tr>'; ?><?php }else
+			
+				{ ?><tr><td colspan="2"><?php echo $lines[$i].'</td></tr>'; }
+			}
+	?>
+
 	</tbody>
+	
 </table>
 </div>
 			</div>
