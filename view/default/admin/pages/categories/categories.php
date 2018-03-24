b26f7d319de1bda7a8df401225c1894b938daf02
musicman3 <micromail@mail.ru>
2018-03-24 23:39:01 +0200

    Вывел модальные окна в папку modal

--- view/default/admin/pages/categories/categories.php
+++ view/default/admin/pages/categories/categories.php
@@ -12,54 +12,11 @@
    <div class="panel-heading">
 	<h3 class="panel-title">
 	 <div class="pull-left"><?php echo $lang['menu_categories'] ?></div>
-
+	 
 	 <div class="add"><button type="submit" name="category_add" class="btn btn-success btn-xs" data-toggle="modal" data-target="#addCategory"><span class="glyphicon glyphicon-plus"></span> <?php echo $lang['button_add'] ?></button>
-	  <div id="addCategory" class="modal fade">
-	   <div class="modal-dialog">
-		<div class="modal-content">
-		 <div class="modal-header"><button class="close" type="button" data-dismiss="modal">×</button>
-		  <h4 class="modal-title"><?php echo $lang['menu_categories'] ?></h4>
-		 </div>
-		 <div class="panel-body">
-		  <form name="category_add" action="/controller/admin/pages/categories/categories.php" method="post" enctype="multipart/form-data">
-		   <fieldset>
-			<input type="hidden" name="parent_id" value="<?php echo $parent_id ?>" />
-			<div class="form-group">
-			 <label>Имя:</label></br>
-			 <img src="/view/default/admin/images/worldflags/ru.png" alt="Russian" title="Russian" width="16" height="10" />Russian</br>
-			 <?php // вывод из массива: name="categories_name[1]" id="categories_name[1] ?>
-			 <input class="input-sm form-control" type="text" name="name" id="name" />
-			</div>
-			<div class="form-group">
-			 <label for="image">Изображение:</label></br>
-			 <input type="file" name="image" id="image" /> Макс.: 100M
-			</div>
-			<div class="form-group">
-			 <label for="sort_category">Сортировать:</label></br>
-			 <input class="input-sm form-control" type="text" name="sort_category" id="sort_category" />
-			</div>
-			<div class="form-group">
-			 <label for="view_category">Отображать </label>
-			 <input class="check-box" type="checkbox" name="view_cat" checked>
-			</div>
-		   </fieldset>
-						  
-		   <p align="center">
-			<input type="hidden" name="subaction" value="confirm" /><button type="submit" class="btn btn-primary btn-xs" /><span class="glyphicon glyphicon-save"></span> <?php echo $lang['save'] ?></button>
-			<button class="btn btn-primary btn-xs" type="button" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> <?php echo $lang['cancel'] ?></button>
-		   </p>
-	  
-		  </form>
-	  
-		 </div>
-		 <div class="modal-footer">
-		 </div>
-		</div>
-	   </div>
-	  </div>
+	  <!-- Модальное окно "Добавить категорию" -->
+	  <?php require_once('modal/categories_add.php') ?>
 	 </div>
-
-
 
 	 <div class="clearfix"></div>
 	</h3>
---
+++ view/default/admin/pages/categories/modal/categories_add.php
@@ -0,0 +1,51 @@
+<?php
+/****** Copyright © 2018 eMarket ******* 
+*   GNU GENERAL PUBLIC LICENSE v.3.0   *    
+* https://github.com/musicman3/eMarket *
+***************************************/
+?>
+	 <!-- Модальное окно "Добавить категорию" -->
+	  <div id="addCategory" class="modal fade">
+	   <div class="modal-dialog">
+		<div class="modal-content">
+		 <div class="modal-header"><button class="close" type="button" data-dismiss="modal">×</button>
+		  <h4 class="modal-title"><?php echo $lang['menu_categories'] ?></h4>
+		 </div>
+		 <div class="panel-body">
+		  
+		  <form name="category_add" action="/controller/admin/pages/categories/categories.php" method="post" enctype="multipart/form-data">
+		   <fieldset>
+			<input type="hidden" name="parent_id" value="<?php echo $parent_id ?>" />
+			<div class="form-group">
+			 <label>Имя:</label></br>
+			 <img src="/view/default/admin/images/worldflags/ru.png" alt="Russian" title="Russian" width="16" height="10" />Russian</br>
+			 <?php // вывод из массива: name="categories_name[1]" id="categories_name[1] ?>
+			 <input class="input-sm form-control" type="text" name="name" id="name" />
+			</div>
+			<div class="form-group">
+			 <label for="image">Изображение:</label></br>
+			 <input type="file" name="image" id="image" /> Макс.: 100M
+			</div>
+			<div class="form-group">
+			 <label for="sort_category">Сортировать:</label></br>
+			 <input class="input-sm form-control" type="text" name="sort_category" id="sort_category" />
+			</div>
+			<div class="form-group">
+			 <label for="view_category">Отображать </label>
+			 <input class="check-box" type="checkbox" name="view_cat" checked>
+			</div>
+		   </fieldset>
+						  
+		   <p align="center">
+			<input type="hidden" name="subaction" value="confirm" /><button type="submit" class="btn btn-primary btn-xs" /><span class="glyphicon glyphicon-save"></span> <?php echo $lang['save'] ?></button>
+			<button class="btn btn-primary btn-xs" type="button" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> <?php echo $lang['cancel'] ?></button>
+		   </p>
+	  
+		  </form>
+		 </div>
+		 <div class="modal-footer">
+		 </div>
+		</div>
+	   </div>
+	  </div>
+	 <!-- КОНЕЦ Модальное окно "Добавить категорию" -->
