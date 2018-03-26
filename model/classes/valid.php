e76c65597ad048df5f1c4ca878164ea678779fd5
musicman3 <micromail@mail.ru>
2018-03-26 03:51:36 +0300

    Правки

--- model/classes/valid.php
+++ model/classes/valid.php
@@ -1,42 +1,41 @@
 <?php
+/****** Copyright © 2018 eMarket ******* 
+*   GNU GENERAL PUBLIC LICENSE v.3.0   *    
+* https://github.com/musicman3/eMarket *
+***************************************/
 
-/* * **** Copyright © 2018 eMarket ******* 
- *   GNU GENERAL PUBLIC LICENSE v.3.0   *    
- * https://github.com/musicman3/eMarket *
- * ************************************* */
+    namespace Model\Classes\Valid;
 
-namespace Model\Classes\Valid;
+    class ValidClass {
 
-class ValidClass {
-
-    // Валидация глобальных переменных $_POST[]
-    function inPOST($name) {
-        if (filter_input(INPUT_POST, $name) == TRUE) {
-            return (isset($_POST[$name])) ? $_POST[$name] : null;
+        // Валидация глобальных переменных $_POST[]
+        function inPOST($name) {
+            if (filter_input(INPUT_POST, $name) == TRUE) {
+                return (isset($_POST[$name])) ? $_POST[$name] : null;
+            }
         }
-    }
 
-    // Валидация глобальных переменных $_GET[]
-    function inGET($name) {
-        if (filter_input(INPUT_GET, $name) == TRUE) {
-            return (isset($_GET[$name])) ? $_GET[$name] : null;
+        // Валидация глобальных переменных $_GET[]
+        function inGET($name) {
+            if (filter_input(INPUT_GET, $name) == TRUE) {
+                return (isset($_GET[$name])) ? $_GET[$name] : null;
+            }
         }
-    }
 
-    // Валидация глобальных переменных $_SERVER[]
-    function inSERVER($name) {
-        if (filter_input(INPUT_SERVER, $name) == TRUE) {
-            return (isset($_SERVER[$name])) ? $_SERVER[$name] : null;
+        // Валидация глобальных переменных $_SERVER[]
+        function inSERVER($name) {
+            if (filter_input(INPUT_SERVER, $name) == TRUE) {
+                return (isset($_SERVER[$name])) ? $_SERVER[$name] : null;
+            }
         }
-    }
 
-    // Валидация глобальных переменных $_COOKIE[]
-    function inCOOKIE($name) {
-        if (filter_input(INPUT_COOKIE, $name) == TRUE) {
-            return (isset($_COOKIE[$name])) ? $_COOKIE[$name] : null;
+        // Валидация глобальных переменных $_COOKIE[]
+        function inCOOKIE($name) {
+            if (filter_input(INPUT_COOKIE, $name) == TRUE) {
+                return (isset($_COOKIE[$name])) ? $_COOKIE[$name] : null;
+            }
         }
-    }
 
-}
+    }
 
 ?>
--- model/router_class.php
+++ model/router_class.php
@@ -1,19 +1,18 @@
 <?php
+/****** Copyright © 2018 eMarket ******* 
+*   GNU GENERAL PUBLIC LICENSE v.3.0   *    
+* https://github.com/musicman3/eMarket *
+***************************************/
 
-/* * **** Copyright © 2018 eMarket ******* 
- *   GNU GENERAL PUBLIC LICENSE v.3.0   *    
- * https://github.com/musicman3/eMarket *
- * ************************************* */
+    //LOAD CLASS PDO
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/model/classes/pdo.php');
+    $PDO = new Model\Classes\Pdo\PdoClass;
 
-//LOAD CLASS PDO
-require_once($_SERVER['DOCUMENT_ROOT'] . '/model/classes/pdo.php');
-$PDO = new Model\Classes\Pdo\PdoClass;
+    //LOAD CLASS VALID
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/model/classes/valid.php');
+    $VALID = new Model\Classes\Valid\ValidClass;
 
-//LOAD CLASS VALID
-require_once($_SERVER['DOCUMENT_ROOT'] . '/model/classes/valid.php');
-$VALID = new Model\Classes\Valid\ValidClass;
-
-//LOAD CLASS VIEW
-require_once($_SERVER['DOCUMENT_ROOT'] . '/model/classes/view.php');
-$View = new Model\Classes\View\ViewClass;
+    //LOAD CLASS VIEW
+    require_once($_SERVER['DOCUMENT_ROOT'] . '/model/classes/view.php');
+    $View = new Model\Classes\View\ViewClass;
 ?>
