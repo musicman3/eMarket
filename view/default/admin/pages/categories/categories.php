ee3202f0fcedc8e9b5dbddf0d014ea27ff8b3e0b
Prizraki <lw12@yandex.ru>
2018-03-29 15:18:57 +0300

    Изменения кнопок

--- view/default/admin/pages/categories/categories.php
+++ view/default/admin/pages/categories/categories.php
@@ -24,11 +24,9 @@
                         <table class="table table-hover">
                             <thead>
                                 <tr>
-                                    <th colspan="2">
+                                    <th colspan="3">
                                         <div class="log-page"><?php echo $lang['s'] ?> <?php echo $i + 1 ?> <?php echo $lang['po'] ?> <?php echo $lines_p ?> ( <?php echo $lang['iz'] ?> <?php echo $counter; ?> )</div>
-                                    </th>
-                                    <th>
-                                        <form>
+					<form>
                                             <input hidden name="i" value="<?php echo $i ?>">
                                             <input hidden name="lines_p" value="<?php echo $lines_p ?>">
                                             <input hidden name="parent_id_temp" value="<?php echo $parent_id ?>">
@@ -50,45 +48,34 @@
                                     </th>
                                 </tr>
                             </thead>
-                            <tfoot>
-                                <tr>
-                                    <th colspan="2" align="left"><?php echo $lang['group_actions'] ?></th>
-                                    <th align="right">
-                                        <div class="log-right"><input class="select-all check-box" type="checkbox" value=""></div>
-                                        <form><input hidden name="log_delete" value="delete"><div class="log-left"><button type="submit" name="cat_delete_batch" class="btn btn-primary btn-xs" data-toggle="confirmation" data-btn-ok-label="<?php echo $lang['confirm-yes'] ?>" data-btn-cancel-label="<?php echo $lang['confirm-no'] ?>" title="<?php echo $lang['confirm-del'] ?>" action="" formmethod="post"><span class="glyphicon glyphicon-trash"> </span></button></div></form>
-                                        <div class="log-left"><button class="btn btn-primary btn-xs" title="<?php echo $lang['button_move'] ?>" action="" formmethod="post"><span class="glyphicon glyphicon-transfer"> </span></button></div>
-
-                                    </th>
-                                </tr>
-                            </tfoot>
 
                             <tbody id="sort-list">
 
                                 <?php $parent_up = $lines[0][4]; ?>
                                 <?php if ($parent_up > 0) { ?>
 
-                                    <tr class="sortno">
-                                        <td colspan="3" align="left"><form><div><button name="parent_up" value="<?php echo $parent_up ?>" class="btn btn-default btn-xs" title="" action="/controller/admin/pages/categories/categories.php" formmethod="post">....</button></div></form></td>
+                                    <tr  colspan="2" class="sortno">
+                                        <td  class="sortleft-m" align="left"><div></div></td>
+					<td colspan="3" align="left"><form><div><button name="parent_up" value="<?php echo $parent_up ?>" class="btn btn-default btn-xs" title="" action="/controller/admin/pages/categories/categories.php" formmethod="post">....</button></div></form></td>
                                     </tr>
 
                                 <?php } for ($i; $i < $lines_p; $i++) { ?>
 
                                     <tr class="sort-list" unitid="<?php echo $lines[$i][0] ?>">
                                         <?php if ($lines[$i][8] == 0) { ?>
-                                            <td class="sortleft" align="left"><form><div><button name="parent_down" value="<?php echo $lines[$i][0] ?>" class="btn btn-default btn-xs" title="<?php echo $lines[$i][1] ?>" action="/controller/admin/pages/categories/categories.php" formmethod="post"><span class="glyphicon glyphicon-folder-open"> </span></button></div></form></td>	  
+                                        <td  class="sortyes sortleft-m" align="left"><div><span class="glyphicon glyphicon-move"> </span></div></td>    
+					<td class="sortleft" align="left"><form><div><button name="parent_down" value="<?php echo $lines[$i][0] ?>" class="btn btn-default btn-xs" title="<?php echo $lines[$i][1] ?>" action="/controller/admin/pages/categories/categories.php" formmethod="post"><span class="glyphicon glyphicon-folder-open"> </span></button></div></form></td>
                                         <?php } else { ?>
-                                            <td class="sortleft" align="left"><form><div><button name="parent_down" value="<?php echo $lines[$i][0] ?>" class="btn btn-primary btn-xs" title="<?php echo $lines[$i][1] ?>" action="/controller/admin/pages/categories/categories.php" formmethod="post"><span class="glyphicon glyphicon-folder-open"> </span></button></div></form></td>
+                                        <td  class="sortyes sortleft-m" align="left"><div><span class="glyphicon glyphicon-move"> </span></div></td>    
+					<td class="sortleft" align="left"><form><div><button name="parent_down" value="<?php echo $lines[$i][0] ?>" class="btn btn-primary btn-xs" title="<?php echo $lines[$i][1] ?>" action="/controller/admin/pages/categories/categories.php" formmethod="post"><span class="glyphicon glyphicon-folder-open"> </span></button></div></form></td>
                                         <?php } ?>
-                                        <td class="sortyes" align="left">
+                                        <td align="left">
                                             <div class="context-one" id="<?php echo $lines[$i][0] ?>"><?php echo $lines[$i][1] ?>
                                                 <!-- Модальное окно "Редактировать категорию" -->
                                                 <?php require('modal/categories_edit.php') ?>
                                                 <!-- КОНЕЦ Модальное окно "Редактировать категорию" -->
                                             </div>
                                         </td>	  
-                                        <td class="sorthidden" align="right">
-                                            <div class="log-right"><input class="select-item check-box" type="checkbox" value=""></div>
-                                        </td>
                                     </tr>
 
                                 <?php } ?>
