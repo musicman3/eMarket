aca06e52871f1b0073f041898e94234fdae2ec0f
musicman3 <micromail@mail.ru>
2018-04-05 03:03:49 +0300

    изменения в БД

--- install/databases/myisam.sql
+++ install/databases/myisam.sql
@@ -5,26 +5,54 @@
 
 DROP TABLE IF EXISTS emkt_administrators;
 CREATE TABLE emkt_administrators (
-	id int NOT NULL auto_increment,
 	login varchar(128) binary NOT NULL,
 	password varchar(64) NOT NULL,
 	language varchar(64) NOT NULL,
 	permission varchar(20) NOT NULL,
 	note varchar(256),
 	status int DEFAULT '0' NOT NULL,
-PRIMARY KEY (id))
+PRIMARY KEY (login))
 ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS emkt_categories;
 CREATE TABLE emkt_categories (
 	id int NOT NULL auto_increment,
-	name varchar(255) NOT NULL,
-	image varchar(255),
+	name varchar(256) NOT NULL,
 	language varchar(64),
 	parent_id int DEFAULT '0' NOT NULL,
-	sort_category int DEFAULT '0' NOT NULL,
+        image varchar(256),
 	date_added datetime,
 	last_modified datetime,
+	sort_category int DEFAULT '0' NOT NULL,
 	status int,
+	PRIMARY KEY (id))
+ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS emkt_products;
+CREATE TABLE emkt_products (
+	id int NOT NULL,
+        name varchar(256) NOT NULL,
+        language varchar(64) NOT NULL,
+	parent_id int DEFAULT '0' NOT NULL,
+	images varchar(256),
+	date_added datetime,
+	last_modified datetime,
+        products_model int NOT NULL,
+        products_type varchar(256),
+        manufacturer int NOT NULL,
+        price decimal(12,2) NOT NULL,
+        quantity int NOT NULL,
+        weight decimal(5,2) NOT NULL,
+        weight_class int NOT NULL,
+        tax_class int NOT NULL,
+        ordered int NOT NULL default '0',
+        download_file varchar(256),
+        keyword varchar(256),
+        tags varchar(256),
+        url varchar(256),
+        viewed int default '0',
+        description text,
+	sort_products int DEFAULT '0' NOT NULL,
+        status int,
 	PRIMARY KEY (id))
 ENGINE=MyISAM DEFAULT CHARSET=utf8;
