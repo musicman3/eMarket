1b034c7d5ae4301052e90447bbc7da75a5c1bc53
musicman3 <micromail@mail.ru>
2018-04-06 14:55:20 +0300

    вынос js из футера

--- controller/admin/pages/categories/categories.php
+++ controller/admin/pages/categories/categories.php
@@ -1,4 +1,5 @@
 <?php
+
 // ****** Copyright © 2018 eMarket *****//
 //   GNU GENERAL PUBLIC LICENSE v.3.0   //
 // https://github.com/musicman3/eMarket //
@@ -248,5 +249,6 @@ if ($VALID->inPOST('token_ajax') == $TOKEN) {
 require_once($VALID->inSERVER('DOCUMENT_ROOT') . '/model/connect_page_end.php');
 require_once($VALID->inSERVER('DOCUMENT_ROOT') . '/model/html_end.php');
 // ************************************ //
-
+//подгрузка JS обработок
+require_once($_SERVER['DOCUMENT_ROOT'] . '/view/default/admin/pages/categories/js/js_categories.php');
 ?>
