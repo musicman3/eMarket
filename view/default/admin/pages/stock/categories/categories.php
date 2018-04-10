58b46552232323e4fcf3ff1abfa7f24e8fb0e758
Prizraki <lw12@yandex.ru>
2018-04-10 14:09:57 +0300

    Правка

--- view/default/admin/pages/stock/categories/categories.php
+++ view/default/admin/pages/stock/categories/categories.php
@@ -16,11 +16,11 @@
 
                 <div class="panel-heading">
                     <h3 class="panel-title">
-                        <div class="pull-left"><?php echo $lang['menu_categories'] ?></div>
+                        <div class="pull-left"><?php echo $lang['title_categories'] ?></div>
 
                         <!-- Количество строк на странице -->
                         <form action="/controller/admin/pages/stock/categories/categories.php" method="post" class="form-inline">
-                            <div class="add-xs">Строк на странице: <select name="select_row" class="input-xs form-control" onchange="this.form.submit()">
+                            <div class="add-xs"><?php echo $lang['rows_page'] ?>: <select name="select_row" class="input-xs form-control" onchange="this.form.submit()">
                                     <option>(<?php echo $lines_page ?>)</option>
                                     <option>20</option>
                                     <option>35</option>
