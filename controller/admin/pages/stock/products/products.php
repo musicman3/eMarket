08a0b0cd79c898bd56477f18041cec7a26ed2018
musicman3 <micromail@mail.ru>
2018-04-10 14:22:20 +0300

    Добавил некоторое наполнение

--- controller/admin/pages/stock/products/products.php
+++ controller/admin/pages/stock/products/products.php
@@ -11,6 +11,76 @@ error_reporting(-1);
 require_once($_SERVER['DOCUMENT_ROOT'] . '/model/connect_page_start.php');
 // ************************************** //
 
+// Устанавливаем родительскую категорию
+$parent_id = $VALID->inPOST('parent_id');
+if ($parent_id == FALSE) {
+    $parent_id = 0;
+}
+
+// КНОПКИ НАВИГАЦИИ НАЗАД-ВПЕРЕД И ПОСТРОЧНЫЙ ВЫВОД ТАБЛИЦЫ
+$lines = array();
+
+// задаем количество строк на странице вывода категорий
+if (isset($_SESSION['select_category']) == FALSE) {
+    $lines_page = 20;
+    $_SESSION['select_category'] = $lines_page;
+} elseif (isset($_SESSION['select_category']) == TRUE && $VALID->inPOST('select_row')) {
+    $_SESSION['select_category'] = $VALID->inPOST('select_row');
+    $lines_page = $_SESSION['select_category'];
+} else {
+    $lines_page = $_SESSION['select_category'];
+}
+
+$i = 0; // устанавливаем страницу в ноль при заходе
+$lines_p = $lines_page;
+
+// Если parrent_id является массивом, то
+if (is_array($parent_id) == TRUE) {
+    $parent_id = 0;
+}
+
+// Устанавливаем родительскую категорию при навигации назад-вперед
+if ($VALID->inPOST('parent_id_temp')) {
+    $parent_id = $VALID->inPOST('parent_id_temp');
+}
+// получаем отсортированное по sort_category содержимое в виде массива
+$lines = $PDO->getColRow("SELECT * FROM " . TABLE_CATEGORIES . " WHERE parent_id=? ORDER BY sort_category DESC", [$parent_id]);
+$lines = array_reverse($lines); // сортируем в обратном порядке
+$counter = count($lines);  //считаем количество строк
+
+if ($counter <= $lines_page) {
+    $lines_p = $counter;
+}
+// Если нажали на кнопку вперед
+if ($VALID->inPOST('lines_p')) {
+    $lines_p = $VALID->inPOST('lines_p') + $lines_page; // пересчитываем количество строк на странице
+    if ($VALID->inPOST('i') == FALSE) {
+        $vali = 0;
+    } else {
+        $vali = $VALID->inPOST('i');
+    }
+    $i = $vali + $lines_page; // задаем значение счетчика
+    if ($i >= $counter) {
+        $i = $vali;
+    }
+    if ($lines_p >= $counter) {
+        $lines_p = $counter;
+    }
+}
+// Если нажали на кнопку назад
+if ($counter >= $lines_page) {
+    if ($VALID->inPOST('lines_p2')) {
+        $lines_p = $VALID->inPOST('i2'); // пересчитываем количество строк на странице
+        $i = $VALID->inPOST('i2') - $lines_page; // задаем значение счетчика
+        if ($i < 0) {
+            $i = 0;
+        }
+        if ($lines_p < $lines_page) {
+            $lines_p = $lines_page;
+        }
+    }
+}
+// КОНЕЦ-> КНОПКИ НАВИГАЦИИ НАЗАД-ВПЕРЕД И ПОСТРОЧНЫЙ ВЫВОД ТАБЛИЦЫ
 
 
 // ********  CONNECT PAGE END  ******** //
