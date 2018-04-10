c48c89c4598460297ca92d224b3f589d1e43b4c6
musicman3 <micromail@mail.ru>
2018-04-10 22:29:10 +0300

    модалы

---
+++ view/default/admin/pages/stock/products/modal/products_add.php
@@ -0,0 +1,62 @@
+<?php
+// ****** Copyright © 2018 eMarket *****// 
+//   GNU GENERAL PUBLIC LICENSE v.3.0   //    
+// https://github.com/musicman3/eMarket //
+// *************************************//
+
+?>
+<!-- Модальное окно "Добавить категорию" -->
+<div id="addCategory" class="modal fade" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header"><button class="close" type="button" data-dismiss="modal">×</button>
+                <h4 class="modal-title"><?php echo $lang['menu_categories'] ?></h4>
+            </div>
+            <form id="form_post" name="form_post" action="javascript:void(null);" onsubmit="call()" method="post" enctype="multipart/form-data">
+                <div class="panel-body">
+                    <fieldset>
+                        <input type="hidden" name="parent_id" value="<?php echo $parent_id ?>" />
+                        <div class="form-group">
+                            <label><?php echo $lang['name'] ?>:</label><br>
+                            <img src="/view/default/admin/images/worldflags/ru.png" alt="Russian" title="Russian" width="16" height="10" />Russian<br>
+                            <input class="input-sm form-control" type="text" name="name" id="name" />
+                        </div>
+                        <div class="form-group">
+                            <label for="image"><?php echo $lang['images'] ?>:</label><br>
+                            <input type="file" name="image" id="image" /> <?php echo $lang['max'] ?>: <?php echo get_cfg_var('upload_max_filesize'); ?>
+                        </div>
+                        <div class="form-group">
+                            <label for="view_category"><?php echo $lang['display'] ?> </label>
+                            <input class="check-box" type="checkbox" name="view_cat" checked>
+                        </div>
+                    </fieldset>
+
+                </div>
+                <div class="modal-footer">
+                    <input type="hidden" name="subaction" value="confirm" />
+                    <button type="submit" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-save"></span> <?php echo $lang['save'] ?></button>
+                    <button class="btn btn-primary btn-xs" type="button" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> <?php echo $lang['cancel'] ?></button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript" language="javascript">
+    function call() {
+        var msg = $('#form_post').serialize();
+        $.ajax({
+            type: 'POST',
+            url: '/controller/admin/pages/stock/products/products.php',
+            data: msg,
+            success: function (data) {
+                $('#addCategory').remove();
+                $('.modal-backdrop').remove();
+                $('body').removeClass('modal-open');
+                $('body').css('padding-right', '');
+                $('#ajax').html(data);
+            }
+        });
+    }
+</script>
+<!-- КОНЕЦ Модальное окно "Добавить категорию" -->
---
+++ view/default/admin/pages/stock/products/modal/products_edit.php
@@ -0,0 +1,72 @@
+<?php
+// ****** Copyright © 2018 eMarket *****// 
+//   GNU GENERAL PUBLIC LICENSE v.3.0   //    
+// https://github.com/musicman3/eMarket //
+// *************************************//
+// 
+// собираем данные для отображения в Редактировании категорий
+$name_category_edit = $PDO->selectPrepare("SELECT name FROM " . TABLE_CATEGORIES . " WHERE id=?", array($lines[$i][0]));
+$status_category_edit = $PDO->selectPrepare("SELECT status FROM " . TABLE_CATEGORIES . " WHERE id=?", array($lines[$i][0]));
+if ($status_category_edit == 1) {
+    $status_category_edit = 'checked';
+} else {
+    $status_category_edit = '';
+}
+
+?>
+
+<!-- Модальное окно "Редактировать категорию" -->
+<div id="<?php echo 'addCategory' . $lines[$i][0] ?>" class="modal fade" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header"><button class="close" type="button" data-dismiss="modal">×</button>
+                <h4 class="modal-title"><?php echo $lang['menu_categories'] ?></h4>
+            </div>
+            <form id="form_post<?php echo $lines[$i][0] ?>" action="javascript:void(null);" onsubmit="call<?php echo $lines[$i][0] ?>()" method="post" enctype="multipart/form-data">
+                <div class="panel-body">
+                    <fieldset>
+                        <input type="hidden" name="parent_id" value="<?php echo $parent_id ?>" />
+                        <input type="hidden" name="cat_edit" value="<?php echo $lines[$i][0] ?>" />
+                        <div class="form-group">
+                            <label><?php echo $lang['name'] ?>:</label><br>
+                            <img src="/view/default/admin/images/worldflags/ru.png" alt="Russian" title="Russian" width="16" height="10" />Russian<br>
+                            <?php // вывод из массива: name="categories_name[1]" id="categories_name[1]   ?>
+                            <input class="input-sm form-control" type="text" name="name_edit" id="name" value="<?php echo $name_category_edit ?>" />
+                        </div>
+                        <div class="form-group">
+                            <label for="image"><?php echo $lang['images'] ?>:</label><br>
+                            <input type="file" name="image" id="image" /> <?php echo $lang['max'] ?>: <?php echo get_cfg_var('upload_max_filesize'); ?>
+                        </div>
+                        <div class="form-group">
+                            <label for="view_category"><?php echo $lang['display'] ?> </label>
+                            <input class="check-box" type="checkbox" name="view_cat" <?php echo $status_category_edit ?>>
+                        </div>
+                    </fieldset>
+                </div>
+                <div class="modal-footer">
+                    <input type="hidden" name="subaction" value="confirm" />
+                    <button type="submit" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-save"></span> <?php echo $lang['save'] ?></button>
+                    <button class="btn btn-primary btn-xs" type="button" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> <?php echo $lang['cancel'] ?></button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+<script type="text/javascript" language="javascript">
+    function call<?php echo $lines[$i][0] ?>() {
+        var msg = $('#form_post<?php echo $lines[$i][0] ?>').serialize();
+        $.ajax({
+            type: 'POST',
+            url: '/controller/admin/pages/stock/products/products.php',
+            data: msg,
+            success: function (data) {
+                $(".modal-backdrop").remove();
+                $('body').removeClass('modal-open');
+                $('body').css('padding-right', '');
+                $('#ajax').html(data);
+            }
+        });
+    }
+</script>
+<!-- КОНЕЦ Модальное окно "Редактировать категорию" -->
