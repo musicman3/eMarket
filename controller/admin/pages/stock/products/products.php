cedba4c3f71d4b7817c436cb79d215949031d9ea
musicman3 <micromail@mail.ru>
2018-04-10 12:17:45 +0300

    папка products

---
+++ controller/admin/pages/stock/products/products.php
@@ -0,0 +1,20 @@
+<?php
+
+// ****** Copyright © 2018 eMarket *****//
+//   GNU GENERAL PUBLIC LICENSE v.3.0   //
+// https://github.com/musicman3/eMarket //
+// *************************************//
+
+error_reporting(-1);
+
+// ********  CONNECT PAGE START  ******** //
+require_once($_SERVER['DOCUMENT_ROOT'] . '/model/connect_page_start.php');
+// ************************************** //
+
+
+
+// ********  CONNECT PAGE END  ******** //
+require_once($VALID->inSERVER('DOCUMENT_ROOT') . '/model/connect_page_end.php');
+require_once($VALID->inSERVER('DOCUMENT_ROOT') . '/model/html_end.php');
+// ************************************ //
+?>
