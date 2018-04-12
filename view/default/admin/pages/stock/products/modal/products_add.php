47d121a91eee1bbea6601645171b52ae6ca2d0b5
Prizraki <lw12@yandex.ru>
2018-04-12 14:42:39 +0300

    Изменение формы

--- view/default/admin/pages/stock/products/modal/products_add.php
+++ view/default/admin/pages/stock/products/modal/products_add.php
@@ -15,37 +15,71 @@
             <form id="form_post" name="form_post" action="javascript:void(null);" onsubmit="call()" method="post" enctype="multipart/form-data">
                 <div class="panel-body">
 		    <ul class="nav nav-tabs">
-			<li class="active"><a data-toggle="tab" href="#panel1">Панель 1</a></li>
-			<li><a data-toggle="tab" href="#panel2">Панель 2</a></li>
-			<li><a data-toggle="tab" href="#panel3">Панель 3</a></li>
+			<li class="active"><a data-toggle="tab" href="#panel1">Основное</a></li>
+			<li><a data-toggle="tab" href="#panel2">Данные</a></li>
+			<li><a data-toggle="tab" href="#panel3">Изображения</a></li>
 		    </ul>
 		    <div class="tab-content">
                         <input type="hidden" name="parent_id" value="<?php echo $parent_id ?>" />
-			<div id="panel1" class="form-group tab-pane fade in active">
+			<div id="panel1" class="tab-pane fade in active">
+			    <div class="form-group">
                             <label><?php echo $lang['name'] ?>:</label><br>
-                            <img src="/view/default/admin/images/worldflags/ru.png" alt="Russian" title="Russian" width="16" height="10" />Russian<br>
-                            <input class="input-sm form-control auto_sm" type="text" name="name" id="name" />
-                            <label><?php echo $lang['product_quantity'] ?>:</label><br>
-                            <input class="input-sm form-control auto_sm" type="text" name="quantity" id="quantity" />
-                            <label><?php echo $lang['product_unit'] ?>:</label><br>
-                            <select name="unit" id="unit" class="input-sm form-control auto_sm">
+                            <img src="/view/default/admin/images/worldflags/ru.png" alt="Russian" title="Russian" width="16" height="10" />Russian
+                            <input class="input-sm form-control" type="text" name="name" id="name" />
+			    </div>
+			    <div class="form-group">
+                            <label><?php echo $lang['product_description'] ?>:</label><br>
+                            <img src="/view/default/admin/images/worldflags/ru.png" alt="Russian" title="Russian" width="16" height="10" />Russian
+                            <textarea rows="3" class="input-sm form-control" name="description" id="description" /></textarea>
+			    </div>
+			    <div class="form-group">
+                            <label>Keyword:</label>
+                            <input class="input-sm form-control" type="text" name="keyword" id="keyword" />
+			    </div>
+			    <div class="form-group">
+                            <label>Tags:</label>
+                            <input class="input-sm form-control" type="text" name="tags" id="tags" />
+			    </div>
+                        </div>
+			<div id="panel2" class="tab-pane fade">
+			    <div class="form-group">
+			    <label for="view_product"><?php echo $lang['display'] ?> </label>
+                            <input class="check-box" type="checkbox" name="view_product" checked>
+			    </div>
+			    <div class="row">
+				<div class="col-left">
+			    <label><?php echo $lang['product_quantity'] ?>:</label>
+                            <input class="input-sm form-control" type="text" name="quantity" id="quantity" />
+				</div>
+				<div class="col-right">
+			    <label><?php echo $lang['product_price'] ?>:</label>
+                            <input class="input-sm form-control" type="text" name="price" id="price" />
+				</div>
+			    </div>
+			    <div class="row">
+				<div class="col-left">
+                            <label><?php echo $lang['product_unit'] ?>:</label>
+                            <select name="unit" id="unit"class="input-sm form-control">
                                     <option>шт.</option>
                                     <option>м.</option>
                                     <option>л.</option>
-                                </select>
-                            <label><?php echo $lang['product_price'] ?>:</label><br>
-                            <input class="input-sm form-control auto_sm" type="text" name="price" id="price" />
-                            <label><?php echo $lang['product_description'] ?>:</label><br>
-                            <img src="/view/default/admin/images/worldflags/ru.png" alt="Russian" title="Russian" width="16" height="10" />Russian<br>
-                            <textarea rows="3" class="input-sm form-control" name="description" id="description" /></textarea>
-                        </div>
-			<div id="panel2" class="form-group tab-pane fade">
-                            <label for="image"><?php echo $lang['images'] ?>:</label><br>
+                            </select>
+				</div>
+				<div class="col-right">
+                            <label>Производители:</label>
+                            <select name="manufacturer" id="manufacturer"class="input-sm form-control">
+                                    <option>Acer</option>
+                                    <option>Asus</option>
+                                    <option>Intel</option>
+                            </select>
+				</div>
+			    </div>
+			</div>
+			<div id="panel3" class="tab-pane fade">
+			    <div class="form-group">
+                            <label for="image"><?php echo $lang['images'] ?>:</label>
                             <input type="file" name="image" id="image" /> <?php echo $lang['max'] ?>: <?php echo get_cfg_var('upload_max_filesize'); ?>
-                        </div>
-			<div id="panel3" class="form-group tab-pane fade">
-                            <label for="view_product"><?php echo $lang['display'] ?> </label>
-                            <input class="check-box" type="checkbox" name="view_product" checked>
+			    </div>
                         </div>
 		    </div>
 
