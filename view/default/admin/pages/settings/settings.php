980023b3fac923a5b487dfc7a8e3893d19670bce
Prizraki <lw12@yandex.ru>
2018-04-25 14:53:25 +0300

    Таблица настроек

--- view/default/admin/pages/settings/settings.php
+++ view/default/admin/pages/settings/settings.php
@@ -5,3 +5,27 @@
 // *************************************//
 
 ?>
+
+    <div id="settings" class="container">
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        <div class="pull-left"><?php echo $lang['title_settings'] ?></div>
+                        <div class="clearfix"></div>
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    <table class="table">
+                        <tbody>
+                            <tr>
+                                <td>111</td>
+				<td>222</td>
+				<td>333</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
