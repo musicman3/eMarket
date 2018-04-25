c852e0fa478fbdd953222417d138a70c50286082
musicman3 <micromail@mail.ru>
2018-04-25 16:07:05 +0300

    еще

--- view/default/admin/pages/settings/settings.php
+++ view/default/admin/pages/settings/settings.php
@@ -19,41 +19,41 @@
                     <tbody>
                         <tr>
                             <td>
-                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-folder-open"> </span></a></div>
+                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-globe"> </span></a></div>
                                 <div class="settings-page">Страны</div>
                                 <div class="settings-right"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/stock/categories/categories.php" formmethod="post"><span class="glyphicon glyphicon-pencil"></span></button></div>
                             </td>
                             <td>
-                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-folder-open"> </span></a></div>
+                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-adjust"> </span></a></div>
                                 <div class="settings-page">Зоны</div>
                                 <div class="settings-right"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/stock/categories/categories.php" formmethod="post"><span class="glyphicon glyphicon-pencil"></span></button></div>
                             </td>
                             <td>
-                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-folder-open"> </span></a></div>
+                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-briefcase"> </span></a></div>
                                 <div class="settings-page">Налоги</div>
                                 <div class="settings-right"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/stock/categories/categories.php" formmethod="post"><span class="glyphicon glyphicon-pencil"></span></button></div>
                             </td>
                         </tr>
                         <tr>
                             <td>
-                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-folder-open"> </span></a></div>
+                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-road"> </span></a></div>
                                 <div class="settings-page">Единица размера</div>
                                 <div class="settings-right"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/stock/categories/categories.php" formmethod="post"><span class="glyphicon glyphicon-pencil"></span></button></div>
                             </td>
                             <td>
-                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-folder-open"> </span></a></div>
+                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-oil"> </span></a></div>
                                 <div class="settings-page">Единица веса</div>
                                 <div class="settings-right"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/stock/categories/categories.php" formmethod="post"><span class="glyphicon glyphicon-pencil"></span></button></div>
                             </td>
                             <td>
-                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-folder-open"> </span></a></div>
+                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-comment"> </span></a></div>
                                 <div class="settings-page">Языки</div>
                                 <div class="settings-right"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/stock/categories/categories.php" formmethod="post"><span class="glyphicon glyphicon-pencil"></span></button></div>
                             </td>
                         </tr>
                         <tr>
                             <td>
-                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-folder-open"> </span></a></div>
+                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-tag"> </span></a></div>
                                 <div class="settings-page">Идентификатор товара</div>
                                 <div class="settings-right"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/stock/categories/categories.php" formmethod="post"><span class="glyphicon glyphicon-pencil"></span></button></div>
                             </td>
@@ -63,8 +63,8 @@
                                 <div class="settings-right"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/stock/categories/categories.php" formmethod="post"><span class="glyphicon glyphicon-pencil"></span></button></div>
                             </td>
                             <td>
-                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-folder-open"> </span></a></div>
-                                <div class="settings-page">Другое</div>
+                                <div class="settings-left"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-barcode"> </span></a></div>
+                                <div class="settings-page">Штриховые коды</div>
                                 <div class="settings-right"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/stock/categories/categories.php" formmethod="post"><span class="glyphicon glyphicon-pencil"></span></button></div>
                             </td>
                         </tr>
