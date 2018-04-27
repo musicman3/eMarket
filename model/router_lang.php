2f555b0710266491f74868dbf67f9dd5e11637e2
musicman3 <micromail@mail.ru>
2018-04-28 00:30:46 +0300

    Откат

--- model/router_lang.php
+++ model/router_lang.php
@@ -47,22 +47,11 @@ for ($i = 0; $i < count($files_patch); $i++) {
 
 // Получаем список языков в массиве (для использования в мультиязычных функциях и т.п.)
 $lang_all = array(); // массив с языками
-$lang_all_x = array(); // массив с языками без языка по дефолту
 $lang_dir = scandir($VALID->inSERVER('DOCUMENT_ROOT') . '/language/');
 array_push($lang_all, ucfirst($lang_default)); // первым в массиве идет язык по умолчанию
 foreach ($lang_dir as $lang_name) {
-    if (!in_array($lang_name, array('.', '..', $lang_default))) {
+    if (!in_array($lang_name, array('.', '..', $lang_default))){
         array_push($lang_all, ucfirst($lang_name));
-        array_push($lang_all_x, $lang_name);
-    }
-}
-
-// Подгрузка главного языкового файла для всех языков админки, кроме языка по умолчанию.
-if (count($lang_all_x) > 0) {
-    $langx = array();
-    for ($lang_x = 0; $lang_x < count($lang_all_x); $lang_x++) {
-        $files_patch_x = $VALID->inSERVER('DOCUMENT_ROOT') . '/language/' . $lang_all_x[$lang_x] . '/admin/' . $lang_all_x[$lang_x] . '.lng';
-        $langx[$lang_x + 1] = parse_ini_file($files_patch_x); // Установка языкового массива
     }
 }
 
