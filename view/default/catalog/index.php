b8a245293ab14d4c80df2b21d31f1148b19b12df
Prizraki <lw12@yandex.ru>
2018-05-01 21:51:04 +0300

    Панель во фронтенд и немного стилизации

--- view/default/catalog/index.php
+++ view/default/catalog/index.php
@@ -10,26 +10,42 @@
 <body>
     <!-- Для смартфонов -->
 
-    <!--<nav class="navbar-inverse" role="navigation">
-    <div class="row"> </div>
-
-            <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    </button>
-            </div>
-
-<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-<a href="#">Корзина</a>
-    <div class="navbar-right col-md-3">
-          
+<nav class="navbar navbar-inverse" role="navigation">
+    <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">Bootstrap 3 Demo</a>
     </div>
-
-</div>
-    </nav>-->
+    
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	<ul class="nav navbar-nav">
+	    <li><a class="dropdown-toggle" data-toggle="dropdown" href="#">Hardware <b class="caret"></b></a>
+		<ul class="dropdown-menu">
+		    <li><a class="dropdown-toggle" data-toggle="dropdown" href="#">Graphics <b class="caret"></b></a>
+			<ul class="dropdown-menu link">
+			    <li><a href="#">Radeon R7</a></li>
+			</ul>
+		    </li>
+		    <li><a href="#">Printers</a></li>
+		    <li><a href="#">Monitors</a></li>
+		    <li><a href="#">Speakers</a></li>
+		</ul>
+	    </li>
+	    <li><a class="dropdown-toggle" data-toggle="dropdown" href="#">Software <b class="caret"></b></a>
+		<ul class="dropdown-menu">
+		    <li><a href="#">Simulation</a></li>
+		    <li><a href="#">Action</a></li>
+		    <li><a href="#">Strategy</a></li>
+		</ul>
+	    </li>
+	    <li><a href="#">Gadgets</a></li>
+	</ul>
+    </div>
+</nav>
 
     <!-- Конец для смартфонов -->
 
