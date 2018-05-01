ca39d23f5933a29dfb2563b2c06f8b5122a42699
musicman3 <micromail@mail.ru>
2018-05-01 16:50:51 +0300

    переделка

--- view/default/admin/pages/stock/categories/modal/categories_edit.php
+++ view/default/admin/pages/stock/categories/modal/categories_edit.php
@@ -4,23 +4,7 @@
 // https://github.com/musicman3/eMarket //
 // *************************************//
 // 
-// собираем данные для отображения в Редактировании категорий
-if (isset($lines[$i][0]) == TRUE) {
-    $name_category_edit = array();
-    if (count($lang_all) > 1) {
-        for ($xl = 0; $xl < count($lang_all); $xl++) {
-            array_push($name_category_edit, $PDO->selectPrepare("SELECT name FROM " . TABLE_CATEGORIES . " WHERE id=? and language=?", [$lines[$i][0], $lang_all[$xl]]));
-        }
-    }
-
-    $status_category_edit = $PDO->selectPrepare("SELECT status FROM " . TABLE_CATEGORIES . " WHERE id=?", [$lines[$i][0]]);
-
-    if ($status_category_edit == 1) {
-        $status_category_edit = 'checked';
-    } else {
-        $status_category_edit = '';
-    }
-}
+require($VALID->inSERVER('DOCUMENT_ROOT') . '/controller/admin/pages/stock/categories/modal/categories_edit.php');
 ?>
 
 <!-- Модальное окно "Редактировать категорию" -->
