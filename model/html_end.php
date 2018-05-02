4fd51e9afc9151e9669c8a6f0b3ba1e3cac4083f
Prizraki <lw12@yandex.ru>
2018-05-02 08:49:10 +0300

    Пропустил

--- model/html_end.php
+++ model/html_end.php
@@ -27,6 +27,7 @@ if ($patch == 'catalog') {
     ?>
     <script type="text/javascript" src="/ext/jquery/jquery.min.js"></script>
     <script type="text/javascript" src="/ext/bootstrap/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/ext/bootstrap/js/menu.js"></script>
     <?php
 } // конец вывода только в каталоге
 
