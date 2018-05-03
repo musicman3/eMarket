6199b5f63ca7b8ccedc3d6dd8f3ebbe1cd849086
musicman3 <micromail@mail.ru>
2018-05-03 22:52:23 +0300

    Основаная вкладка на родном языке

--- view/default/admin/pages/settings/modal/taxes_add.php
+++ view/default/admin/pages/settings/modal/taxes_add.php
@@ -17,7 +17,7 @@
 
                     <!-- Языковые панели -->
                     <ul class="nav nav-tabs">
-                        <li class="active"><a data-toggle="tab" href="#<?php echo $lang_all[0] ?>"><img src="/view/default/admin/images/langflags/<?php echo $lang_all[0] ?>.png" alt="<?php echo $lang_all[0] ?>" title="<?php echo $lang_all[0] ?>" width="16" height="10" /> <?php echo $lang_all[0] ?></a></li>
+                        <li class="active"><a data-toggle="tab" href="#<?php echo $lang_all[0] ?>"><img src="/view/default/admin/images/langflags/<?php echo $lang_all[0] ?>.png" alt="<?php echo $lang_all[0] ?>" title="<?php echo $lang_all[0] ?>" width="16" height="10" /> <?php echo $lang['menu_language'] ?></a></li>
 
                         <?php
                         if (count($lang_all) > 1) {
