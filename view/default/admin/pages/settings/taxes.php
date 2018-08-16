9d7935d8b1c1d64a526f332c320de5acbec666ac
Prizraki <lw12@yandex.ru>
2018-08-16 10:39:09 +0300

    Таблица налогов

--- view/default/admin/pages/settings/taxes.php
+++ view/default/admin/pages/settings/taxes.php
@@ -20,8 +20,51 @@
                     </h3>
                 </div>
                 <div class="panel-body">
-                    Добавить налог:
-                    <div class="settings-right"><a href="#taxes_add" class="btn btn-primary btn-xs" data-toggle="modal"><span class="glyphicon glyphicon-plus"></span></a></div>
+		    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th colspan="2">
+                                    <div class="log-page"><?php echo $lang['s'] ?> <?php echo $i + 1 ?> <?php echo $lang['po'] ?> <?php echo $lines_p ?> ( <?php echo $lang['iz'] ?> <?php echo $counter; ?> )</div>
+                                </th>
+
+                                <th>
+                                    <form>
+                                        <input hidden name="i" value="<?php echo $i ?>">
+                                        <input hidden name="lines_p" value="<?php echo $lines_p ?>">
+                                        <div class="log-right"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/error_log/error_log.php" formmethod="post"><span class="glyphicon glyphicon-chevron-right"></span></button></div>
+                                    </form>
+
+
+                                    <form>
+                                        <input hidden name="i2" value="<?php echo $i ?>">
+                                        <input hidden name="lines_p2" value="<?php echo $lines_p ?>">
+                                        <div class="log-left"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/error_log/error_log.php" formmethod="post"><span class="glyphicon glyphicon-chevron-left"></span></button></div>
+                                    </form>
+
+                                    <form>
+                                        <div class="log-left"><a href="#taxes_add" class="btn btn-primary btn-xs" data-toggle="modal"><span class="glyphicon glyphicon-plus"></span></a></div>
+                                    </form>
+                                </th>
+                            </tr>
+                            <tr class="border">
+                                <th class="al-text">Налог</th>
+                                <th class="al-text">Ставка</th>
+                                <th class="al-text-w">Редактировать</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>...</td>
+                                <td class="al-text">...</td>
+                                <td class="al-text-w">...</td>
+                            </tr>
+			    <tr>
+                                <td>...</td>
+                                <td class="al-text">...</td>
+                                <td class="al-text-w">...</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
