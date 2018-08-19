ec5547f6744ca1f3e5ba0bf5ff41f86438593ead
musicman3 <micromail@mail.ru>
2018-08-20 00:48:20 +0300

    Удаление налога

--- view/default/admin/pages/settings/taxes.php
+++ view/default/admin/pages/settings/taxes.php
@@ -24,7 +24,7 @@
                         <thead>
                             <tr>
                                 <th colspan="2">
-                                    <div class="log-page"><?php echo $lang['s'] ?> <?php echo $i+1 ?> <?php echo $lang['po'] ?> <?php echo $lines_p ?> ( <?php echo $lang['iz'] ?> <?php echo $counter ?> )</div>
+                                    <div class="log-page"><?php echo $lang['s'] ?> <?php echo $i + 1 ?> <?php echo $lang['po'] ?> <?php echo $lines_p ?> ( <?php echo $lang['iz'] ?> <?php echo $counter ?> )</div>
                                 </th>
 
                                 <th>
@@ -51,18 +51,25 @@
                                 </th>
                             </tr>
                             <tr class="border">
-                                <th class="al-text">Налог</th>
+                                <th>Налог</th>
                                 <th class="al-text">Ставка</th>
-                                <th class="al-text-w">Редактировать</th>
+                                <th class="al-text-w"></th>
                             </tr>
                         </thead>
                         <tbody>
                             <?php for ($i; $i < $lines_p; $i++) { ?>
-                            <tr>
-                                <td><?php echo $lines[$i][1] ?></td>
-                                <td class="al-text"><?php echo $lines[$i][2] ?></td>
-                                <td class="al-text-w">...</td>
-                            </tr>
+                                <tr>
+                                    <td><?php echo $lines[$i][1] ?></td>
+                                    <td class="al-text"><?php echo $lines[$i][2] ?></td>
+                                    <td class="al-text-w">
+                                        <form>
+                                            <input hidden name="tax_delete" value="<?php echo $lines[$i][0] ?>">
+                                            <div class="log-left">
+                                                <button type="submit" name="tax_delete_but" class="btn btn-primary btn-xs" data-toggle="confirmation" data-btn-ok-label="<?php echo $lang['confirm-yes'] ?>" data-btn-cancel-label="<?php echo $lang['confirm-no'] ?>" title="<?php echo $lang['confirm-del'] ?>" action="/controller/admin/pages/setting/taxes.php" formmethod="post"><span class="glyphicon glyphicon-trash"> </span></button>
+                                            </div>
+                                        </form>
+                                    </td>
+                                </tr>
                             <?php } ?>
                         </tbody>
                     </table>
