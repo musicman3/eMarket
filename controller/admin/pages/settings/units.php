3816450d42d46735092b1c87ab6876e52494b708
musicman3 <micromail@mail.ru>
2018-10-09 17:01:32 +0300

    фикс

--- controller/admin/pages/settings/units.php
+++ controller/admin/pages/settings/units.php
@@ -24,11 +24,11 @@ if ($VALID->inGET('unit'.$lang_all[0])) {
 }
 
 // Если нажали на кнопку Редактировать
-if ($VALID->inGET('unit_edit')) {
+if ($VALID->inGET('unit_edit'.$lang_all[0])) {
 
     for ($xl = 0; $xl < count($lang_all); $xl++) {
         // обновляем запись
-        $PDO->insertPrepare("UPDATE " . TABLE_UNITS . " SET name=?, rate=? WHERE id=? AND language=?", [$VALID->inGET('name_edit' . $lang_all[$xl]), $VALID->inGET('rate_edit'), $VALID->inGET('tax_edit'), $lang_all[$xl]]);
+        $PDO->insertPrepare("UPDATE " . TABLE_UNITS . " SET name=?, unit=? WHERE id=? AND language=?", [$VALID->inGET('name_edit' . $lang_all[$xl]), $VALID->inGET('unit_edit' . $lang_all[$xl]), $VALID->inGET('unit_edit'), $lang_all[$xl]]);
     }
 }
 
