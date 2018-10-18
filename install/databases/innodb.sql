e86ace38f14e9801aea6395fb2060f59a7d7bcb9
musicman3 <micromail@mail.ru>
2018-10-18 21:20:42 +0300

    фикс

--- install/databases/innodb.sql
+++ install/databases/innodb.sql
@@ -126,6 +126,6 @@ INSERT INTO emkt_regions (country_id, region_code, name, language) VALUES (1, 'A
 INSERT INTO emkt_regions (country_id, region_code, name, language) VALUES (1, 'AL', 'Республика Алтай', 'Russian');
 
 /* English */
-INSERT INTO emkt_countries VALUES (1, 'Российская Федерация', 'English', 'RU', 'RUS', '');
+INSERT INTO emkt_countries VALUES (1, 'Russian Federation', 'English', 'RU', 'RUS', '');
 INSERT INTO emkt_regions (country_id, region_code, name, language) VALUES (1, 'AD', 'Республика Адыгея', 'English');
 INSERT INTO emkt_regions (country_id, region_code, name, language) VALUES (1, 'AL', 'Республика Алтай', 'English');
