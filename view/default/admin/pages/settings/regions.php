ee78620e275aa514d236a6c6cda8bbb7f32fb82d
musicman3 <micromail@mail.ru>
2018-10-20 20:52:09 +0300

    вернул

--- view/default/admin/pages/settings/regions.php
+++ view/default/admin/pages/settings/regions.php
@@ -26,12 +26,6 @@
                 <div class="panel-heading">
                     <h3 class="panel-title">
                         <div class="pull-left"><?php echo $lang['title_regions'] ?></div>
-                                                                <form>
-                                            <div class="log-right">
-                                                тут кнопка и ее ссылка на предыдущую<br>
-<?php echo $_SERVER['HTTP_REFERER'] ?>
-                                            
-                                      
                         <div class="clearfix"></div>
                     </h3>
                 </div>
