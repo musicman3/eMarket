cf316e8a02b9e8f2bab33e87bdc1607d19cf2efd
Prizraki <lw12@yandex.ru>
2018-10-20 22:34:18 +0300

    Новая кнопка + класс для разворота

--- view/default/admin/pages/settings/regions.php
+++ view/default/admin/pages/settings/regions.php
@@ -25,7 +25,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h3 class="panel-title">
-                        <div class="pull-left"><a class="btn btn-primary btn-xs" href="<?php echo $_SESSION['country_page'] ?>"><span class="glyphicon glyphicon-arrow-left"></span> Страны</a> - <?php echo $lang['title_regions'] ?></div>
+                        <div class="pull-left"><a class="btn btn-primary btn-xs" href="<?php echo $_SESSION['country_page'] ?>"><span class="back glyphicon glyphicon-share-alt"></span> Страны</a> - <?php echo $lang['title_regions'] ?></div>
                         <div class="clearfix"></div>
                     </h3>
                 </div>
