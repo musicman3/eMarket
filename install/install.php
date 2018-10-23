6954ad30e804619359cb98f72f9a34b13b1ea899
musicman3 <micromail@mail.ru>
2018-10-23 14:21:03 +0300

    набросок на зоны

--- install/install.php
+++ install/install.php
@@ -63,6 +63,8 @@
         $tab_regions = $dbpref . 'regions';
         $tab_taxes = $dbpref . 'taxes';
         $tab_units = $dbpref . 'units';
+        $tab_zones = $dbpref . 'zones';
+        $tab_value_zones = $dbpref . 'value_zones';
         $tab_vendor_codes = $dbpref . 'vendor_codes';
         $hashmet = $_POST['hash_method'];
         $crypt = $_POST['crypt_method'];
@@ -89,6 +91,8 @@
                 '  define(\'TABLE_REGIONS\', \'' . $tab_regions . '\');' . "\n" .
                 '  define(\'TABLE_TAXES\', \'' . $tab_taxes . '\');' . "\n" .
                 '  define(\'TABLE_UNITS\', \'' . $tab_units . '\');' . "\n" .
+                '  define(\'TABLE_ZONES\', \'' . $tab_zones . '\');' . "\n" .
+                '  define(\'TABLE_VALUE_ZONES\', \'' . $tab_value_zones . '\');' . "\n" .
                 '  define(\'TABLE_VENDOR_CODES\', \'' . $tab_vendor_codes . '\');' . "\n" .
                 '?>';
 
---
+++ view/default/admin/pages/settings/zones.php
@@ -0,0 +1,113 @@
+<?php
+// ****** Copyright © 2018 eMarket *****// 
+//   GNU GENERAL PUBLIC LICENSE v.3.0   //    
+// https://github.com/musicman3/eMarket //
+// *************************************//
+
+?>
+<!-- Модальное окно "Добавить" -->
+<?php require_once('modal/zones_add.php') ?>
+<!-- КОНЕЦ Модальное окно "Добавить" -->
+
+<!-- Дублируем модальные окна Редактирования -->
+<?php $k = $i; // дублируем переменную   ?>
+
+<?php for ($k; $k < $lines_p; $k++) { // запускаем цикл формирования модальных окон  ?>
+
+    <!-- Вставляем модальное окно "Редактировать" -->
+    <?php require($VALID->inSERVER('DOCUMENT_ROOT') . '/view/default/admin/pages/settings/modal/zones_edit.php') ?>
+
+<?php } ?>
+
+<div id="ajax">
+    <div id="settings" class="container">
+        <div class="row">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        <div class="pull-left"><?php echo $lang['title_zones'] ?></div>
+                        <div class="clearfix"></div>
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th colspan="5">
+                                    <?php if ($lines == TRUE) { ?>
+                                        <div class="log-page"><?php echo $lang['s'] ?> <?php echo $i + 1 ?> <?php echo $lang['po'] ?> <?php echo $lines_p ?> ( <?php echo $lang['iz'] ?> <?php echo $counter ?> )</div>
+                                        <?php
+                                    } else {
+
+                                        ?>
+                                        <div class="log-page"><?php echo $lang['no_zones'] ?></div>
+                                    <?php } ?>
+                                </th>
+
+                                <th>
+                                    <form>
+                                        <?php if ($counter > $lines_page) { ?>
+                                            <input hidden name="i" value="<?php echo $i ?>">
+                                            <input hidden name="lines_p" value="<?php echo $lines_p ?>">
+                                        <?php } ?>
+                                        <div class="log-right"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/settings/zones.php" formmethod="get"><span class="glyphicon glyphicon-chevron-right"></span></button></div>
+                                    </form>
+
+
+                                    <form>
+                                        <?php if ($counter > $lines_page) { ?>
+                                            <input hidden name="i2" value="<?php echo $i ?>">
+                                            <input hidden name="lines_p2" value="<?php echo $lines_p ?>">
+                                        <?php } ?>
+                                        <div class="log-left"><button type="submit" class="btn btn-primary btn-xs" action="/controller/admin/pages/settings/zones.php" formmethod="get"><span class="glyphicon glyphicon-chevron-left"></span></button></div>
+                                    </form>
+
+                                    <div class="log-left">
+                                        <a href="#zones_add" class="btn btn-primary btn-xs" data-toggle="modal"><span class="glyphicon glyphicon-plus"></span></a>
+                                    </div>
+                                </th>
+                            </tr>
+			    <?php if ($lines == TRUE) { ?>
+                            <tr class="border">
+                                <th></th>
+                                <th><?php echo $lang['zone'] ?></th>
+                                <th class="al-text"><?php echo $lang['alpha_2'] ?></th>
+                                <th class="al-text"><?php echo $lang['alpha_3'] ?></th>
+                                <th class="al-text"><?php echo $lang['zone_flag'] ?></th>
+                                <th class="al-text-w"></th>
+                            </tr>
+			    <?php } ?>
+                        </thead>
+                        <tbody>
+                            <?php for ($i; $i < $lines_p; $i++) { ?>
+                                <tr>
+                                    <td>
+                                        <form action="/controller/admin/pages/settings/regions.php">
+                                            <input hidden name="zone_id" value="<?php echo $lines[$i][0] ?>">
+                                            <button type="submit" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-cog"></span></button>
+                                        </form>
+                                    </td>
+                                    <td><?php echo $lines[$i][1] ?></td>
+                                    <td class="al-text"><?php echo $lines[$i][2] ?></td>
+                                    <td class="al-text"><?php echo $lines[$i][3] ?></td>
+                                    <td class="al-text"><img src='/view/<?php echo $TEMPLATE ?>/admin/images/worldflags/<?php echo strtolower($lines[$i][2]) ?>.png'></td>
+                                    <td class="al-text-w">
+                                        <form>
+                                            <input hidden name="zone_delete" value="<?php echo $lines[$i][0] ?>">
+                                            <div class="log-right">
+                                                <button type="submit" name="zone_delete_but" class="btn btn-primary btn-xs" data-toggle="confirmation" data-btn-ok-label="<?php echo $lang['confirm-yes'] ?>" data-btn-cancel-label="<?php echo $lang['confirm-no'] ?>" title="<?php echo $lang['confirm-del'] ?>" action="/controller/admin/pages/settings/zones.php" formmethod="get"><span class="glyphicon glyphicon-trash"> </span></button>
+                                            </div>
+                                            <div class="log-left">
+                                                <a href="#zones_edit<?php echo $lines[$i][0] ?>" class="btn btn-primary btn-xs" data-toggle="modal"><span class="glyphicon glyphicon-edit"></span></a>
+                                            </div>
+                                        </form>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
