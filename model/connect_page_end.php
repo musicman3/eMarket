eaadb991d4f9d1a785965d5c69fa9e1cc6300dfb
musicman3 <micromail@mail.ru>
2018-10-25 00:41:58 +0300

    упрощение

--- model/connect_page_end.php
+++ model/connect_page_end.php
@@ -12,7 +12,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/model/connect_end.php');
 require_once($VALID->inSERVER('DOCUMENT_ROOT') . '/model/html_end.php');
 
 //Если существует $JS_END и JS.PHP
-if (isset($JS_END) && file_exists($JS_END . '/js/js.php')) {
+if (isset($JS_END)) {
     //то подгружаем JS.PHP файл
     require_once($JS_END . '/js/js.php');
 }
