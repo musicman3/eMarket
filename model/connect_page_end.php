cfbb2764321a0b0342f159ab0e36bb5c6a0dfca2
musicman3 <micromail@mail.ru>
2018-10-25 00:45:14 +0300

    еще

--- model/connect_page_end.php
+++ model/connect_page_end.php
@@ -11,7 +11,7 @@ require_once($VIEW->Routing());
 require_once($VALID->inSERVER('DOCUMENT_ROOT') . '/model/connect_end.php');
 require_once($VALID->inSERVER('DOCUMENT_ROOT') . '/model/html_end.php');
 
-//Если существует $JS_END и JS.PHP
+//Если существует $JS_END
 if (isset($JS_END)) {
     //то подгружаем JS.PHP файл
     require_once($JS_END . '/js/js.php');
