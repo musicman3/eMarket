ff6a340428a5d04fe279e595cb2ddc9104182a1a
musicman3 <micromail@mail.ru>
2018-10-27 23:31:14 +0300

    фикс

--- view/default/admin/pages/settings/units/index.php
+++ view/default/admin/pages/settings/units/index.php
@@ -15,7 +15,7 @@
 <?php for ($k; $k < $lines_p; $k++) { // запускаем цикл формирования модальных окон  ?>
 
     <!-- Вставляем модальное окно "Редактировать" -->
-    <?php require($VALID->inSERVER('DOCUMENT_ROOT') . '/view/default/admin/pages/settings/units/modal/edit.php') ?>
+    <?php require($VALID->inSERVER('DOCUMENT_ROOT') . '/view/'.$TEMPLATE.'/admin/pages/settings/units/modal/edit.php') ?>
 
 <?php } ?>
 
