9844019934b0984c604c863416d252094d6581ec
musicman3 <micromail@mail.ru>
2018-10-29 14:50:21 +0200

    кнопки больше в настройках

--- view/default/admin/pages/settings/index.php
+++ view/default/admin/pages/settings/index.php
@@ -19,46 +19,46 @@
                         <tbody>
                             <tr>
                                 <td>
-                                    <div class="settings"><a href="countries/" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-globe"> </span></a><?php echo $lang['title_countries'] ?></div>
+                                    <div class="settings"><a href="countries/" class="btn btn-primary btn-xl"><span class="glyphicon glyphicon-globe"> </span></a><?php echo $lang['title_countries'] ?></div>
                                 </td>
                                 <td>
-                                    <div class="settings"><a href="zones/" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-adjust"> </span></a><?php echo $lang['title_zones'] ?></div>
+                                    <div class="settings"><a href="zones/" class="btn btn-primary btn-xl"><span class="glyphicon glyphicon-adjust"> </span></a><?php echo $lang['title_zones'] ?></div>
                                 </td>
                                 <td>
-                                    <div class="settings"><a href="taxes/" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-briefcase"> </span></a><?php echo $lang['title_taxes'] ?></div>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <div class="settings"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-road"> </span></a>Размер</div>
-                                </td>
-                                <td>
-                                    <div class="settings"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-oil"> </span></a>Вес</div>
-                                </td>
-                                <td>
-                                    <div class="settings"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-comment"> </span></a>Языки</div>
+                                    <div class="settings"><a href="taxes/" class="btn btn-primary btn-xl"><span class="glyphicon glyphicon-briefcase"> </span></a><?php echo $lang['title_taxes'] ?></div>
                                 </td>
                             </tr>
                             <tr>
                                 <td>
-                                    <div class="settings"><a href="vendor_codes/" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-tag"> </span></a><?php echo $lang['title_vendor_codes'] ?></div>
+                                    <div class="settings"><a href="#" class="btn btn-default btn-xl disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-road"> </span></a>Размер</div>
                                 </td>
                                 <td>
-                                    <div class="settings"><a href="units/" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-flag"> </span></a><?php echo $lang['title_units'] ?></div>
+                                    <div class="settings"><a href="#" class="btn btn-default btn-xl disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-oil"> </span></a>Вес</div>
                                 </td>
                                 <td>
-                                    <div class="settings"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-barcode"> </span></a>Штриховые коды</div>
+                                    <div class="settings"><a href="#" class="btn btn-default btn-xl disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-comment"> </span></a>Языки</div>
                                 </td>
                             </tr>
                             <tr>
                                 <td>
-                                    <div class="settings"><a href="#" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-cog"> </span></a>Базовые настройки</div>
+                                    <div class="settings"><a href="vendor_codes/" class="btn btn-primary btn-lg"><span class="glyphicon glyphicon-tag"> </span></a><?php echo $lang['title_vendor_codes'] ?></div>
                                 </td>
                                 <td>
-                                    <div class="settings"><a href="#" class="btn btn-default btn-xs disabled"><span class="glyphicon glyphicon-flag"> </span></a>#</div>
+                                    <div class="settings"><a href="units/" class="btn btn-primary btn-xl"><span class="glyphicon glyphicon-flag"> </span></a><?php echo $lang['title_units'] ?></div>
                                 </td>
                                 <td>
-                                    <div class="settings"><a href="#" class="btn btn-default btn-xs disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-barcode"> </span></a>#</div>
+                                    <div class="settings"><a href="#" class="btn btn-default btn-xl disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-barcode"> </span></a>Штриховые коды</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <div class="settings"><a href="#" class="btn btn-primary btn-xl"><span class="glyphicon glyphicon-cog"> </span></a>Базовые настройки</div>
+                                </td>
+                                <td>
+                                    <div class="settings"><a href="#" class="btn btn-default btn-xl disabled"><span class="glyphicon glyphicon-flag"> </span></a>#</div>
+                                </td>
+                                <td>
+                                    <div class="settings"><a href="#" class="btn btn-default btn-xl disabled" role="button" aria-disabled="true"><span class="glyphicon glyphicon-barcode"> </span></a>#</div>
                                 </td>
                             </tr>
                         </tbody>
