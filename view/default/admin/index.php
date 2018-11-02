2b987b843c8a7e9c409edcdeb6945c6bb6f7a87c
musicman3 <micromail@mail.ru>
2018-11-03 01:58:51 +0200

    Merge origin/master
    Conflicts:
    	view/default/admin/index.php

--- view/default/admin/index.php
+++ view/default/admin/index.php
@@ -14,9 +14,15 @@
             <div class="col-md-12">
 
                 <div class="welcome text-center">
+<<<<<<< HEAD
     <?php echo lang('index-title') ?>
                     <div class="welcome_logo">eMarket <span>v1</span></div>
                     <div class="welcome_description"><?php echo lang('index-text') ?>: <strong>***@gmail.com</strong></div>
+=======
+    <?php echo $lang['index-title'] ?>
+                    <div class="welcome_logo">eMarket <span>v 0.1.0</span></div>
+                    <div class="welcome_description"><?php echo $lang['index-text'] ?>: <strong>***@gmail.com</strong></div>
+>>>>>>> origin/master
                 </div>
 
             </div>
