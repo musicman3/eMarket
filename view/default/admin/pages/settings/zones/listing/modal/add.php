2dc9370bc8f0ece6c75f63aef41da383241cde54
musicman3 <micromail@mail.ru>
2018-11-05 21:50:10 +0200

    доработка

--- view/default/admin/pages/settings/zones/listing/modal/add.php
+++ view/default/admin/pages/settings/zones/listing/modal/add.php
@@ -17,8 +17,6 @@
                     <input type="hidden" name="add" value="ok" />
 
                     <!-- Build your select: -->
-
-
                     <script type="text/javascript">
                         $(document).ready(function () {
                             $('#example-enableCollapsibleOptGroups-enableClickableOptGroups-enableFiltering-includeSelectAllOption').multiselect({
@@ -29,8 +27,9 @@
                             });
                         });
                     </script>
-                    
-                    <span class="multiselect-native-select"><select id="example-enableCollapsibleOptGroups-enableClickableOptGroups-enableFiltering-includeSelectAllOption" multiple="multiple">
+
+                    <span class="multiselect-native-select">
+                        <select id="example-enableCollapsibleOptGroups-enableClickableOptGroups-enableFiltering-includeSelectAllOption" multiple="multiple">
                             <optgroup label="Group 1">
                                 <option value="1-1" disabled="">Option 1.1</option>
                                 <option value="1-2" selected="selected">Option 1.2</option>
@@ -42,93 +41,8 @@
                                 <option value="2-3">Option 2.3</option>
                             </optgroup>
                         </select>
-                        
-                        <div class="btn-group open">
-                            
-                            <button type="button" class="multiselect dropdown-toggle btn btn-default" data-toggle="dropdown" title="Option 1.2, Option 1.3" aria-expanded="true">
-                                <span class="multiselect-selected-text">Option 1.2, Option 1.3</span> <b class="caret"></b>
-                            </button>
-                            
-                            <ul class="multiselect-container dropdown-menu">
-                                <li class="multiselect-item multiselect-filter" value="0">
-                                    <div class="input-group">
-                                        <span class="input-group-addon"><i class="glyphicon glyphicon-search"></i></span>
-                                        <input class="form-control multiselect-search" type="text" placeholder="Search">
-                                        
-                                        <button class="btn btn-default multiselect-clear-filter" type="button">
-                                                <i class="glyphicon glyphicon-remove-circle"></i>
-                                        </button>
-                                       
-                                    </div>
-                                </li>
-                                <li class="multiselect-item multiselect-all" style="background: rgb(243, 243, 243) none repeat scroll 0% 0%; border-bottom: 1px solid rgb(234, 234, 234);">
-                                    <a tabindex="0" class="multiselect-all">
-                                        <label class="checkbox" style="padding: 3px 20px 3px 35px;">
-                                            <input type="checkbox" value="multiselect-all">  Select all</label>
-                                    </a>
-                                </li>
-                                <li class="multiselect-item multiselect-group active">
-                                    <a href="javascript:void(0);">
-                                        <label>
-                                            <input type="checkbox" value="undefined">
-                                            <b> Group 1</b>
-                                        </label>
-                                        <span class="caret-container">
-                                            <b class="caret"></b>
-                                        </span>
-                                    </a>
-                                </li>
-                                <li class="disabled"><a tabindex="-1">
-                                        <label class="checkbox" title="Option 1.1">
-                                            <input type="checkbox" value="1-1" disabled=""> Option 1.1</label>
-                                    </a>
-                                </li>
-                                <li class="active">
-                                    <a tabindex="0">
-                                        <label class="checkbox" title="Option 1.2">
-                                            <input type="checkbox" value="1-2"> Option 1.2</label>
-                                    </a>
-                                </li>
-                                <li class="active">
-                                    <a tabindex="0">
-                                        <label class="checkbox" title="Option 1.3">
-                                            <input type="checkbox" value="1-3"> Option 1.3</label>
-                                    </a>
-                                </li>
-                                <li class="multiselect-item multiselect-group">
-                                    <a href="javascript:void(0);">
-                                        <label>
-                                            <input type="checkbox" value="undefined">
-                                            <b> Group 2</b>
-                                        </label>
-                                        <span class="caret-container">
-                                            <b class="caret"></b>
-                                        </span>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a tabindex="0"><label class="checkbox" title="Option 2.1">
-                                            <input type="checkbox" value="2-1"> Option 2.1</label>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a tabindex="0">
-                                        <label class="checkbox" title="Option 2.2">
-                                            <input type="checkbox" value="2-2"> Option 2.2</label>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a tabindex="0">
-                                        <label class="checkbox" title="Option 2.3">
-                                            <input type="checkbox" value="2-3"> Option 2.3</label>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
                     </span>
-
-
-
+                    
                 </div>
 
                 <div class="modal-footer">
