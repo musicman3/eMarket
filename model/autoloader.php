76f5057a13c9c48931c201d7594433f4316cc4d3
musicman3 <micromail@mail.ru>
2018-11-08 03:33:11 +0200

    причесал

--- model/autoloader.php
+++ model/autoloader.php
@@ -13,22 +13,16 @@ spl_autoload_register(function ($class_name) {
 //
 //ЗАГРУЖАЕМ CLASS Valid
 $VALID = new eMarket\Classes\Core\Valid;
-//
 //ЗАГРУЖАЕМ CLASS Pdo
 $PDO = new eMarket\Classes\Core\Pdo;
-//
 //ЗАГРУЖАЕМ CLASS View
 $VIEW = new eMarket\Classes\Core\View;
-
 //ЗАГРУЖАЕМ CLASS Tree
 $TREE = new eMarket\Classes\Core\Tree;
-
 //ЗАГРУЖАЕМ CLASS Navigation
 $NAVIGATION = new eMarket\Classes\Core\Navigation;
-
 //ЗАГРУЖАЕМ CLASS Debug
 $DEBUG = new eMarket\Classes\Other\Debug;
-
 //ЗАГРУЖАЕМ CLASS Func
 $FUNC = new eMarket\Classes\Other\Func;
 ?>
