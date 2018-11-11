01924fd2789286ab416e7ceb84125e3bf775830e
musicman3 <micromail@mail.ru>
2018-11-12 01:53:40 +0200

    причесал

--- controller/admin/pages/settings/zones/listing/index.php
+++ controller/admin/pages/settings/zones/listing/index.php
@@ -10,16 +10,6 @@ error_reporting(-1);
 require_once(getenv('DOCUMENT_ROOT') . '/model/start.php');
 // ************************************** //
 // 
-// Собираем данные для массива Стран
-$countries_multiselect_temp = $PDO->getColRow("SELECT name, id  FROM " . TABLE_COUNTRIES . " WHERE language=?", [$lang_all[0]]);
-// Собираем одномерный массив id=>Страна
-$countries_multiselect = array_column($countries_multiselect_temp, 0, 1);
-// Сортируем Страны по возрастанию
-asort($countries_multiselect);
-
-// Собираем данные для массива Регионов
-$regions_multiselect = $PDO->getColRow("SELECT id, country_id, name, region_code  FROM " . TABLE_REGIONS . " WHERE language=?", [$lang_all[0]]);
-
 // Получаем zones_id
 if ($VALID->inPOST('zone_id')) {
     $zones_id = (int) $VALID->inPOST('zone_id');
@@ -31,10 +21,10 @@ if ($VALID->inGET('zone_id')) {
 
 // Если нажали на кнопку Добавить
 if ($VALID->inPOST('add')) {
-    
+
     //Очищаем страны и регионы от этой зоны
     $PDO->inPrepare("DELETE FROM " . TABLE_ZONES_VALUE . " WHERE zones_id=?", [$zones_id]);
-    
+
     if (empty($VALID->inPOST('multiselect')) == FALSE) {
         //Создаем многомерный массив из одномерного, разбитого на части разделителем "-"
         $multiselect = $FUNC->array_explode($VALID->inPOST('multiselect'), '-');
@@ -46,20 +36,28 @@ if ($VALID->inPOST('add')) {
     }
 }
 
+// Собираем данные для массива Стран в мульиселекте
+$countries_multiselect_temp = $PDO->getColRow("SELECT name, id  FROM " . TABLE_COUNTRIES . " WHERE language=?", [$lang_all[0]]);
+// Собираем одномерный массив id=>Страна
+$countries_multiselect = array_column($countries_multiselect_temp, 0, 1);
+// Сортируем Страны по возрастанию
+asort($countries_multiselect);
+// Собираем данные для массива Регионов в мульиселекте
+$regions_multiselect = $PDO->getColRow("SELECT id, country_id, name, region_code  FROM " . TABLE_REGIONS . " WHERE language=?", [$lang_all[0]]);
+// Собираем название стран и регионов для вывода в View
+$name_country = $PDO->getColRow("SELECT id, name FROM " . TABLE_COUNTRIES . " WHERE language=? ORDER BY id DESC", [$lang_all[0]]);
+$name_regions = $PDO->getColRow("SELECT country_id, name FROM " . TABLE_REGIONS . " WHERE language=? ORDER BY id DESC", [$lang_all[0]]);
+// Собираем данные по сопоставлению Страна->Регионы для конкретной зоны
+$regions = $PDO->getColRow("SELECT country_id, regions_id FROM " . TABLE_ZONES_VALUE . " WHERE zones_id=?", [$zones_id]);
+
 //КНОПКИ НАВИГАЦИИ НАЗАД-ВПЕРЕД И ПОСТРОЧНЫЙ ВЫВОД ТАБЛИЦЫ
 // Собираем данные для вывода списка стран
 $lines_temp = $PDO->getColRow("SELECT country_id FROM " . TABLE_ZONES_VALUE . " WHERE zones_id=?", [$zones_id]);
 $lines = array_values(array_unique($lines_temp, SORT_REGULAR)); // Выбираем по 1 экземпляру стран и сбрасываем ключи массива
-
 $navigate = $NAVIGATION->getLink(count($lines), $lines_of_page = 20);
 $start = $navigate[0];
 $finish = $navigate[1];
 
-// Собираем название стран и регионов для вывода в View
-$name_country = $PDO->getColRow("SELECT id, name FROM " . TABLE_COUNTRIES . " WHERE language=? ORDER BY id DESC", [$lang_all[0]]);
-$name_regions = $PDO->getColRow("SELECT country_id, name FROM " . TABLE_REGIONS . " WHERE language=? ORDER BY id DESC", [$lang_all[0]]);
-
-$regions = $PDO->getColRow("SELECT country_id, regions_id FROM " . TABLE_ZONES_VALUE . " WHERE zones_id=?", [$zones_id]);
 //$DEBUG->var_dump($regions);
 //Создаем маркер для подгрузки JS/JS.PHP в конце перед </body>
 $JS_END = __DIR__;
