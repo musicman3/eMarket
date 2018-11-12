32273e97c7e52b4716c6ac92a4e0573fd875fe7e
Prizraki <lw12@yandex.ru>
2018-11-12 09:51:57 +0300

    Вид | поправлен

--- view/default/admin/pages/settings/zones/listing/index.php
+++ view/default/admin/pages/settings/zones/listing/index.php
@@ -83,11 +83,11 @@
 
                                 <!--Формирование списка для всплывающих подсказок-->
                                 <?php
-                                $text = '|';
+                                $text = '| ';
                                 $y = 0;
                                 for ($x = 0; $x < count($regions); $x++) {
                                     if (isset($regions[$x][0]) == TRUE && isset($lines[$start][0]) == TRUE && $regions[$x][0] == $lines[$start][0]) { // если регион есть
-                                        $text .= $FUNC->filter_array_to_key($name_regions, 0, $regions[$x][0], 1)[$y] . '| '; // то, добавляем название региона
+                                        $text .= $FUNC->filter_array_to_key($name_regions, 0, $regions[$x][0], 1)[$y] . ' | '; // то, добавляем название региона
                                         $y++;
                                     }
                                 }
