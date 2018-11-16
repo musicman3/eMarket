5fecb018189919f79a3bdf624b950e9d5975675a
Prizraki <lw12@yandex.ru>
2018-11-16 19:07:53 +0300

    Правка ссылок

--- view/default/install/footer.php
+++ view/default/install/footer.php
@@ -13,7 +13,7 @@
             <div class="col-md-12">
                 <p><img src="/view/<?php echo $TEMPLATE ?>/install/images/emarket.png" width="80" alt="" class="img-responsive center-block"></p>
 
-                <p>Copyright (c) 2018-<?php echo date('Y') ?>, <a href="https://github.com/musicman3/eMarket">eMarket Team</a>. All rights reserved.</p>
+                <p>Copyright (c) 2018-<?php echo date('Y') ?>, <a target=_blank href="https://github.com/musicman3/eMarket">eMarket Team</a>. All rights reserved.</p>
             </div>
         </div><!-- /row -->
     </div><!-- /container -->
