3689a47227ce0041a3407e44baf12406022bb0e9
musicman3 <micromail@mail.ru>
2018-11-24 23:20:21 +0200

    еще переименовал

--- model/router_lang.php
+++ model/router_lang.php
@@ -23,23 +23,23 @@ $files_path = $TREE->filesTree(getenv('DOCUMENT_ROOT') . '/language/' . $DEFAULT
 $parse_temp = parse_ini_file($files_path[0]);
 for ($i = 0; $i < count($files_path); $i++) {
     $ini = parse_ini_file($files_path[$i]);
-    $_lang = array_merge($parse_temp, $ini); // Установка языкового массива
+    $lang = array_merge($parse_temp, $ini); // Установка языкового массива
 }
 
 // Получаем список языков в массиве (для использования в мультиязычных функциях и т.п.)
-$lang_all = array(); // массив с языками
-$lang_dir = scandir(getenv('DOCUMENT_ROOT') . '/language/');
-$lang_temp = parse_ini_file(getenv('DOCUMENT_ROOT') . '/language/' . $DEFAULT_LANGUAGE . '/admin/lang.lng', TRUE);
-array_push($lang_all, $DEFAULT_LANGUAGE); // первым в массиве идет язык по умолчанию
-foreach ($lang_dir as $lang_name) {
+$_lang_all = array(); // массив с языками
+$_lang_dir = scandir(getenv('DOCUMENT_ROOT') . '/language/');
+$_lang_temp = parse_ini_file(getenv('DOCUMENT_ROOT') . '/language/' . $DEFAULT_LANGUAGE . '/admin/lang.lng', TRUE);
+array_push($_lang_all, $DEFAULT_LANGUAGE); // первым в массиве идет язык по умолчанию
+foreach ($_lang_dir as $_lang_name) {
 
     // Собираем данные для списка языков
-    if (!in_array($lang_name, array('.', '..', $DEFAULT_LANGUAGE))) {
-        array_push($lang_all, $lang_name);
+    if (!in_array($_lang_name, array('.', '..', $DEFAULT_LANGUAGE))) {
+        array_push($_lang_all, $_lang_name);
 
         // Собираем данные из всех general.lng
-        $ini_lang = parse_ini_file(getenv('DOCUMENT_ROOT') . '/language/' . $lang_name . '/admin/lang.lng', TRUE);
-        $lang = array_merge($lang_temp, $ini_lang);
+        $ini_lang = parse_ini_file(getenv('DOCUMENT_ROOT') . '/language/' . $_lang_name . '/admin/lang.lng', TRUE);
+        $_lang = array_merge($_lang_temp, $ini_lang);
     }
 }
 
@@ -50,19 +50,19 @@ foreach ($lang_dir as $lang_name) {
  * @return строка $a
  */
 function lang($a, $b = null) {
-    global $_lang, $lang;
+    global $lang, $_lang;
     // Вывод для основных языковых переменных
     if ($b == null) {
-        if (isset($_lang[$a])) {
-            return $_lang[$a]; // Если языковая переменная найдена, то выводим ее значение
+        if (isset($lang[$a])) {
+            return $lang[$a]; // Если языковая переменная найдена, то выводим ее значение
         } else {
             return $a; // Если языковая переменная не найдена, то выводим ее название
         }
     }
     // Вывод для языковых переменных из lang.lng
     if ($b != null) {
-        if (isset($lang[$b][$a])) {
-            return $lang[$b][$a]; // Если языковая переменная найдена, то выводим ее значение
+        if (isset($_lang[$b][$a])) {
+            return $_lang[$b][$a]; // Если языковая переменная найдена, то выводим ее значение
         } else {
             return $a; // Если языковая переменная не найдена, то выводим ее название
         }
