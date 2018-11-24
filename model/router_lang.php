409df878beb324ec26298fb75b28866ab1c684bf
musicman3 <micromail@mail.ru>
2018-11-25 01:15:41 +0200

    фикс

--- model/router_lang.php
+++ model/router_lang.php
@@ -27,19 +27,19 @@ for ($i = 0; $i < count($files_path); $i++) {
 }
 
 // Получаем список языков в массиве (для использования в мультиязычных функциях и т.п.)
-$_lang_all = array(); // массив с языками
-$_lang_dir = scandir(getenv('DOCUMENT_ROOT') . '/language/');
-$_lang_temp = parse_ini_file(getenv('DOCUMENT_ROOT') . '/language/' . $DEFAULT_LANGUAGE . '/admin/lang.lng', TRUE);
-array_push($_lang_all, $DEFAULT_LANGUAGE); // первым в массиве идет язык по умолчанию
-foreach ($_lang_dir as $_lang_name) {
+$lang_all = array(); // массив с языками
+$lang_dir = scandir(getenv('DOCUMENT_ROOT') . '/language/');
+$lang_temp = parse_ini_file(getenv('DOCUMENT_ROOT') . '/language/' . $DEFAULT_LANGUAGE . '/admin/lang.lng', TRUE);
+array_push($lang_all, $DEFAULT_LANGUAGE); // первым в массиве идет язык по умолчанию
+foreach ($lang_dir as $lang_name) {
 
     // Собираем данные для списка языков
-    if (!in_array($_lang_name, array('.', '..', $DEFAULT_LANGUAGE))) {
-        array_push($_lang_all, $_lang_name);
+    if (!in_array($lang_name, array('.', '..', $DEFAULT_LANGUAGE))) {
+        array_push($lang_all, $lang_name);
 
         // Собираем данные из всех general.lng
-        $ini_lang = parse_ini_file(getenv('DOCUMENT_ROOT') . '/language/' . $_lang_name . '/admin/lang.lng', TRUE);
-        $_lang = array_merge($_lang_temp, $ini_lang);
+        $ini_lang = parse_ini_file(getenv('DOCUMENT_ROOT') . '/language/' . $lang_name . '/admin/lang.lng', TRUE);
+        $_lang = array_merge($lang_temp, $ini_lang);
     }
 }
 
