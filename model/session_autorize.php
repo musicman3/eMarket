3c797c8f36e4d76cf849e8d8a21276642ee23d67
musicman3 <micromail@mail.ru>
2018-12-04 01:13:03 +0200

    Упростил

--- model/session_autorize.php
+++ model/session_autorize.php
@@ -8,31 +8,24 @@
 if ($SET->path() == 'admin' && $SET->titleDir() != 'login') {
 
     session_start();
-    
+
     if (isset($_SESSION['session_start']) && (time() - $_SESSION['session_start']) / 60 > $SET->sessionExprTime()) { // Если истекло время сеанса
         session_destroy();
         header('Location: /controller/admin/login/'); // переадресация на LOGIN
     }
-        $_SESSION['session_start'] = time();
-
-
-    $login = null;
-    $pass = null;
+    $_SESSION['session_start'] = time();
 
     if (isset($_SESSION['login']) && isset($_SESSION['pass'])) {
-        $login = $_SESSION['login'];
-        $pass = $_SESSION['pass'];
+        $VERIFY = $PDO->getRowCount("SELECT * FROM " . TABLE_ADMINISTRATORS . " WHERE login=? AND password=?", [$_SESSION['login'], $_SESSION['pass']]);
     }
 
-    $verify = $PDO->getRowCount("SELECT * FROM " . TABLE_ADMINISTRATORS . " WHERE login=? AND password=?", [$login, $pass]);
-
-    if ($verify != 1) { // Если нет пользователя
+    if (!isset($VERIFY) OR $VERIFY != 1) { // Если нет пользователя
         session_destroy();
         header('Location: /controller/admin/login/'); // переадресация на LOGIN
     } else {
-        $TOKEN = hash(HASH_METHOD, $login . $pass); // создаем токен для ajax и пр.
+        $TOKEN = hash(HASH_METHOD, $_SESSION['login'] . $_SESSION['pass']); // создаем токен для ajax и пр.
         //Язык авторизованного администратора
-        $DEFAULT_LANGUAGE = $PDO->selectPrepare("SELECT language FROM " . TABLE_ADMINISTRATORS . " WHERE login=? AND password=?", [$login, $pass]);
+        $DEFAULT_LANGUAGE = $PDO->selectPrepare("SELECT language FROM " . TABLE_ADMINISTRATORS . " WHERE login=? AND password=?", [$_SESSION['login'], $_SESSION['pass']]);
     }
 }
 
@@ -40,20 +33,15 @@ if ($SET->path() == 'admin' && $SET->titleDir() != 'login') {
 if ($SET->path() == 'catalog' && $SET->titleDir() != 'login') {
 
     session_start();
-    $login = null;
-    $pass = null;
 
     if (isset($_SESSION['login']) && isset($_SESSION['pass'])) {
-        $login = $_SESSION['login'];
-        $pass = $_SESSION['pass'];
+        $VERIFY = $PDO->getRowCount("SELECT * FROM " . TABLE_ADMINISTRATORS . " WHERE login=? AND password=?", [$_SESSION['login'], $_SESSION['pass']]);
     }
 
-    $verify = $PDO->getRowCount("SELECT * FROM " . TABLE_ADMINISTRATORS . " WHERE login=? AND password=?", [$login, $pass]);
-
-    if ($verify != 1) { //NO USER
+    if (!isset($VERIFY) OR $VERIFY != 1) { // Если нет пользователя
         header('Location: /controller/admin/login/'); // переадресация на LOGIN
     } else {
-        $TOKEN = hash(HASH_METHOD, $login . $pass); // создаем токен для ajax и пр.
+        $TOKEN = hash(HASH_METHOD, $_SESSION['login'] . $_SESSION['pass']); // создаем токен для ajax и пр.
         //Язык авторизованного пользователя
         $DEFAULT_LANGUAGE = DEFAULT_LANGUAGE;
     }
