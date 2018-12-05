5de6ad1d1a2e740bd7bd71b8c078fa35a3e3d118
musicman3 <micromail@mail.ru>
2018-12-06 01:13:05 +0200

    лишнее

--- view/default/admin/footer.php
+++ view/default/admin/footer.php
@@ -1,6 +1,6 @@
 <?php
 /* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
-  |    GNU GENERAL PUBLIC LICENSE v.3.0    |    
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 
@@ -21,13 +21,5 @@
             </div><!-- /row -->
         </div><!-- /container -->
     </div><!-- /footerwrap -->
-    
-<?php } 
-if (isset($j) == false) {
-    $j = 0;
-}
-if (isset($TOKEN) == false) {
-    $TOKEN = 0;
-}
 
-?>
+<?php } ?>
