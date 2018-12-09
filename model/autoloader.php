de40fc5c5d801b4a4c39806ac69db2deaded1cca
musicman3 <micromail@mail.ru>
2018-12-10 00:46:53 +0200

    переименовал

--- model/autoloader.php
+++ model/autoloader.php
@@ -31,7 +31,7 @@ $MESSAGES = new eMarket\Other\Messages;
 
 //АВТОЗАГРУЗЧИК ФУНКЦИЙ
 //Получаем список путей к файлам функций
-foreach ($TREE->filesTree(getenv('DOCUMENT_ROOT') . '/model/functions/') as $i) {
-    require_once($i);
+foreach ($TREE->filesTree(getenv('DOCUMENT_ROOT') . '/model/functions/') as $path) {
+    require_once($path);
 }
 ?>
