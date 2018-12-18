11f446fd13e13f9d60256582170ff4dea8d52ebf
musicman3 <micromail@mail.ru>
2018-12-18 19:07:33 +0200

    автопересчет значений веса

--- controller/admin/pages/settings/weight/index.php
+++ controller/admin/pages/settings/weight/index.php
@@ -11,7 +11,7 @@ require_once(getenv('DOCUMENT_ROOT') . '/model/start.php');
 // 
 // Если нажали на кнопку Добавить
 if ($VALID->inGET('add')) {
-    
+
     // Если есть установка по-умолчанию
     if ($VALID->inGET('default_weight')) {
         $default_weight = 1;
@@ -26,17 +26,30 @@ if ($VALID->inGET('add')) {
     // Оставляем один экземпляр значения по-умолчанию
     if ($id > 1 && $default_weight != 0) {
         $PDO->inPrepare("UPDATE " . TABLE_WEIGHT . " SET default_weight=?", [0]);
-    }
 
-    // добавляем запись для всех вкладок
-    for ($xl = 0; $xl < count(lang('#lang_all')); $xl++) {
-        $PDO->inPrepare("INSERT INTO " . TABLE_WEIGHT . " SET id=?, name=?, language=?, code=?, value_weight=?, default_weight=?", [$id, $VALID->inGET($SET->titleDir() . '_' . lang('#lang_all')[$xl]), lang('#lang_all')[$xl], $VALID->inGET('code' . lang('#lang_all')[$xl]), $VALID->inGET('value_weight'), $default_weight]);
+        //Пересчитываем значения
+        $value_weight_all = $PDO->getColAssoc("SELECT id, value_weight, language FROM " . TABLE_WEIGHT, []);
+        $count_value_weight_all = count($value_weight_all);
+        for ($xl = 0; $xl < $count_value_weight_all; $xl++) {
+            $PDO->inPrepare("UPDATE " . TABLE_WEIGHT . " SET value_weight=? WHERE id=? AND language=?", [($value_weight_all[$xl]['value_weight'] / $VALID->inGET('value_weight')), $value_weight_all[$xl]['id'], $value_weight_all[$xl]['language']]);
+        }
+        
+        // добавляем запись для всех вкладок
+        for ($xl = 0; $xl < count(lang('#lang_all')); $xl++) {
+            $PDO->inPrepare("INSERT INTO " . TABLE_WEIGHT . " SET id=?, name=?, language=?, code=?, value_weight=?, default_weight=?", [$id, $VALID->inGET($SET->titleDir() . '_' . lang('#lang_all')[$xl]), lang('#lang_all')[$xl], $VALID->inGET('code' . lang('#lang_all')[$xl]), 1, $default_weight]);
+        }
+    } else {
+
+        // добавляем запись для всех вкладок
+        for ($xl = 0; $xl < count(lang('#lang_all')); $xl++) {
+            $PDO->inPrepare("INSERT INTO " . TABLE_WEIGHT . " SET id=?, name=?, language=?, code=?, value_weight=?, default_weight=?", [$id, $VALID->inGET($SET->titleDir() . '_' . lang('#lang_all')[$xl]), lang('#lang_all')[$xl], $VALID->inGET('code' . lang('#lang_all')[$xl]), $VALID->inGET('value_weight'), $default_weight]);
+        }
     }
 }
 
 // Если нажали на кнопку Редактировать
 if ($VALID->inGET('edit')) {
-    
+
     // Если есть установка по-умолчанию
     if ($VALID->inGET('status_weight_edit')) {
         $default_weight = 1;
@@ -47,11 +60,24 @@ if ($VALID->inGET('edit')) {
     // Оставляем один экземпляр значения по-умолчанию
     if ($default_weight != 0) {
         $PDO->inPrepare("UPDATE " . TABLE_WEIGHT . " SET default_weight=?", [0]);
-    }
 
-    for ($xl = 0; $xl < count(lang('#lang_all')); $xl++) {
-        // обновляем запись
-        $PDO->inPrepare("UPDATE " . TABLE_WEIGHT . " SET name=?, code=?, value_weight=?, default_weight=? WHERE id=? AND language=?", [$VALID->inGET('name_edit_' . $SET->titleDir() . '_' . lang('#lang_all')[$xl]), $VALID->inGET('code_edit_' . $SET->titleDir() . '_' . lang('#lang_all')[$xl]), $VALID->inGET('value_weight_edit'), $default_weight, $VALID->inGET('edit'), lang('#lang_all')[$xl]]);
+        //Пересчитываем значения
+        $value_weight_all = $PDO->getColAssoc("SELECT id, value_weight, language FROM " . TABLE_WEIGHT, []);
+        $count_value_weight_all = count($value_weight_all);
+        for ($xl = 0; $xl < $count_value_weight_all; $xl++) {
+            $PDO->inPrepare("UPDATE " . TABLE_WEIGHT . " SET value_weight=? WHERE id=? AND language=?", [($value_weight_all[$xl]['value_weight'] / $VALID->inGET('value_weight_edit')), $value_weight_all[$xl]['id'], $value_weight_all[$xl]['language']]);
+        }
+
+        for ($xl = 0; $xl < count(lang('#lang_all')); $xl++) {
+            // обновляем запись
+            $PDO->inPrepare("UPDATE " . TABLE_WEIGHT . " SET name=?, code=?, value_weight=?, default_weight=? WHERE id=? AND language=?", [$VALID->inGET('name_edit_' . $SET->titleDir() . '_' . lang('#lang_all')[$xl]), $VALID->inGET('code_edit_' . $SET->titleDir() . '_' . lang('#lang_all')[$xl]), 1, $default_weight, $VALID->inGET('edit'), lang('#lang_all')[$xl]]);
+        }
+    } else {
+
+        for ($xl = 0; $xl < count(lang('#lang_all')); $xl++) {
+            // обновляем запись
+            $PDO->inPrepare("UPDATE " . TABLE_WEIGHT . " SET name=?, code=?, value_weight=?, default_weight=? WHERE id=? AND language=?", [$VALID->inGET('name_edit_' . $SET->titleDir() . '_' . lang('#lang_all')[$xl]), $VALID->inGET('code_edit_' . $SET->titleDir() . '_' . lang('#lang_all')[$xl]), $VALID->inGET('value_weight_edit'), $default_weight, $VALID->inGET('edit'), lang('#lang_all')[$xl]]);
+        }
     }
 }
 
@@ -61,9 +87,7 @@ if ($VALID->inPOST('delete')) {
     // Удаляем
     $PDO->inPrepare("DELETE FROM " . TABLE_WEIGHT . " WHERE id=?", [$VALID->inPOST('delete')]);
 }
-
-//$DEBUG->trace($PDO->getColAssoc("SELECT id, value_weight FROM " . TABLE_WEIGHT ." WHERE language=?", [lang('#lang_all')[0]]));
-
+//print_r($value_weight_all[0]['language']);
 //КНОПКИ НАВИГАЦИИ НАЗАД-ВПЕРЕД И ПОСТРОЧНЫЙ ВЫВОД ТАБЛИЦЫ
 $lines = $PDO->getColRow("SELECT id, name, code, value_weight, default_weight FROM " . TABLE_WEIGHT . " WHERE language=? ORDER BY id DESC", [lang('#lang_all')[0]]);
 $lines_on_page = $SET->linesOnPage();
