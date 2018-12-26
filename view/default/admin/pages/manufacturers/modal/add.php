7d3c26664dbee70429f3a22d258190df3dd93718
Prizraki <lw12@yandex.ru>
2018-12-26 20:09:49 +0300

    Еще

--- view/default/admin/pages/manufacturers/modal/add.php
+++ view/default/admin/pages/manufacturers/modal/add.php
@@ -59,8 +59,7 @@
                         <div class="form-group">
                             <!-- The fileinput-button span is used to style the file input field as button -->
                             <span class="btn btn-success fileinput-button">
-                                <i class="glyphicon glyphicon-plus"></i>
-                                <span>Select files...</span>
+                                <i class="glyphicon glyphicon-plus"></i><span> Select files...</span>
                                 <!-- The file input field used as target for the file upload widget -->
                                 <input class="input-sm form-control" id="fileupload" type="file" name="files[]" multiple>
                             </span>
