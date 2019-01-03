5cae720c5be907ff4f57961dd4cc5903bc86e8a1
musicman3 <micromail@mail.ru>
2019-01-03 18:29:05 +0200

    размер иконки 120х120

--- downloads/upload_handler/UploadHandler.php
+++ downloads/upload_handler/UploadHandler.php
@@ -181,8 +181,8 @@ class UploadHandler
                     // 'jpeg_quality' => 70,
                     // 'no_cache' => true, (there's a caching option, but this remembers thumbnail sizes from a previous action!)
                     // 'strip' => true, (this strips EXIF tags, such as geolocation)
-                    'max_width' => 80, // either specify width, or set to 0. Then width is automatically adjusted - keeping aspect ratio to a specified max_height.
-                    'max_height' => 80 // either specify height, or set to 0. Then height is automatically adjusted - keeping aspect ratio to a specified max_width.
+                    'max_width' => 120, // either specify width, or set to 0. Then width is automatically adjusted - keeping aspect ratio to a specified max_height.
+                    'max_height' => 120 // either specify height, or set to 0. Then height is automatically adjusted - keeping aspect ratio to a specified max_width.
                 )
             ),
             'print_response' => true
