da9de5cdd656120ee5f0cc33dcaf6a070737629a
musicman3 <micromail@mail.ru>
2019-01-09 16:05:18 +0200

    еще коммент

--- controller/admin/pages/manufacturers/index.php
+++ controller/admin/pages/manufacturers/index.php
@@ -29,8 +29,8 @@ if ($VALID->inPOST('edit')) {
 }
 
 // Загручик изображений (ВСТАВЛЯТЬ ПЕРЕД УДАЛЕНИЕМ)
-$image_max = [];// Прописываем используемые размеры по высоте ['94', '150', '244', '394', '638']
-array_push($image_max, [null,'94']);
+$image_max = [];// Размеры по Y ['94', '150', '244', '394', '638'] пропорционально X= 125, 200, 325, 525, 850 при 4:3
+array_push($image_max, [null,'94']); // X, Y
 //array_push($image_max, ['150','150']);
 
 $FILES->imgUpload(TABLE_MANUFACTURERS, 'manufacturers', $image_max);
