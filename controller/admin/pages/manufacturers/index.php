3e6e141b27e13e35f521d42ef04d484a07125e0f
musicman3 <micromail@mail.ru>
2019-01-12 01:14:07 +0200

    упростил

--- controller/admin/pages/manufacturers/index.php
+++ controller/admin/pages/manufacturers/index.php
@@ -33,8 +33,10 @@ if ($VALID->inPOST('edit')) {
 $image_max = []; // Размеры по Y ['94', '150', '244', '394', '638'] пропорционально X= 125, 200, 325, 525, 850 при 4:3
 array_push($image_max, ['125', '94']); // X, Y
 //array_push($image_max, ['150','150']);
+
+$count_image_max = count($image_max);
 $resize_max = [];
-array_push($resize_max, ['125', '94']); // X, Y
+array_push($resize_max, [$image_max[$count_image_max-1][0], $image_max[$count_image_max-1][1]]); // X, Y
 
 $FILES->imgUpload(TABLE_MANUFACTURERS, 'manufacturers', $image_max);
 
