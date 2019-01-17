5084c0a5b42de0fe529ade5a0b4c6bac66f6e857
musicman3 <micromail@mail.ru>
2019-01-17 12:40:55 +0200

    упрощаем

--- controller/admin/pages/manufacturers/index.php
+++ controller/admin/pages/manufacturers/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
   |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
@@ -16,12 +17,8 @@ if ($VALID->inPOST('add')) {
         if ($VALID->inPOST($SET->titleDir() . '_' . lang('#lang_all')[$x]) == NULL) {
             $_SESSION['message'] = ['danger', lang('action_completed_failed')];
             goto end_add;
-        } else {
-            $_SESSION['message'] = ['success', lang('action_completed_successfully')];
         }
     }
-    // Разрешаем добавить изображения
-    $_SESSION['add_image'] = 'ok';
 
     // Получаем последний id и увеличиваем его на 1
     $id_max = $PDO->selectPrepare("SELECT id FROM " . TABLE_MANUFACTURERS . " WHERE language=? ORDER BY id DESC", [lang('#lang_all')[0]]);
@@ -31,6 +28,10 @@ if ($VALID->inPOST('add')) {
     for ($x = 0; $x < $LANG_COUNT; $x++) {
         $PDO->inPrepare("INSERT INTO " . TABLE_MANUFACTURERS . " SET id=?, name=?, language=?, site=?", [$id, $VALID->inPOST($SET->titleDir() . '_' . lang('#lang_all')[$x]), lang('#lang_all')[$x], $VALID->inPOST('site')]);
     }
+
+    // Разрешаем добавить изображения
+    $_SESSION['message'] = ['success', lang('action_completed_successfully')];
+    $_SESSION['add_image'] = 'ok';
     end_add:
 }
 
@@ -41,16 +42,16 @@ if ($VALID->inPOST('edit')) {
         if ($VALID->inPOST('name_edit_' . $SET->titleDir() . '_' . lang('#lang_all')[$x]) == NULL) {
             $_SESSION['message'] = ['danger', lang('action_completed_failed')];
             goto end_edit;
-        } else {
-            $_SESSION['message'] = ['success', lang('action_completed_successfully')];
         }
     }
-    // Разрешаем добавить изображения
-    $_SESSION['edit_image'] = 'ok';
 
     for ($x = 0; $x < $LANG_COUNT; $x++) {
         $PDO->inPrepare("UPDATE " . TABLE_MANUFACTURERS . " SET name=?, site=? WHERE id=? AND language=?", [$VALID->inPOST('name_edit_' . $SET->titleDir() . '_' . lang('#lang_all')[$x]), $VALID->inPOST('site_edit'), $VALID->inPOST('edit'), lang('#lang_all')[$x]]);
     }
+
+    // Разрешаем добавить изображения
+    $_SESSION['message'] = ['success', lang('action_completed_successfully')];
+    $_SESSION['edit_image'] = 'ok';
     end_edit:
 }
 
@@ -84,5 +85,4 @@ $JS_END = __DIR__;
 /* ->-->-->-->  CONNECT PAGE END  <--<--<--<- */
 require_once(ROOT . '/model/end.php');
 /* ------------------------------------------ */
-
 ?>
