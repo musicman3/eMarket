31d1babc4b3f518ea8e58a0f71522f212ee3d701
musicman3 <micromail@mail.ru>
2019-01-26 10:36:35 +0200

    фикс модала в модале

--- controller/admin/pages/stock/js/js.php
+++ controller/admin/pages/stock/js/js.php
@@ -546,6 +546,41 @@ if (!isset($idsx_real_parent_id)) {
             ]
         });
     });
+    // Фикс модала в модале
+    $(document).on("show.bs.modal", '.modal', function (event) {
+        console.log("Global show.bs.modal fire");
+        var zIndex = 100000 + (10 * $(".modal:visible").length);
+        $(this).css("z-index", zIndex);
+        setTimeout(function () {
+            $(".modal-backdrop").not(".modal-stack").first().css("z-index", zIndex - 1).addClass("modal-stack");
+        }, 0);
+
+    }).on("hidden.bs.modal", '.modal', function (event) {
+        console.log("Global hidden.bs.modal fire");
+        $(".modal:visible").length && $("body").addClass("modal-open");
+    });
+
+    $(document).on('inserted.bs.tooltip', function (event) {
+        console.log("Global show.bs.tooltip fire");
+        var zIndex = 100000 + (10 * $(".modal:visible").length);
+        var tooltipId = $(event.target).attr("aria-describedby");
+        $("#" + tooltipId).css("z-index", zIndex);
+    });
+
+    $(document).on('inserted.bs.popover', function (event) {
+        console.log("Global inserted.bs.popover fire");
+        var zIndex = 100000 + (10 * $(".modal:visible").length);
+        var popoverId = $(event.target).attr("aria-describedby");
+        $("#" + popoverId).css("z-index", zIndex);
+    });
+
+    //Если закрыли модальное окно #add_product
+    $('#add_product').on('hidden.bs.modal', function (event) {
+        // Destroy Summernote
+        for (var x = 0; x < count_lang; x++) {
+            $('#add_product_' + x).summernote('destroy');
+        }
+    });
 
     count_lang = '<?php echo $LANG_COUNT ?>';
 
