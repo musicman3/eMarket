c34830b420cb0cf776358750ce922329aa4ac811
musicman3 <micromail@mail.ru>
2019-01-26 11:07:41 +0200

    упростил фикс

--- controller/admin/pages/stock/js/js.php
+++ controller/admin/pages/stock/js/js.php
@@ -547,31 +547,15 @@ if (!isset($idsx_real_parent_id)) {
         });
     });
     // Фикс модала в модале
-    $(document).on("show.bs.modal", '.modal', function (event) {
-        console.log("Global show.bs.modal fire");
-        var zIndex = 100000 + (10 * $(".modal:visible").length);
-        $(this).css("z-index", zIndex);
-        setTimeout(function () {
-            $(".modal-backdrop").not(".modal-stack").first().css("z-index", zIndex - 1).addClass("modal-stack");
-        }, 0);
-
-    }).on("hidden.bs.modal", '.modal', function (event) {
-        console.log("Global hidden.bs.modal fire");
+    $(document).on("hidden.bs.modal", '.modal', function (event) {
         $(".modal:visible").length && $("body").addClass("modal-open");
     });
 
     $(document).on('inserted.bs.tooltip', function (event) {
-        console.log("Global show.bs.tooltip fire");
         var zIndex = 100000 + (10 * $(".modal:visible").length);
-        var tooltipId = $(event.target).attr("aria-describedby");
-        $("#" + tooltipId).css("z-index", zIndex);
-    });
-
-    $(document).on('inserted.bs.popover', function (event) {
-        console.log("Global inserted.bs.popover fire");
-        var zIndex = 100000 + (10 * $(".modal:visible").length);
-        var popoverId = $(event.target).attr("aria-describedby");
-        $("#" + popoverId).css("z-index", zIndex);
+        var Id = $(event.target).attr("aria-describedby");
+        $("#" + Id).css("z-index", zIndex);
+        $("#" + Id).css("z-index", zIndex);
     });
 
     //Если закрыли модальное окно #add_product
