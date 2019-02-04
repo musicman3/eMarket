9bf81bb1de07a8f5729fb99dfc956208ee7966ac
Prizraki <lw12@yandex.ru>
2019-02-04 18:23:03 +0300

    Вывод во фронт

--- view/default/catalog/index.php
+++ view/default/catalog/index.php
@@ -159,67 +159,16 @@ $(document).ready(function(){
 
 			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item">
 			    <div class="productHolder">
-				<div class="list-item"><a href="#"><img src="/uploads/images/products/resize_4/2.png" class="img-responsive img-rounded center-block"><button class="hidden-xs hidden-sm" data-toggle="modal" data-target="#Modal1">Быстрый просмотр</button></a></div>
+				<div class="list-item"><a href="<?php echo $products_new[0][0]; ?>"><img src="/uploads/images/products/resize_4/2.png" class="img-responsive img-rounded center-block"></a></div>
 				<div>
-				    <h5 class="text-center list-group-item-heading"><a href="#">Unreal Tournament Primary Holder Primary Holder</a></h5>
+				    <h5 class="text-center list-group-item-heading"><a href="<?php echo $products_new[0][0]; ?>"><?php echo $products_new[0][1]; ?></a></h5>
 				    <div class="clearfix"></div>
 				    <div class="row button">
 					<div class="col-xs-6">
-					    <button type="button" class="btn btn-default btn-sm">$241 189.99</button>
+					    <button type="button" class="btn btn-default btn-sm"><?php echo $products_new[0][12]; ?></button>
 					</div>
 					<div class="col-xs-6 text-right">
-					    <a id="btn1" href="#" class="btn btn-primary btn-sm">Buy Now</a>
-					</div>
-				    </div>
-				</div>
-			    </div>
-			</div>
-			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item">
-			    <div class="productHolder">
-				<div class="list-item"><a href="#"><img src="/uploads/images/products/resize_4/2.png" class="img-responsive img-rounded center-block"><button class="hidden-xs hidden-sm" data-toggle="modal" data-target="#Modal2">Быстрый просмотр</button></a></div>
-				<div>
-				    <h5 class="text-center list-group-item-heading"><a href="#">Unreal Tournament Primary Holder</a></h5>
-				    <div class="clearfix"></div>
-				    <div class="row button">
-					<div class="col-xs-6">
-					    <button type="button" class="btn btn-default btn-sm">$241 189.99</button>
-					</div>
-					<div class="col-xs-6 text-right">
-					    <a id="btn1" href="#" class="btn btn-primary btn-sm">Buy Now</a>
-					</div>
-				    </div>
-				</div>
-			    </div>
-			</div>
-			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item">
-			    <div class="productHolder">
-				<div class="list-item"><a href="#"><img src="/uploads/images/products/resize_4/3.png" class="img-responsive img-rounded center-block"><button class="hidden-xs hidden-sm" data-toggle="modal" data-target="#Modal3">Быстрый просмотр</button></a></div>
-				<div>
-				    <h5 class="text-center list-group-item-heading"><a href="#">Unreal Tournament Primary Holder</a></h5>
-				    <div class="clearfix"></div>
-				    <div class="row button">
-					<div class="col-xs-6">
-					    <button type="button" class="btn btn-default btn-sm">$241 189.99</button>
-					</div>
-					<div class="col-xs-6 text-right">
-					    <a id="btn1" href="#" class="btn btn-primary btn-sm">Buy Now</a>
-					</div>
-				    </div>
-				</div>
-			    </div>
-			</div>
-			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item">
-			    <div class="productHolder">
-				<div class="list-item"><a href="#"><img src="/uploads/images/products/resize_4/4.png" class="img-responsive img-rounded center-block"><button class="hidden-xs hidden-sm" data-toggle="modal" data-target="#Modal3">Быстрый просмотр</button></a></div>
-				<div>
-				    <h5 class="text-center list-group-item-heading"><a href="#">Unreal Tournament Primary Holder</a></h5>
-				    <div class="clearfix"></div>
-				    <div class="row button">
-					<div class="col-xs-6">
-					    <button type="button" class="btn btn-default btn-sm">$241 189.99</button>
-					</div>
-					<div class="col-xs-6 text-right">
-					    <a id="btn1" href="#" class="btn btn-primary btn-sm">Buy Now</a>
+					    <a id="btn1" href="<?php echo $products_new[0][0]; ?>" class="btn btn-primary btn-sm">Buy Now</a>
 					</div>
 				    </div>
 				</div>
