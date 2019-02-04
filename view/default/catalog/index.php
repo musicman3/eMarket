d6c516ad67ff72d9f4a3a67df664df09c3c0465a
musicman3 <micromail@mail.ru>
2019-02-04 20:34:10 +0200

    размер

--- view/default/catalog/index.php
+++ view/default/catalog/index.php
@@ -140,7 +140,7 @@ $(document).ready(function(){
 			<?php for ($x=0; $x<count($products_new); $x++){ ?>
 			<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item">
 			    <div class="productHolder">
-				<div class="list-item"><a href="<?php echo $products_new[$x][0]; ?>"><img src="/uploads/images/products/resize_2/<?php echo $products_new[$x][7]; ?>" class="img-responsive img-rounded center-block"></a></div>
+				<div class="list-item"><a href="<?php echo $products_new[$x][0]; ?>"><img src="/uploads/images/products/resize_1/<?php echo $products_new[$x][7]; ?>" class="img-responsive img-rounded center-block"></a></div>
 				<div>
 				    <h5 class="text-center list-group-item-heading"><a href="<?php echo $products_new[$x][0]; ?>"><?php echo $products_new[$x][1]; ?></a></h5>
 				    <div class="clearfix"></div>
