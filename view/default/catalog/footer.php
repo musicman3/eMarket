0bb81d3d29dba17a198c41cd5a82ee28b3515031
Prizraki <lw12@yandex.ru>
2019-02-06 18:53:56 +0300

    Расположение

---
+++ view/default/catalog/footer.php
@@ -0,0 +1,64 @@
+<?php
+/* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
+  |  https://github.com/musicman3/eMarket  |
+  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
+?>
+
+<?php if (isset($_SESSION['login']) && isset($_SESSION['pass'])) { // Выводим если авторизованы    ?>
+
+    <div id="footerwrap">
+        <footer class="clearfix"></footer>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <p><img src="/view/<?php echo $SET->template() ?>/admin/images/emarket.png" width="57" alt="" class="img-responsive center-block"></p>
+
+                    <p>Copyright © 2018-<?php echo date('Y') ?>, <a target=_blank href="https://github.com/musicman3/eMarket">eMarket Team</a>. All rights reserved.</p>
+                </div>
+            </div><!-- /row -->
+        </div><!-- /container -->
+    </div><!-- /footerwrap -->
+
+<script type="text/javascript">
+$(document).ready(function() {
+    $('.navbar a.dropdown-toggle').on('click', function(e) {
+        var $el = $(this);
+        var $parent = $(this).offsetParent(".dropdown-menu");
+        $(this).parent("li").toggleClass('open');
+
+        if(!$parent.parent().hasClass('nav')) {
+            $el.next().css({"top": $el[0].offsetTop - 9, "left": $parent.outerWidth() - 2});
+        }
+
+        $('.nav li.open').not($(this).parents("li")).removeClass("open");
+
+        return false;
+    });
+});
+
+$(function() {
+  var changeHeightNavbarCollapse = function() {
+    $('.navbar-collapse').css({
+      maxHeight: $(window).height() - $('.navbar-header').height() + 'px'
+    });
+  };
+
+  changeHeightNavbarCollapse();
+
+  $(window).resize(function() {
+    if (window.innerWidth <= 767) {
+      changeHeightNavbarCollapse();
+    }
+  });
+
+});
+</script>
+
+<script type="text/javascript">
+    $('[data-toggle=confirmation]').confirmation();
+</script>
+
+<?php } ?>
