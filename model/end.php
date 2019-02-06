2293b93ffe2096ec185286c3b4b687bc55725093
musicman3 <micromail@mail.ru>
2019-02-06 16:37:36 +0200

    разбил на 3 настроечных html

--- model/end.php
+++ model/end.php
@@ -1,12 +1,24 @@
 <?php
+
 /* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
-  |    GNU GENERAL PUBLIC LICENSE v.3.0    |    
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 // 
 //Загружаем HTML
-require_once(getenv('DOCUMENT_ROOT') . '/model/html.php');
-
+//
+//ВЫВОД ТОЛЬКО В АДМИНКЕ
+if ($SET->path() == 'admin') {
+    require_once(getenv('DOCUMENT_ROOT') . '/model/html_admin.php');
+}
+//ВЫВОД ТОЛЬКО В КАТАЛОГЕ
+if ($SET->path() == 'catalog') {
+    require_once(getenv('DOCUMENT_ROOT') . '/model/html_catalog.php');
+}
+//ВЫВОД ТОЛЬКО В КАТАЛОГЕ
+if ($SET->path() == 'install') {
+    require_once(getenv('DOCUMENT_ROOT') . '/model/html_install.php');
+}
 //Закрываем соединение с БД
-$PDO->connect('end'); 
+$PDO->connect('end');
 ?>
---
+++ model/html_install.php
@@ -0,0 +1,47 @@
+<?php
+/* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
+  |  https://github.com/musicman3/eMarket  |
+  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+?>
+
+<!doctype html>
+<html dir="ltr" lang="<?php echo lang('meta-language') ?>">
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="noindex,nofollow" />
+        <meta name="generator" content="HippoEDIT, Netbeans, Notepad++" />
+        <meta name="classification" content="software" />
+        <meta name="author" content="eMarket" />
+        <meta name="owner" content="eMarket" />
+        <meta name="copyright" content="Copyright © 2018 by eMarket Team. All right reserved." />
+        <!-- Автогенерация Title" -->
+        <title><?php echo lang('title_' . $SET->titleDir() . '_' . basename($VALID->inSERVER('PHP_SELF'), '.php')) ?></title>
+
+        <link href="/ext/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+        <link href="/ext/bootstrap/css/normalize.css" rel="stylesheet" media="screen" />
+        <link rel="stylesheet" type="text/css" href="/view/<?php echo $SET->template() ?>/install/style.css" media="screen" />
+        <script type="text/javascript" src="/ext/jquery/jquery.min.js"></script>
+    </head>
+    <body>
+        <?php
+        // ЗАГРУЖАЕМ ТЕЛО HTML СТРАНИЦЫ
+        require_once($VIEW->routing());
+
+        require_once(getenv('DOCUMENT_ROOT') . '/controller/install/footer.php');
+        require_once(getenv('DOCUMENT_ROOT') . '/view/' . $SET->template() . '/install/footer.php');
+        ?>
+
+        <script type="text/javascript" src="/ext/bootstrap/js/bootstrap.min.js"></script>
+
+        <?php
+        //Если существует $JS_END
+        if (isset($JS_END)) {
+            //то подгружаем JS.PHP файл
+            require_once($JS_END . '/js/js.php');
+        }
+        ?>
+    </body>
+</html>
