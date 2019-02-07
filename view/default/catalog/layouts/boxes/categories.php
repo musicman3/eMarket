dc2c3dd5a4da1522bbbf1d1f49cc3a0067c12e15
musicman3 <micromail@mail.ru>
2019-02-07 17:30:53 +0200

    файл бокса категорий

---
+++ view/default/catalog/layouts/boxes/categories.php
@@ -0,0 +1,29 @@
+<?php
+/* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |    
+  |  https://github.com/musicman3/eMarket  |
+  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
+?>
+
+    <h3>Name Categories</h3>
+    <div class="contentContainer">
+	<div class="contentText">
+
+	    <div id="products" class="row list-group">
+		<div class="item col-sm-4 list-group-item">
+		    <div class="productHolder">
+			<a href="#><img src="images.jpg" alt="" title="" class="img-responsive img-rounded group list-group-image"></a>
+			<h4 class="group inner list-group-item-heading"><a href="#">Name</a></h4>
+			<p class="group inner list-group-item-text">Text…</p>
+			<div class="clearfix"></div>
+			<div class="row button">
+			    <div class="col-xs-6"><button type="button" class="btn btn-default">Price</button></div>
+			    <div class="col-xs-6 text-right"><a id="btn1" href="#" class="btn btn-primary btn-sm"><span class="cart"></span> Buy Now</a></div>
+			</div>
+		    </div>
+		</div>
+	    </div>
+
+	</div>
+    </div>
