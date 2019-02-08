9e6b1eecc84c741e5f16752e6e310ec96a349671
musicman3 <micromail@mail.ru>
2019-02-08 18:31:45 +0200

    сортировка

--- view/default/admin/pages/settings/templates/index.php
+++ view/default/admin/pages/settings/templates/index.php
@@ -3,7 +3,10 @@
   |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
 ?>
+
+
 
 <div id="settings_templates" class="container-fluid hidden-sm hidden-xs">
     <div class="row-fluid">
@@ -15,24 +18,31 @@
                 </h3>
             </div>
             <div class="panel-body">
-		<table class="table table-bordered">
-		<tbody>
-		    <tr>
-			<td class="text-center" colspan="3">Навигация</td>
-			<td class="text-center" rowspan="4">Модули</td>
-		    </tr>
-		    <tr>
-			<td class="text-center" colspan="3">Хедер</td>
-		    </tr>
-		    <tr>
-			<td class="text-center">Левый</td>
-			<td class="text-center">Центр</td>
-			<td class="text-center">Правый</td>
-		    </tr>
-		    <tr>
-			<td class="text-center" colspan="3">Футер</td>
-		    </tr>
-		</tbody>
+                <table class="table table-bordered">
+                    <tbody>
+                        <tr>
+                            <td class="text-center connectedSortable" colspan="3" id="sortable2">Навигация</td>
+                            <td class="text-center connectedSortable" rowspan="4" id="sortable1">Модули
+
+                                <div id="1" class="text-center">Item 1</div>
+                                <div id="2" class="text-center">Item 2</div>
+                                <div id="3" class="text-center">Item 3</div>
+                                <div id="4" class="text-center">Item 4</div>
+
+                            </td>
+                        </tr>
+                        <tr>
+                            <td class="text-center connectedSortable" colspan="3" id="sortable3">Хедер</td>
+                        </tr>
+                        <tr>
+                            <td class="text-center connectedSortable" id="sortable4">Левый</td>
+                            <td class="text-center connectedSortable" id="sortable5">Центр</td>
+                            <td class="text-center connectedSortable" id="sortable6">Правый</td>
+                        </tr>
+                        <tr>
+                            <td class="text-center connectedSortable" colspan="3" id="sortable7">Футер</td>
+                        </tr>
+                    </tbody>
                 </table>
             </div>
         </div>
@@ -49,8 +59,26 @@
                 </h3>
             </div>
             <div class="panel-body">
-		На планшетах и смартфонах не доступно
+                На планшетах и смартфонах не доступно
             </div>
         </div>
     </div>
 </div>
+
+<script>
+    $(function () {
+        $("#sortable1, #sortable2, #sortable3, #sortable4, #sortable5, #sortable6, #sortable7").sortable({
+            connectWith: ".connectedSortable",
+            stop: function (event, div) {
+                var arrList2 = $('#sortable2 div').map(function () {
+                    return $(this).attr('id');
+                }).get();
+
+                var arrList3 = $('#sortable3 div').map(function () {
+                    return $(this).attr('id');
+                }).get();
+                //alert(arrList3);
+            }
+        }).disableSelection();
+    });
+</script>
