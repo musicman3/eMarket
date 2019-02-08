d7341361ba1abc409245812ce34c656a17fc2de1
Prizraki <lw12@yandex.ru>
2019-02-08 12:42:37 +0300

    Таблица в шаблоны

--- view/default/admin/pages/settings/templates/index.php
+++ view/default/admin/pages/settings/templates/index.php
@@ -5,3 +5,52 @@
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 ?>
 
+<div id="templates" class="container-fluid hidden-sm hidden-xs">
+    <div class="row-fluid">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    <div class="pull-left">Шаблон</div>
+                    <div class="clearfix"></div>
+                </h3>
+            </div>
+            <div class="panel-body">
+		<table class="table table-bordered">
+		<tbody>
+		    <tr>
+			<td class="text-center" colspan="3">Навигация</td>
+			<td class="text-center" rowspan="4">Модули</td>
+		    </tr>
+		    <tr>
+			<td class="text-center" colspan="3">Хедер</td>
+		    </tr>
+		    <tr>
+			<td class="text-center">Левый</td>
+			<td class="text-center">Центр</td>
+			<td class="text-center">Правый</td>
+		    </tr>
+		    <tr>
+			<td class="text-center" colspan="3">Футер</td>
+		    </tr>
+		</tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="templates" class="container-fluid visible-sm visible-xs">
+    <div class="row-fluid">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">
+                    <div class="pull-left">Шаблон</div>
+                    <div class="clearfix"></div>
+                </h3>
+            </div>
+            <div class="panel-body">
+		На планшетах и смартфонах не доступно
+            </div>
+        </div>
+    </div>
+</div>
