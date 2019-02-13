a45bbc527beac414caeade6791ba163970b52858
musicman3 <micromail@mail.ru>
2019-02-13 13:53:14 +0200

    перенес в JS

--- controller/admin/pages/settings/templates/index.php
+++ controller/admin/pages/settings/templates/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
   |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
@@ -7,11 +8,11 @@
 /* >-->-->-->  CONNECT PAGE START  <--<--<--< */
 require_once(getenv('DOCUMENT_ROOT') . '/model/start.php');
 /* ------------------------------------------ */
-// 
 
+//Создаем маркер для подгрузки JS/JS.PHP в конце перед </body>
+$JS_END = __DIR__;
 
 /* ->-->-->-->  CONNECT PAGE END  <--<--<--<- */
 require_once(ROOT . '/model/end.php');
 /* ------------------------------------------ */
-
 ?>
--- controller/admin/pages/settings/templates/js/js.php
+++ controller/admin/pages/settings/templates/js/js.php
@@ -3,6 +3,68 @@
   |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
-
 ?>
 
+<script>
+    $(function () {
+        $("#sortable1, #sortable2").sortable({
+            connectWith: ".connectedSortable",
+            items: "li:not(.sortno)",
+            over: function (event, ui) {
+                ui.helper.css("color", "#204d74");
+            },
+            beforeStop: function (event, ui) {
+                ui.helper.css("color", "");
+            },
+            stop: function (event, li) {
+                var arrList2 = $('#sortable1 li').map(function () {
+                    return $(this).attr('id');
+                }).get();
+                var arrList3 = $('#sortable2 li').map(function () {
+                    return $(this).attr('id');
+                }).get();
+                //alert(arrList3);
+            }
+        });
+
+        $("#sortable3, #sortable4, #sortable5, #sortable6").sortable({
+            connectWith: ".connectedSortable2",
+            items: "li:not(.sortno)",
+            over: function (event, ui) {
+                ui.helper.css("color", "#204d74");
+            },
+            beforeStop: function (event, ui) {
+                ui.helper.css("color", "");
+            },
+            stop: function (event, li) {
+                var arrList2 = $('#sortable1 li').map(function () {
+                    return $(this).attr('id');
+                }).get();
+                var arrList3 = $('#sortable2 li').map(function () {
+                    return $(this).attr('id');
+                }).get();
+                //alert(arrList3);
+            }
+        });
+
+        $("#sortable7, #sortable8").sortable({
+            connectWith: ".connectedSortable3",
+            items: "li:not(.sortno)",
+            over: function (event, ui) {
+                ui.helper.css("color", "#204d74");
+            },
+            beforeStop: function (event, ui) {
+                ui.helper.css("color", "");
+            },
+            stop: function (event, li) {
+                var arrList2 = $('#sortable1 li').map(function () {
+                    return $(this).attr('id');
+                }).get();
+                var arrList3 = $('#sortable2 li').map(function () {
+                    return $(this).attr('id');
+                }).get();
+                //alert(arrList3);
+            }
+        });
+    });
+</script>
