4e015e0b7b19261360d6cfe60bdbd2093efb598f
Prizraki <lw12@yandex.ru>
2019-02-20 11:00:04 +0300

    Фикс

--- view/default/catalog/layouts/logo_search.php
+++ view/default/catalog/layouts/logo_search.php
@@ -7,21 +7,23 @@
 ?>
 
 <div id="header" class="container-fluid">
-    <div class="col-sm-4">
-        <a href=""><img class="logo img-responsive pull-left" src="/view/<?php echo $SET->template() ?>/catalog/images/emarket.png"></a>
-    </div>
-    <div class="col-sm-8">
-	<div class="searchbox-margin">
-	    <form name="quick_find" action="#" method="get" class="form-horizontal">
-		<div class="input-group">
-		    <input type="search" name="keywords" required="" placeholder="Search" class="form-control">
-		    <span class="input-group-btn">
-			<button type="submit" class="btn btn-primary">
-				<i class="glyphicon glyphicon-search"></i>
-			</button>
-		    </span>
-		</div>
-	    </form>
+    <div class="row">
+	<div class="col-sm-4">
+	    <a href=""><img class="logo img-responsive pull-left" src="/view/<?php echo $SET->template() ?>/catalog/images/emarket.png"></a>
+	</div>
+	<div class="col-sm-8">
+	    <div class="searchbox-margin">
+		<form name="quick_find" action="#" method="get" class="form-horizontal">
+		    <div class="input-group">
+			<input type="search" name="keywords" required="" placeholder="Search" class="form-control">
+			<span class="input-group-btn">
+			    <button type="submit" class="btn btn-primary">
+				    <i class="glyphicon glyphicon-search"></i>
+			    </button>
+			</span>
+		    </div>
+		</form>
+	    </div>
 	</div>
     </div>
 </div>
