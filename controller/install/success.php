a47215c92fc18408f96da261f1502153f5981901
musicman3 <micromail@mail.ru>
2019-02-24 16:36:05 +0200

    убираем pages в ссылке по каталогу

--- controller/install/success.php
+++ controller/install/success.php
@@ -152,7 +152,11 @@ RewriteEngine On
 RewriteCond %{DOCUMENT_ROOT}/controller/catalog/$1 -d
 RewriteRule ^(.*)$ controller/catalog/$1 [L,QSA]
 RewriteCond %{DOCUMENT_ROOT}/controller/catalog/$1 -f
-RewriteRule ^(.*)$ controller/catalog/$1 [L,QSA]";
+RewriteRule ^(.*)$ controller/catalog/$1 [L,QSA]
+RewriteCond %{DOCUMENT_ROOT}/controller/catalog/pages/$1 -d
+RewriteRule ^(.*)$ controller/catalog/pages/$1 [L,QSA]
+RewriteCond %{DOCUMENT_ROOT}/controller/catalog/pages/$1 -f
+RewriteRule ^(.*)$ controller/catalog/pages/$1 [L,QSA]";
 
 // Если файл существует, то ставим права 777
 if (file_exists(ROOT . '/.htaccess') && !is_writeable(ROOT . '/.htaccess')) {
