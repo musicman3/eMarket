857c6abe706cfcf1be9d4e80f7ef728f5372e0bc
Prizraki <lw12@yandex.ru>
2019-02-27 17:02:25 +0300

    Правка

--- view/default/catalog/layouts/categories_listing.php
+++ view/default/catalog/layouts/categories_listing.php
@@ -10,7 +10,7 @@
     <div id="categories_listing" class="row grid-group">
 
 	<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item">
-	    <div class="productHolder">
+	    <div class="categoriesHolder">
 		<a href="#"><img src="1.jpg" class="img-responsive img-rounded center-block"></a>
 		<h5 class="text-center"><a href="#">Categories 1</a></h5>
 	    </div>
