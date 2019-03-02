5cdb7895f93cffee3b19f38d70e48ee0bce39443
Prizraki <lw12@yandex.ru>
2019-03-02 21:19:54 +0300

    Правка листинга

--- view/default/catalog/pages/listing/index.php
+++ view/default/catalog/pages/listing/index.php
@@ -18,17 +18,15 @@ foreach ($VIEW->layoutRouting('content') as $path) {
         <?php foreach ($products as $value) { ?>
 	<div class="list-group-item">
 	    <div class="productHolder">
-		<div class="col-xs-2">
-		    <a href="/products/?id=<?php echo $value[0] ?>"><img src="/uploads/images/products/resize_1/<?php echo $value[2] ?>" class="img-responsive list-group-image"></a>
-		</div>
-		<div class="col-xs-10">
+		<a href="/products/?id=<?php echo $value[0] ?>"><img src="/uploads/images/products/resize_1/<?php echo $value[2] ?>" class="img-responsive list-group-image"></a>
+		<div class="caption">
 		    <h4 class="list-item-heading"><a href="/products/?id=<?php echo $value[0] ?>"><?php echo $value[1] ?></a></h4>
 		    <div class="list-item-text"><?php echo $value[4] ?></div>
 		</div>
 		<div class="clearfix"></div>
 		<div class="row button">
-		    <div class="col-xs-6"><button type="button" class="btn btn-default"><?php echo $PRODUCTS->productPrice($value[3], $CURRENCIES, 1) ?></button></div>
-		    <div class="col-xs-6 text-right"><a id="btn1" href="#" class="btn btn-primary"><span class="cart"></span> Buy Now</a></div>
+		    <div class="col-md-6 col-xs-12"><button type="button" class="btn btn-default"><?php echo $PRODUCTS->productPrice($value[3], $CURRENCIES, 1) ?></button></div>
+		    <div class="col-md-6 col-xs-12 text-right"><a id="btn1" href="#" class="btn btn-primary"><span class="cart"></span> Buy Now</a></div>
 		</div>
 	    </div>
 	</div>
