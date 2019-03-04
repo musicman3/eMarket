3daf6a74d90793ec16a93e3b26ce92806f58288c
musicman3 <micromail@mail.ru>
2019-03-04 02:26:49 +0200

    сохранение состояния переключателя через куки

--- view/default/catalog/pages/listing/index.php
+++ view/default/catalog/pages/listing/index.php
@@ -17,17 +17,43 @@ foreach ($VIEW->layoutRouting('content') as $path) {
         $(".grid-item-heading").simpleEQH();
     });
 </script>
+
+<!-- Функция для установки Cookie -->
+<script type="text/javascript">
+    function setCookie(key, value) {
+        var expires = new Date();
+        expires.setTime(expires.getTime() + (1 * 24 * 60 * 60 * 1000));
+        document.cookie = key + '=' + value + ';expires=' + expires.toUTCString();
+    }
+
+    function getCookie(key) {
+        var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+        return keyValue ? keyValue[2] : null;
+    }
+</script>
+
 <script type="text/javascript" language="javascript">
     $(document).ready(function () {
+
+        if (getCookie('cookie_list') === 'list') {
+            $('#listing .item').removeClass('col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item');
+            $('#listing .item').addClass('col-xs-12 list-group-item');
+        } if (getCookie('cookie_list') === 'grid'){
+            $('#listing .item').removeClass('col-xs-12 list-group-item');
+            $('#listing .item').addClass('col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item');
+        }
+        
         $('#list').click(function (event) {
             event.preventDefault();
             $('#listing .item').removeClass('col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item');
             $('#listing .item').addClass('col-xs-12 list-group-item');
+            setCookie('cookie_list', 'list');
         });
         $('#grid').click(function (event) {
             event.preventDefault();
             $('#listing .item').removeClass('col-xs-12 list-group-item');
             $('#listing .item').addClass('col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item');
+            setCookie('cookie_list', 'grid');
         });
     });
 </script>
@@ -43,7 +69,7 @@ foreach ($VIEW->layoutRouting('content') as $path) {
     </div>
     <div id="listing" class="row">
         <?php foreach ($products as $value) { ?>
-            <div class="item col-xs-12 list-group-item">
+            <div class="item col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item">
                 <div class="productHolder">
                     <a href="/?route=products&id=<?php echo $value[0] ?>"><img src="/uploads/images/products/resize_1/<?php echo $value[2] ?>" class="img-responsive"></a>
                     <div class="caption">
