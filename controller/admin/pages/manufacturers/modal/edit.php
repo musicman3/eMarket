2b2921769adeb927fe75b991a70ec96ea145bc84
musicman3 <micromail@mail.ru>
2019-03-05 22:54:56 +0200

    оптимизация БД производителей

--- controller/admin/pages/manufacturers/modal/edit.php
+++ controller/admin/pages/manufacturers/modal/edit.php
@@ -14,9 +14,11 @@ for ($i = $start; $i < $finish; $i++) {
         for ($x = 0; $x < $count_lang; $x++) {
             $name_edit_temp[$x][$modal_id] = $PDO->selectPrepare("SELECT name FROM " . TABLE_MANUFACTURERS . " WHERE id=? and language=?", [$modal_id, lang('#lang_all')[$x]]);
         }
-        $site_edit_temp[$modal_id] = $PDO->selectPrepare("SELECT site FROM " . TABLE_MANUFACTURERS . " WHERE id=?", [$modal_id]);
-        $logo_edit_temp[$modal_id] = explode(',', $PDO->selectPrepare("SELECT logo FROM " . TABLE_MANUFACTURERS . " WHERE id=?", [$modal_id]), -1);
-        $logo_general_edit_temp[$modal_id] = $PDO->selectPrepare("SELECT logo_general FROM " . TABLE_MANUFACTURERS . " WHERE id=?", [$modal_id]);
+        
+        $query = $PDO->getColRow("SELECT site, logo, logo_general FROM " . TABLE_MANUFACTURERS . " WHERE id=?", [$modal_id])[0];
+        $site_edit_temp[$modal_id] = $query[0];
+        $logo_edit_temp[$modal_id] = explode(',', $query[1], -1);
+        $logo_general_edit_temp[$modal_id] = $query[2];
         // ПАРАМЕТРЫ ДЛЯ ПЕРЕДАЧИ В МОДАЛ
         $name_edit = json_encode($name_edit_temp); // Имя
         $site_edit = json_encode($site_edit_temp); // Короткое имя
