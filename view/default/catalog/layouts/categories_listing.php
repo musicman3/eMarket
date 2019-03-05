a03a222077f71e5db146423c86468aaa59d97e8f
Prizraki <lw12@yandex.ru>
2019-03-05 09:54:33 +0300

    Вывод когда есть

--- view/default/catalog/layouts/categories_listing.php
+++ view/default/catalog/layouts/categories_listing.php
@@ -6,6 +6,7 @@
 
 ?>
 
+<?php if ($categories == true) { ?>
 <div class="contentText">
     <h4>Categories</h4>
     <div id="categories_listing" class="row grid-group">
@@ -13,10 +14,13 @@
         <?php foreach ($categories as $value) { ?>
             <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item">
                 <div class="categoriesHolder">
+                    <?php if ($value[2] == true) { ?>
                     <a href="/?route=listing&category_id=<?php echo $value[0] ?>&parent_id=<?php echo $VALID->inGET('category_id') ?>"><img src="/uploads/images/categories/resize_0/<?php echo $value[2] ?>" class="img-responsive img-rounded center-block"></a>
+                    <?php } ?>
                     <h5 class="text-center"><a href="/?route=listing&category_id=<?php echo $value[0] ?>&parent_id=<?php echo $VALID->inGET('category_id') ?>"><?php echo $value[1] ?></a></h5>
                 </div>
             </div>
         <?php } ?>  
     </div>
 </div>
+<?php } ?>
