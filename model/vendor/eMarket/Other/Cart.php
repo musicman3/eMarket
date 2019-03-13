be604038fd77744e847ab8aadcdfdffbbab17cfa
musicman3 <micromail@mail.ru>
2019-03-14 00:21:56 +0200

    еще немного

--- model/vendor/eMarket/Other/Cart.php
+++ model/vendor/eMarket/Other/Cart.php
@@ -1,5 +1,4 @@
 <?php
-
 /* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
   |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
@@ -22,12 +21,19 @@ class Cart {
      * @param string $id (ID товара)
      */
     public function add($id, $quantity = null) {
-
+        
+        $count = 0;
         if (!isset($_SESSION['cart'])) {
             $_SESSION['cart'] = [['id' => $id, 'quantity' => $quantity]];
-        }
-        if (isset($_SESSION['cart'])) {
-            array_push($_SESSION['cart'], [['id' => 2, 'quantity' => 2]]);
+        } else {
+            foreach ($_SESSION['cart'] as $value) {
+                if ($value['id'] != $id) {
+                    array_push($_SESSION['cart'], ['id' => $id, 'quantity' => $quantity]);
+                } elseif ($value['id'] == $id) {
+                    $_SESSION['cart'][$count]['quantity'] = $_SESSION['cart'][$count]['quantity'] + $quantity;
+                }
+                $count++;
+            }
         }
     }
 
