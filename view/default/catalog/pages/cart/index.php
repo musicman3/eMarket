20e33f5428c7b4bdba8a90c769b5ceccd9709a47
Prizraki <lw12@yandex.ru>
2019-03-15 10:42:15 +0300

    Стрелки добавления кол-ва товара

--- view/default/catalog/pages/cart/index.php
+++ view/default/catalog/pages/cart/index.php
@@ -22,7 +22,7 @@ foreach ($VIEW->layoutRouting('content') as $path) {
                         <td class="text-left"><a href="/">Название товара</a></td>
                         <td class="text-left">
                             <div class="input-group btn-block">
-                                <input type="text" class="form-control quantity" size="1" value="1" name="quantity">
+                                <input type="number" class="form-control quantity" value="1" name="quantity" min="0">
                                 <span class="input-group-btn">
                                     <button class="btn btn-primary" type="submit"><span class="glyphicon glyphicon-refresh"></span></button>
                                     <button class="btn btn-primary" type="button"><span class="glyphicon glyphicon-trash"></span></button>
