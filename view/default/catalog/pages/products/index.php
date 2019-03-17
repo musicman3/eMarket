e4ded6531607f412606643acb7f7e026f4efcd3f
Prizraki <lw12@yandex.ru>
2019-03-17 11:28:56 +0300

    Еще чуть чуть и все

--- view/default/catalog/pages/products/index.php
+++ view/default/catalog/pages/products/index.php
@@ -21,14 +21,12 @@
 	</div>
     </div>
     <div class="row">
-	<div class="list-group-item">
-	    <ul class="nav nav-tabs">
-		<li class="active"><a data-toggle="tab" href="#panel_add_1"><?php echo lang('description_product') ?></a></li>
-	    </ul>
-	    <div class="tab-content">
-		<div id="panel_add_1" class="tab-pane fade in active">
-		    <div class="item-text"><?php echo $products['description'] ?></div>
-		</div>
+	<ul class="nav nav-tabs">
+	    <li class="active"><a data-toggle="tab" href="#panel_add_1"><?php echo lang('description_product') ?></a></li>
+	</ul>
+	<div class="tab-content">
+	    <div id="panel_add_1" class="tab-pane fade in active">
+		<div class="item-text"><?php echo $products['description'] ?></div>
 	    </div>
 	</div>
     </div>
