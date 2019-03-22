7b3ce76510b5164a7ff655157cb5f78c3dcebe53
Prizraki <lw12@yandex.ru>
2019-03-22 16:33:45 +0300

    Слайды

---
+++ view/default/admin/pages/slideshow/index.php
@@ -0,0 +1,75 @@
+<?php
+/* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
+  |  https://github.com/musicman3/eMarket  |
+  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
+?>
+<!-- Модальное окно "Добавить" -->
+<?php require_once('modal/add.php') ?>
+<!-- КОНЕЦ Модальное окно "Добавить" -->
+
+<!-- Модальное окно "Редактировать" -->
+<?php require(ROOT . '/view/' . $SET->template() . '/admin/pages/slideshow/modal/edit.php') ?>
+<!-- КОНЕЦ Модальное окно "Редактировать" -->
+
+<div id="ajax">
+    <div id="slideshow" class="container-fluid">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <!--Выводим уведомление об успешном действии-->
+                    <?php $MESSAGES->alert(); ?>
+                    <h3 class="panel-title">
+                        <div class="pull-left"><?php echo lang('title_' . $SET->titleDir() . '_index') ?></div>
+                        <div class="clearfix"></div>
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th colspan="3">
+                                        <div class="page">с 1 по 1 ( из 1 )</div>
+                                </th>
+
+                                <th>
+                                
+                                    <div class="right"><a href="#add" class="btn btn-primary btn-xs" data-toggle="modal"><span class="glyphicon glyphicon-plus"></span></a></div>
+                                
+                                    <form>
+                                            <div class="left"><button type="submit" class="btn btn-primary btn-xs" formmethod="get"><span class="glyphicon glyphicon-chevron-right"></span></button></div>
+                                    </form>
+
+                                    <form>
+                                            <div class="left"><button type="submit" class="btn btn-primary btn-xs" formmethod="get"><span class="glyphicon glyphicon-chevron-left"></span></button></div>
+                                    </form>
+
+                                </th>
+                            </tr>
+                                <tr class="border">
+                                    <th>Рисунок</th>
+                                    <th class="al-text">Ссылка</th>
+                                    <th class="al-text">Активна</th>
+                                    <th class="al-text-w"></th>
+                                </tr>
+                        </thead>
+                        <tbody>
+                                <tr>
+                                    <td>Рисунок</td>
+                                    <td class="al-text">Ссылка</td>
+                                    <td class="al-text">Активна</td>
+                                    <td class="al-text-w">
+                                            <div class="right">
+                                                <button type="submit" name="delete_but" class="btn btn-primary btn-xs" data-toggle="confirmation" data-btn-ok-label="<?php echo lang('confirm-yes') ?>" data-btn-cancel-label="<?php echo lang('confirm-no') ?>" title="<?php echo lang('confirm-del') ?>"><span class="glyphicon glyphicon-trash"> </span></button>
+                                            </div>
+                                        <div class="left">
+                                            <button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#edit"><span class="glyphicon glyphicon-edit"></span></button>
+                                        </div>
+                                    </td>
+                                </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+    </div>
+</div>
---
+++ view/default/admin/pages/slideshow/modal/add.php
@@ -0,0 +1,101 @@
+<?php
+/* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
+  |  https://github.com/musicman3/eMarket  |
+  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+?>
+<!-- Модальное окно "Добавить" -->
+<div id="add" class="modal fade" tabindex="-1">
+    <div class="modal-dialog">
+        <div class="modal-content">
+
+            <div class="modal-header">
+		<div class="pull-right"><a href="#" ><span data-toggle="tooltip" data-placement="left" data-original-title="Сокращенное наименование указывается любыми символами" class="glyphicon glyphicon-question-sign"></span></a>&nbsp;&nbsp;<button class="close" type="button" data-dismiss="modal">×</button></div>
+                <h4 class="modal-title"><?php echo lang('title_' . $SET->titleDir() . '_index') ?></h4>
+            </div>
+            
+            <div class="panel-body">
+		<ul class="nav nav-tabs">
+                    <li class="active"><a data-toggle="tab" href="#panel_add_1">Основное</a></li>
+                    <li><a data-toggle="tab" href="#panel_add_2">Изображения</a></li>
+                </ul>
+                <div class="tab-content">
+		    <div id="panel_add_1" class="tab-pane fade in active">
+			<ul class="nav nav-tabs">
+			    <li class="active"><a data-toggle="tab" href="#russian"><img src="/view/default/admin/images/langflags/russian.png" alt="russian" title="russian" width="16" height="10"> Русский</a></li>
+			</ul>
+			<!-- Содержимое языковых панелей -->
+			<div class="tab-content">
+			    <div id="russian" class="tab-pane fade in active">
+				<div class="form-group">
+				    <div class="input-group has-success">
+					<span class="input-group-addon"><span class="glyphicon glyphicon-globe"></span></span>
+					<input class="input-sm form-control" placeholder="Адрес ссылки" type="text" name="" id="" required />
+				    </div>
+				</div>
+				<div class="form-group">
+				    <div class="input-group has-success">
+					<span class="input-group-addon"><span class="glyphicon glyphicon-list-alt"></span></span>
+					<input class="input-sm form-control" placeholder="Заголовок (?)" type="text" name="" id="" required />
+				    </div>
+				</div>
+				<div class="form-group">
+				    <div class="input-group has-success">
+					<span class="input-group-addon"><span class="glyphicon glyphicon-list-alt"></span></span>
+					<input class="input-sm form-control" placeholder="Текст (?)" type="text" name="" id="" required />
+				    </div>
+				</div>
+			    </div>
+			</div>
+		
+			<div class="form-group">
+			    <div class="input-group has-error">
+				<span class="input-group-addon"><span class="glyphicon glyphicon-globe"></span></span>
+				<input class="input-sm form-control" placeholder="Интервал (мс)" type="text" name="" id="" required />
+			    </div>
+			</div>
+
+			<label>Останавливать смену слайда при нахождении курсора? </label>
+			<input class="check-box" type="checkbox" checked="">
+			<br>
+			<label>Зацикливать смену слайдов? </label>
+			<input class="check-box" type="checkbox" checked="">
+			<br>
+			<label>Показывать индикаторы? </label>
+			<input class="check-box" type="checkbox" checked="">
+			<br>
+			<label>Показывать стрелки навигации? </label>
+			<input class="check-box" type="checkbox" checked="">
+			<br>
+			<label>Отображать? </label>
+			<input class="check-box" type="checkbox" checked="">
+
+			<!-- Выводим сообщения -->
+			<div id="alert_messages_add"></div>
+		    </div>
+		    <div id="panel_add_2" class="tab-pane fade">
+			<!-- ЗАГРУЗКА jQuery-File-Upload -->
+			<div class="form-group">
+			    <span class="btn btn-primary btn-sm fileinput-button">
+				<i class="glyphicon glyphicon-picture"></i><span> <?php echo lang('button_add_image') ?></span>
+				<input class="input-sm form-control" id="fileupload-add" type="file" name="files[]" accept="image/jpeg,image/png,image/gif" multiple>
+			    </span>
+			    <div id="progress" class="progress">
+				<div class="progress-bar progress-bar-warning progress-bar-striped active"></div>
+			    </div>
+			    <div id="logo-add" class="text-center"></div>
+			</div>
+		    </div>
+		</div>
+
+            </div>
+
+            <div class="modal-footer">
+                <button class="btn btn-primary btn-xs" type="button" data-dismiss="modal"><span class="glyphicon glyphicon-floppy-remove"></span> <?php echo lang('cancel') ?></button>
+                <button type="submit" class="btn btn-primary btn-xs"><span class="glyphicon glyphicon-floppy-disk"></span> <?php echo lang('save') ?></button>
+            </div>
+
+        </div>
+    </div>
+</div>
+<!-- КОНЕЦ Модальное окно "Добавить" -->
