d4d176da23839ac2a29dcb7e2411d1ffe226b796
musicman3 <micromail@mail.ru>
2019-03-25 23:36:57 +0200

    правка бд

--- model/databases/myisam.sql
+++ model/databases/myisam.sql
@@ -22,11 +22,11 @@ CREATE TABLE emkt_basic_settings (
 	lines_on_page int DEFAULT '20' NOT NULL,
         session_expr_time int DEFAULT '60' NOT NULL,
         debug int DEFAULT '0' NOT NULL,
-        host_email varchar(128) DEFAULT 'smtp.localhost.ru' NOT NULL,
+        host_email varchar(128) DEFAULT 'smtp.mail.ru' NOT NULL,
         username_email varchar(128) DEFAULT 'login' NOT NULL,
         password_email varchar(128) DEFAULT 'password' NOT NULL,
-        smtp_secure varchar(64) DEFAULT 'tls' NOT NULL,
-        smtp_port int DEFAULT '587' NOT NULL,
+        smtp_secure varchar(64) DEFAULT 'ssl' NOT NULL,
+        smtp_port int DEFAULT '465' NOT NULL,
         smtp_auth int DEFAULT '0' NOT NULL,
         smtp_status int DEFAULT '0' NOT NULL,
         email varchar(128) DEFAULT 'sale@localhost.ru' NOT NULL,
@@ -10249,7 +10249,7 @@ INSERT INTO emkt_regions VALUES(4248, 238, 'MV', 'Masvingo', 'English');
 INSERT INTO emkt_regions VALUES(4249, 238, 'MW', 'Mashonaland West', 'English');
 
 /* ЗАГРУЗКА НАСТРОЕК */
-INSERT INTO emkt_basic_settings VALUES (1, 20, 60, 0, 'smtp.localhost', 'login', 'password', 'tls', 587, 0, 0, 'sale@localhost', 'eMarket');
+INSERT INTO emkt_basic_settings VALUES (1, 20, 60, 0, 'smtp.mail.ru', 'login', 'password', 'ssl', 465, 0, 0, 'sale@localhost.ru', 'eMarket');
 
 /* ЗАГРУЗКА ЗОН */
 INSERT INTO emkt_zones VALUES (1, 'Moskow', null, 'english');
