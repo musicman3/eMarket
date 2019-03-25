29512e48eae1c287ee1021b306626801ac44c492
musicman3 <micromail@mail.ru>
2019-03-25 21:24:14 +0200

    автотестовое письмо

--- controller/admin/pages/settings/basic_settings/index.php
+++ controller/admin/pages/settings/basic_settings/index.php
@@ -58,6 +58,8 @@ if ($VALID->inPOST('email')) {
     $_SESSION['message'] = ['success', lang('action_completed_successfully')];
     // Считываем значение
     $email = $PDO->getCell("SELECT email FROM " . TABLE_BASIC_SETTINGS . "", []);
+    
+    $MESSAGES->sendMail($VALID->inPOST('email'));
 }
 
 // E-Mail Имя отправителя
