1854d3a9138a51a65dca559e742c42626455a628
musicman3 <micromail@mail.ru>
2019-03-28 02:02:55 +0200

    упростил

--- controller/catalog/pages/login/index.php
+++ controller/catalog/pages/login/index.php
@@ -7,9 +7,9 @@
 if ($VALID->inGET('activation_code')) {
     $id_actvation = $PDO->selectPrepare("SELECT id FROM " . TABLE_CUSTOMERS_ACTIVATION . " WHERE activation_code=?", [$VALID->inGET('activation_code')]);
     if ($id_actvation != NULL) {
-        $account_date = $PDO->selectPrepare("SELECT date_account_created FROM " . TABLE_CUSTOMERS . " WHERE id=?", [$id_actvation]);
+        $account_date = $PDO->selectPrepare("SELECT UNIX_TIMESTAMP (date_account_created) FROM " . TABLE_CUSTOMERS . " WHERE id=?", [$id_actvation]);
         // Если дата активации не истекла
-        if (strtotime($account_date) + (3 * 24 * 60 * 60) > time()) {
+        if ($account_date + (3 * 24 * 60 * 60) > time()) {
             $PDO->inPrepare("DELETE FROM " . TABLE_CUSTOMERS_ACTIVATION . " WHERE id=?", [$id_actvation]);
             $PDO->inPrepare("UPDATE " . TABLE_CUSTOMERS . " SET status=? WHERE id=?", [1, $id_actvation]);
             $_SESSION['message'] = ['success', lang('messages_activation_complete'), 7000, TRUE];
