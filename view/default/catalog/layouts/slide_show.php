8c8e27258c0c11cdaa8e8044452bcd203240f388
Prizraki <lw12@yandex.ru>
2019-03-29 09:49:12 +0300

    Добавляем к рисункам alt

--- view/default/catalog/layouts/slide_show.php
+++ view/default/catalog/layouts/slide_show.php
@@ -10,15 +10,17 @@
     <div id="Carousel" class="carousel slide hidden-sm hidden-xs" data-interval="5000" data-pause="hover" data-ride="carousel" data-wrap="true">
         <ol class="carousel-indicators">
             <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+            <li data-target="#Carousel" data-slide-to="1" class=""></li>
         </ol>
         <div class="carousel-inner" role="listbox" >
             <div class="item active">
 		<a href="#">
-		    <img class="center-block" src="/uploads/images/slideshow/resize_0/slider-1.jpg">
-		    <div class="carousel-caption">
-		    <h3>Los Angeles</h3>
-		    <p>LA is always so much fun!</p>
-		    </div>
+		    <img src="http://www.make-sign.com/uploads/201714834/ImgScroll/ba201709141552443132191.jpg" alt="" class="center-block" >
+		</a>
+	    </div>
+            <div class="item">
+		<a href="#">
+		    <img class="center-block" src="https://armored.com.ua/files/img_front/main-5.jpg">
 		</a>
 	    </div>
         </div>
