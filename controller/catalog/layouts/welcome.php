8233eefff292400ac8d27d2b82f54702511dd00c
musicman3 <micromail@mail.ru>
2019-03-31 22:40:26 +0300

    имя в приветствии если залогинен

--- controller/catalog/layouts/welcome.php
+++ controller/catalog/layouts/welcome.php
@@ -1,7 +1,11 @@
 <?php
 /* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
-  |    GNU GENERAL PUBLIC LICENSE v.3.0    |    
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
+if (isset($_SESSION['email_customer'])) {
+    $name = $PDO->selectPrepare("SELECT lastname FROM " . TABLE_CUSTOMERS . " WHERE email=?", [$_SESSION['email_customer']]);
+}
 
 ?>
--- view/default/catalog/layouts/welcome.php
+++ view/default/catalog/layouts/welcome.php
@@ -1,12 +1,16 @@
 <?php
 /* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
-  |    GNU GENERAL PUBLIC LICENSE v.3.0    |    
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 
 ?>
 
 <div class="contentText">
-    <h4>Welcome Guest!</h4>
+    <?php if ($CUSTOMER == FALSE) { ?>
+        <h4><?php echo sprintf(lang('welcome_name'), lang('welcome_guest')) ?></h4>
+    <?php } else { ?>
+        <h4><?php echo sprintf(lang('welcome_name'), $name) ?></h4>
+    <?php } ?>
     <p>The default shopping cart comes with Jquery UI, Grid960, Fancybox and BxGallery, in this demo those have been replaced by Boostrap and Bootstrap 3 Lightbox making it lighter, faster and responsive.</p>
 </div>
