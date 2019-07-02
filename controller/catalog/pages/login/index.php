e2fdd7c112fe427ad150028066286849e365dbbd
musicman3 <micromail@mail.ru>
2019-07-03 00:55:43 +0300

    перенес

--- controller/catalog/pages/login/index.php
+++ controller/catalog/pages/login/index.php
@@ -31,15 +31,6 @@ if ($VALID->inPOST('email')) {
     }
 }
 
-if ($VALID->inPOST('email_for_recovery')) {
-    $recovery = $PDO->getCellFalse("SELECT email FROM " . TABLE_CUSTOMERS . " WHERE email=?", [$VALID->inPOST('email_for_recovery')]);
-    if ($recovery != FALSE) {
-        $_SESSION['message'] = ['success', lang('password_recovery_message_success'), 7000, TRUE];
-    } else {
-        $_SESSION['message'] = ['danger', lang('password_recovery_message_failed'), 7000, TRUE];
-    }
-}
-
 
 if ($VALID->inGET('logout')) {
     unset($_SESSION['password_customer']);
