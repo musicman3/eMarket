214b9f90c88255f032374847ac429f3c315d53c2
musicman3 <micromail@mail.ru>
2019-08-27 15:12:17 +0300

    переключатель

--- controller/admin/pages/settings/modules/js/js.php
+++ controller/admin/pages/settings/modules/js/js.php
@@ -6,4 +6,13 @@
 
 // Подгружаем Ajax Добавить, Редактировать, Удалить
 $AJAX->action('?route=settings/modules');
+
 ?>
+<!-- Загрузка bootstrap-switch -->
+<link rel="stylesheet" href="/ext/bootstrap-switch/css/bootstrap-switch.min.css" type="text/css"/>
+<script type="text/javascript" src="/ext/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+
+<!-- Инициализация bootstrap-switch -->
+<script type="text/javascript">
+    $("[name='switch']").bootstrapSwitch();
+</script>
