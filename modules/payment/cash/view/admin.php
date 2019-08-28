4de5744dbec198b98f1b5f5949334153a6fecc54
musicman3 <micromail@mail.ru>
2019-08-28 15:57:13 +0300

    лишнее

--- modules/payment/cash/view/admin.php
+++ modules/payment/cash/view/admin.php
@@ -6,11 +6,9 @@
 
 ?>
 
-<div class="panel-body">
-    <div class="form-group">
-        <div class="input-group has-error">
-            <span class="input-group-addon"><span class="glyphicon glyphicon-sort-by-order"></span></span>
-            <input class="input-sm form-control" placeholder="<?php echo lang('value') ?>" type="text" pattern="\d+(\.\d{0,7})?" name="value_length" id="value_length" required />
-        </div>
+<div class="form-group">
+    <div class="input-group has-error">
+        <span class="input-group-addon"><span class="glyphicon glyphicon-sort-by-order"></span></span>
+        <input class="input-sm form-control" placeholder="<?php echo lang('value') ?>" type="text" pattern="\d+(\.\d{0,7})?" name="value_length" id="value_length" required />
     </div>
 </div>
