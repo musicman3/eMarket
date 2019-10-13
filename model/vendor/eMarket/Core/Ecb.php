eb5e8249a59f6635dd5133b956130540230352d7
musicman3 <micromail@mail.ru>
2019-10-13 21:15:58 +0300

    готовим звенья

--- model/vendor/eMarket/Core/Ecb.php
+++ model/vendor/eMarket/Core/Ecb.php
@@ -1,4 +1,5 @@
 <?php
+
 /* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
   |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
@@ -21,8 +22,61 @@ final class Ecb {
      * @return array array()
      */
     public function init() {
-        echo ('test');
+        //echo ('test');
+    }
 
+    /**
+     * Блок формирования входящей стоимости
+     * @param 1
+     * @return array array()
+     */
+    public function inputPrice() {
+        //echo ('test');
+    }
+
+    /**
+     * Блок формирования скидки на товар
+     * @param 1
+     * @return array array()
+     */
+    public function productSaleBlock() {
+        //echo ('test');
+    }
+
+    /**
+     * Блок формирования итоговой скидки на заказ
+     * @param 1
+     * @return array array()
+     */
+    public function totalSaleBlock() {
+        //echo ('test');
+    }
+
+    /**
+     * Блок формирования стоимости доставки
+     * @param 1
+     * @return array array()
+     */
+    public function shippingBlock() {
+        //echo ('test');
+    }
+
+    /**
+     * Блок формирования оплаты
+     * @param 1
+     * @return array array()
+     */
+    public function checkoutBlock() {
+        //echo ('test');
+    }
+
+    /**
+     * Блок формирования исходящей стоимости
+     * @param 1
+     * @return array array()
+     */
+    public function outputPrice() {
+        //echo ('test');
     }
 
 }
