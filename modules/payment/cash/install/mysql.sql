5388ab285cb3083920b206aab094a74505a8e0ce
musicman3 <micromail@mail.ru>
2019-10-16 11:48:32 +0300

    еще

--- modules/payment/cash/install/mysql.sql
+++ modules/payment/cash/install/mysql.sql
@@ -6,7 +6,7 @@
 DROP TABLE IF EXISTS emkt_modules_payment_cash;
 CREATE TABLE emkt_modules_payment_cash (
 	id int NOT NULL auto_increment,
-        sort int(5),
+        sort varchar(5),
 	zones_id int(64),
 	PRIMARY KEY (id))
 ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
