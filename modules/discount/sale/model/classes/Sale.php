c31b1cb0b2cdf683e8266bdf2d90c9cfaf82115c
musicman3 <micromail@mail.ru>
2019-12-02 01:48:50 +0200

    немного доработал

--- modules/discount/sale/model/classes/Sale.php
+++ modules/discount/sale/model/classes/Sale.php
@@ -32,12 +32,23 @@ class Sale {
     public static function interface($input, $CURRENCIES) {
 
         if ($input[2] != '' && $input[2] != NULL) {
-            $discount = \eMarket\Pdo::getCell("SELECT sale_value FROM " . DB_PREFIX . 'modules_discount_sale' . " WHERE language=? AND id=?", [lang('#lang_all')[0], $input[2]]);
-            $price = $input[1] / 100 * (100 - $discount);
-            $this_time = time();
-            $date_start = \eMarket\Pdo::getCell("SELECT UNIX_TIMESTAMP (date_start) FROM " . DB_PREFIX . 'modules_discount_sale' . " WHERE id=?", [$input[2]]);
-            $date_end = \eMarket\Pdo::getCell("SELECT UNIX_TIMESTAMP (date_end) FROM " . DB_PREFIX . 'modules_discount_sale' . " WHERE id=?", [$input[2]]);
-            
+
+            $explode_id = explode(',', $input[2]);
+            $discount_out = 0;
+            foreach ($explode_id as $val) {
+                $discount = \eMarket\Pdo::getCell("SELECT sale_value FROM " . DB_PREFIX . 'modules_discount_sale' . " WHERE language=? AND id=?", [lang('#lang_all')[0], $val]);
+                
+                $this_time = time();
+                $date_start = \eMarket\Pdo::getCell("SELECT UNIX_TIMESTAMP (date_start) FROM " . DB_PREFIX . 'modules_discount_sale' . " WHERE id=?", [$val]);
+                $date_end = \eMarket\Pdo::getCell("SELECT UNIX_TIMESTAMP (date_end) FROM " . DB_PREFIX . 'modules_discount_sale' . " WHERE id=?", [$val]);
+                
+                if ($this_time > $date_start && $this_time < $date_end) {
+                    $discount_out = $discount_out + $discount;
+                }
+                
+                $price = $input[1] / 100 * (100 - $discount_out);
+            }
+
             if ($this_time > $date_start && $this_time < $date_end) {
                 return '<del>' . \eMarket\Products::productPrice($input[1], $CURRENCIES, 1) . '</del><br><span class="label label-danger">' . \eMarket\Products::productPrice($price, $CURRENCIES, 1) . '</span>';
             }
