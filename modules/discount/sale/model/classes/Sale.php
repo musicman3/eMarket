a8d4dd7ab3ea4c38732dcd897198b099ef755dd2
musicman3 <micromail@mail.ru>
2019-12-01 16:14:22 +0200

    структура для классов модулей

---
+++ modules/discount/sale/model/classes/Sale.php
@@ -0,0 +1,37 @@
+<?php
+/* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
+  |  https://github.com/musicman3/eMarket  |
+  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+?>
+<?php
+
+/* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
+  |  https://github.com/musicman3/eMarket  |
+  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
+namespace eMarket\Modules\Discount;
+
+/**
+ * Класс для валидации данных
+ *
+ * @package Sale
+ * @author eMarket
+ * 
+ */
+class Sale {
+
+    /**
+     * Выходные данные для внутреннего интерфейса калькулятора
+     *
+     * @param array|string $name (массив или строка $name)
+     * @return array|string $name (массив или строка $name)
+     */
+    public static function interface() {
+        return 'abc';
+    }
+
+}
+
+?>
