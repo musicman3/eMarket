651f500bd3e1188289265d175a361ffeb7984ec4
musicman3 <micromail@mail.ru>
2020-01-01 23:09:12 +0200

    готовим модуль Бесплатная доставка

---
+++ modules/shipping/free/install/mysql.sql
@@ -0,0 +1,12 @@
+/* =-=-=-= Copyright © 2018 eMarket =-=-=-= 
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
+  |  https://github.com/musicman3/eMarket  |
+  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
+DROP TABLE IF EXISTS emkt_modules_shipping_free;
+CREATE TABLE emkt_modules_shipping_free (
+	id int NOT NULL auto_increment,
+        minimum_price decimal(4,2),
+        shipping_zone int(1),
+	PRIMARY KEY (id))
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
