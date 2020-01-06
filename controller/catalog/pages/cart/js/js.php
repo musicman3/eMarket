64e1b384d8d2ed37d4e6492dfc077a0b7a5215d3
musicman3 <micromail@mail.ru>
2020-01-07 01:46:01 +0200

    немного упростил

--- controller/catalog/pages/cart/js/js.php
+++ controller/catalog/pages/cart/js/js.php
@@ -30,21 +30,21 @@
                     $('#shipping_price').html('<?php echo lang('cart_shipping_price') ?> <b><?php echo \eMarket\Products::productPrice(0, 1) ?></b>');
                     $('#total_price_modal').html('<h5><?php echo lang('cart_total_to_pay') ?> <?php echo \eMarket\Products::productPrice(\eMarket\Ecb::totalPriceCartWithSale(), 1) ?></h5>');
                 } else {
-                    for (x = 0; x < shipping_method.length; x++) {
+                    for (var shipping_val of shipping_method) {
                         //Если минимальная стоимость заказа ниже указанной
-                        if (shipping_method[x]['chanel_total_price'] < shipping_method[x]['chanel_minimum_price']) {
-                            $("#shipping_method").append($('<option value="no">' + shipping_method[x]['chanel_name'] + '<?php echo lang('cart_shipping_is_not_available_and_min_price') ?> ' + shipping_method[x]['chanel_minimum_price_format'] + '</option>'));
+                        if (shipping_val['chanel_total_price'] < shipping_val['chanel_minimum_price']) {
+                            $("#shipping_method").append($('<option value="no">' + shipping_val['chanel_name'] + '<?php echo lang('cart_shipping_is_not_available_and_min_price') ?> ' + shipping_val['chanel_minimum_price_format'] + '</option>'));
                             $('#shipping_method_class').removeClass('has-success');
                             $('#shipping_method_class').addClass('has-error');
-                            $('#shipping_price').html('<?php echo lang('cart_shipping_price') ?> <b>' + shipping_method[x]['chanel_shipping_price_format'] + '</b>');
-                            $('#total_price_modal').html('<h5><?php echo lang('cart_total_to_pay') ?> ' + shipping_method[x]['chanel_total_price_with_shipping_format'] + '</h5>');
+                            $('#shipping_price').html('<?php echo lang('cart_shipping_price') ?> <b>' + shipping_val['chanel_shipping_price_format'] + '</b>');
+                            $('#total_price_modal').html('<h5><?php echo lang('cart_total_to_pay') ?> ' + shipping_val['chanel_total_price_with_shipping_format'] + '</h5>');
                         } else {
                             // Если есть доставка
-                            $("#shipping_method").append($('<option value="' + shipping_method[x]['chanel_module'] + '">' + shipping_method[x]['chanel_name'] + '</option>'));
+                            $("#shipping_method").append($('<option value="' + shipping_val['chanel_module'] + '">' + shipping_val['chanel_name'] + '</option>'));
                             $('#shipping_method_class').removeClass('has-error');
                             $('#shipping_method_class').addClass('has-success');
-                            $('#shipping_price').html('<?php echo lang('cart_shipping_price') ?> <b>' + shipping_method[x]['chanel_shipping_price_format'] + '</b>');
-                            $('#total_price_modal').html('<h5><?php echo lang('cart_total_to_pay') ?> ' + shipping_method[x]['chanel_total_price_with_shipping_format'] + '</h5>');
+                            $('#shipping_price').html('<?php echo lang('cart_shipping_price') ?> <b>' + shipping_val['chanel_shipping_price_format'] + '</b>');
+                            $('#total_price_modal').html('<h5><?php echo lang('cart_total_to_pay') ?> ' + shipping_val['chanel_total_price_with_shipping_format'] + '</h5>');
                         }
                     }
                 }
