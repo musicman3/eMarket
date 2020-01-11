d340c60aa8c3eab0eaf8c1ea9c240f6d383be180
musicman3 <micromail@mail.ru>
2020-01-11 13:52:37 +0200

    еще по модулю

--- modules/payment/cash/controller/admin/index.php
+++ modules/payment/cash/controller/admin/index.php
@@ -7,20 +7,21 @@
 
 $MODULE_DB = \eMarket\Set::moduleDatabase();
 
+$shipping_method = \eMarket\Pdo::getColAssoc("SELECT * FROM " . TABLE_MODULES . " WHERE type=? AND install=? AND active=? ORDER BY name ASC", ['shipping', 1, 1]);
+$order_status = \eMarket\Pdo::getColAssoc("SELECT * FROM " . TABLE_ORDER_STATUS . " WHERE language=? ORDER BY sort DESC", [lang('#lang_all')[0]]);
+
 // Если нажали на кнопку Сохранить
 if (\eMarket\Valid::inPOST('save')) {
 
     $data = \eMarket\Pdo::getCellFalse("SELECT * FROM " . $MODULE_DB, []);
     if ($data == FALSE) {
         if (empty(\eMarket\Valid::inPOST('multiselect')) == FALSE) {
-            $multiselect = \eMarket\Valid::inPOST('multiselect');
-            $multiselect_implode = implode(',', $multiselect);
-            \eMarket\Pdo::inPrepare("INSERT INTO " . $MODULE_DB . " SET order_status=?, shipping_module=?", [\eMarket\Valid::inPOST('order_status'), $multiselect_implode]);
+            $multiselect = json_encode(\eMarket\Valid::inPOST('multiselect'));
+            \eMarket\Pdo::inPrepare("INSERT INTO " . $MODULE_DB . " SET order_status=?, shipping_module=?", [\eMarket\Valid::inPOST('order_status'), $multiselect]);
         }
     } elseif (empty(\eMarket\Valid::inPOST('multiselect')) == FALSE) {
-        $multiselect = \eMarket\Valid::inPOST('multiselect');
-        $multiselect_implode = implode(',', $multiselect);
-        \eMarket\Pdo::inPrepare("UPDATE " . $MODULE_DB . " SET order_status=?, shipping_module=?", [\eMarket\Valid::inPOST('order_status'), $multiselect_implode]);
+        $multiselect = json_encode(\eMarket\Valid::inPOST('multiselect'));
+        \eMarket\Pdo::inPrepare("UPDATE " . $MODULE_DB . " SET order_status=?, shipping_module=?", [\eMarket\Valid::inPOST('order_status'), $multiselect]);
     }
 
     // Выводим сообщение об успехе
--- modules/payment/cash/view/admin/index.php
+++ modules/payment/cash/view/admin/index.php
@@ -7,29 +7,38 @@
 
 <form id="form_save_mod" name="form_save_mod" action="javascript:void(null);" onsubmit="callSaveMod('<?php echo \eMarket\Valid::inSERVER('REQUEST_URI') ?>')">
 
-        <input type="hidden" name="save" value="ok" />
+    <input type="hidden" name="save" value="ok" />
 
-            <div class="form-group">
-                <label for="shipping_method">Модуль доставки</label>
-                <div class="input-group">
-                    <select id="shipping_method" name="multiselect[]" multiple="multiple">
-                        <option value="free">Бесплатная доставка</option>
-                        <option value="free_2">Самовывоз</option>
-                    </select>
-                </div>
-                <small id="shipping_method_action" class="form-text text-muted">Выберите модуль доставки</small>
-            </div>
-            <div class="form-group">
-                <label for="order_status">Статус заказа</label>
-                <div class="input-group has-success">
-                    <span class="input-group-addon"><span class="glyphicon glyphicon-pencil"></span></span>
-                    <select name="order_status" id="order_status" class="input-sm form-control">
-                        <option value="1">Ожидает оплаты</option>
-                        <option value="2">Оплачено</option>
-                    </select>
-                </div>
-                <small id="order_status_action" class="form-text text-muted">Выберите статус заказа</small>
-            </div>
+    <div class="form-group">
+        <label for="shipping_method">Модуль доставки</label>
+        <div class="input-group">
+            <select id="shipping_method" name="multiselect[]" multiple="multiple">
+                <?php foreach ($shipping_method as $val) { ?>
+                    <option value="<?php echo $val['name'] ?>"><?php echo lang('modules_shipping_' . $val['name'] . '_name') ?></option>
+                <?php } ?>
+            </select>
+        </div>
+        <small id="shipping_method_action" class="form-text text-muted">Выберите модуль доставки</small>
+    </div>
+    <div class="form-group">
+        <label for="order_status">Статус заказа</label>
+        <div class="input-group has-success">
+            <span class="input-group-addon"><span class="glyphicon glyphicon-pencil"></span></span>
+            <select name="order_status" id="order_status" class="input-sm form-control">
+                <?php
+                foreach ($order_status as $val) {
+                    if ($val['default_order_status'] == 1) {
+                        $selected = 'selected ';
+                    } else {
+                        $selected = '';
+                    }
+                    ?>
+                    <option <?php echo $selected ?>value="<?php echo $val['id'] ?>"><?php echo $val['name'] ?></option>
+                <?php } ?>
+            </select>
+        </div>
+        <small id="order_status_action" class="form-text text-muted">Выберите статус заказа</small>
+    </div>
 
     <div class="text-right">
         <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-disk"></span> <?php echo lang('save') ?></button>
