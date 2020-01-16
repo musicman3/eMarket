8aab323eb84010811860e3946beb147b840baba1
musicman3 <micromail@mail.ru>
2020-01-17 01:03:07 +0200

    правка

--- view/default/js/breadcrumb.php
+++ view/default/js/breadcrumb.php
@@ -45,17 +45,7 @@
         $(document).ready(function () {
             breadcrumb();
         });
-<?php } elseif (\eMarket\Valid::inGET('route') == '') { ?>
-        function breadcrumb() {
-            var breadcrumbid = $('div#data_breadcrumb').data('breadcrumbid');
-            var breadcrumbparentid = $('div#data_breadcrumb').data('breadcrumbparentid');
-            var breadcrumbname = $('div#data_breadcrumb').data('breadcrumbname');
-        }
-
-        $(document).ready(function () {
-            breadcrumb();
-        });
-<?php } else { ?>
+<?php } elseif (\eMarket\Valid::inGET('route') != '') { ?>
         function breadcrumb() {
             var breadcrumbid = $('div#data_breadcrumb').data('breadcrumbid');
             var breadcrumbparentid = $('div#data_breadcrumb').data('breadcrumbparentid');
