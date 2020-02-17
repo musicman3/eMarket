629ecc3f03acbe1e03745e8a10d440a05f8e215f
musicman3 <micromail@mail.ru>
2020-02-17 14:14:21 +0200

    немного структуры

--- controller/catalog/pages/result/index.php
+++ controller/catalog/pages/result/index.php
@@ -5,5 +5,14 @@
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 
+// Если добавлен новый заказ
+if (\eMarket\Valid::inPOST('add')) {
+    
+}
+
+// Если получен ответ от процессингового центра на изменение статуса
+if (\eMarket\Valid::inPOST('callback')) {
+    
+}
 
 ?>
