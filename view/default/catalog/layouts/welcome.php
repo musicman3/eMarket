f8cf7a24aca69b29a2a0fbd4641016f07e7925b9
Prizraki <lw12@yandex.ru>
2020-06-09 17:52:56 +0300

    Перенос в языковые

--- view/default/catalog/layouts/welcome.php
+++ view/default/catalog/layouts/welcome.php
@@ -9,7 +9,7 @@
 <div class="contentText">
     <?php if ($CUSTOMER == FALSE) { ?>
         <h4><?php echo sprintf(lang('welcome_name'), lang('welcome_guest')) ?></h4>
-        <p>The default shopping cart comes with Jquery UI, Grid960, Fancybox and BxGallery, in this demo those have been replaced by Boostrap and Bootstrap 3 Lightbox making it lighter, faster and responsive.</p>
+        <p><?php echo sprintf(lang('welcome_text')) ?></p>
     <?php } else { ?>
         <h4><?php echo sprintf(lang('welcome_name'), $CUSTOMER['firstname']) ?></h4>
         <p> </p>
