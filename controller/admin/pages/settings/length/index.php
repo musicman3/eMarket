1014ca6ea3661f6ba66fd3b6c362ce0a0978b8fb
musicman3 <micromail@mail.ru>
2020-06-10 15:00:31 +0300

    1 модал на Длину

--- controller/admin/pages/settings/length/index.php
+++ controller/admin/pages/settings/length/index.php
@@ -50,7 +50,7 @@ if (\eMarket\Valid::inPOST('add')) {
 if (\eMarket\Valid::inPOST('edit')) {
 
     // Если есть установка по-умолчанию
-    if (\eMarket\Valid::inPOST('default_length_edit')) {
+    if (\eMarket\Valid::inPOST('default_length')) {
         $default_length = 1;
     } else {
         $default_length = 0;
@@ -64,18 +64,18 @@ if (\eMarket\Valid::inPOST('edit')) {
         $value_length_all = \eMarket\Pdo::getColAssoc("SELECT id, value_length, language FROM " . TABLE_LENGTH, []);
         $count_value_length_all = count($value_length_all);
         for ($x = 0; $x < $count_value_length_all; $x++) {
-            \eMarket\Pdo::inPrepare("UPDATE " . TABLE_LENGTH . " SET value_length=? WHERE id=? AND language=?", [($value_length_all[$x]['value_length'] / \eMarket\Valid::inPOST('value_length_edit')), $value_length_all[$x]['id'], $value_length_all[$x]['language']]);
+            \eMarket\Pdo::inPrepare("UPDATE " . TABLE_LENGTH . " SET value_length=? WHERE id=? AND language=?", [($value_length_all[$x]['value_length'] / \eMarket\Valid::inPOST('value_length')), $value_length_all[$x]['id'], $value_length_all[$x]['language']]);
         }
 
         for ($x = 0; $x < $LANG_COUNT; $x++) {
             // обновляем запись
-            \eMarket\Pdo::inPrepare("UPDATE " . TABLE_LENGTH . " SET name=?, code=?, value_length=?, default_length=? WHERE id=? AND language=?", [\eMarket\Valid::inPOST('name_length_edit_' . $x), \eMarket\Valid::inPOST('code_length_edit_' . $x), 1, $default_length, \eMarket\Valid::inPOST('edit'), lang('#lang_all')[$x]]);
+            \eMarket\Pdo::inPrepare("UPDATE " . TABLE_LENGTH . " SET name=?, code=?, value_length=?, default_length=? WHERE id=? AND language=?", [\eMarket\Valid::inPOST('name_length_' . $x), \eMarket\Valid::inPOST('code_length_' . $x), 1, $default_length, \eMarket\Valid::inPOST('edit'), lang('#lang_all')[$x]]);
         }
     } else {
 
         for ($x = 0; $x < $LANG_COUNT; $x++) {
             // обновляем запись
-            \eMarket\Pdo::inPrepare("UPDATE " . TABLE_LENGTH . " SET name=?, code=?, value_length=?, default_length=? WHERE id=? AND language=?", [\eMarket\Valid::inPOST('name_length_edit_' . $x), \eMarket\Valid::inPOST('code_length_edit_' . $x), \eMarket\Valid::inPOST('value_length_edit'), $default_length, \eMarket\Valid::inPOST('edit'), lang('#lang_all')[$x]]);
+            \eMarket\Pdo::inPrepare("UPDATE " . TABLE_LENGTH . " SET name=?, code=?, value_length=?, default_length=? WHERE id=? AND language=?", [\eMarket\Valid::inPOST('name_length_' . $x), \eMarket\Valid::inPOST('code_length_' . $x), \eMarket\Valid::inPOST('value_length'), $default_length, \eMarket\Valid::inPOST('edit'), lang('#lang_all')[$x]]);
         }
     }
     // Выводим сообщение об успехе
@@ -99,6 +99,9 @@ $lines_on_page = \eMarket\Set::linesOnPage();
 $navigate = \eMarket\Navigation::getLink(count($lines), $lines_on_page);
 $start = $navigate[0];
 $finish = $navigate[1];
+
+// Модальное окно
+require_once('modal/index.php');
 
 //Создаем маркер для подгрузки JS/JS.PHP в конце перед </body>
 $JS_END = __DIR__;
--- controller/admin/pages/settings/length/js/js.php
+++ controller/admin/pages/settings/length/js/js.php
@@ -12,30 +12,42 @@
 <?php if (isset($name_edit)) { ?>
     <!-- Загрузка данных в модальное окно -->
     <script type="text/javascript">
-        $('#edit').on('show.bs.modal', function (event) {
-            $('#default_length_edit').bootstrapSwitch('destroy', true);
+        $('#index').on('show.bs.modal', function (event) {
+            $('#default_length').bootstrapSwitch('destroy', true);
             var button = $(event.relatedTarget);
             var modal_id = button.data('edit'); // Получаем ID из data-edit при клике на кнопку редактирования
-            // Получаем массивы данных
-            var name_edit = $('div#ajax_data').data('name');
-            var code_edit = $('div#ajax_data').data('code');
-            var value_length_edit = $('div#ajax_data').data('value');
-            var status = $('div#ajax_data').data('status');
+            if (Number.isInteger(modal_id)) {
+                // Получаем массивы данных
+                var name_edit = $('div#ajax_data').data('name');
+                var code_edit = $('div#ajax_data').data('code');
+                var value_length_edit = $('div#ajax_data').data('value');
+                var status = $('div#ajax_data').data('status');
 
-            // Ищем id и добавляем данные
-            for (x = 0; x < name_edit.length; x++) {
-                $('#name_length_edit_' + x).val(name_edit[x][modal_id]);
-                $('#code_length_edit_' + x).val(code_edit[x][modal_id]);
+                $('#edit').val(modal_id);
+                $('#add').val('');
+
+                // Ищем id и добавляем данные
+                for (x = 0; x < name_edit.length; x++) {
+                    $('#name_length_' + x).val(name_edit[x][modal_id]);
+                    $('#code_length_' + x).val(code_edit[x][modal_id]);
+                }
+
+                $('#value_length').val(value_length_edit[modal_id]);
+                // Меняем значение чекбокса
+                $('#default_length').prop('checked', status[modal_id]);
+                $('#default_length').bootstrapSwitch();
+            } else {
+                $('#edit').val('');
+                $('#add').val('ok');
+                //Очищаем поля
+                $('.input-sm').val('');
+                // Меняем значение чекбокса
+                $('#default_length').prop('checked', '1');
+                $('#default_length').bootstrapSwitch();
             }
-
-            $('#value_length_edit').val(value_length_edit[modal_id]);
-            $('#js_edit').val(modal_id);
-            // Меняем значение чекбокса
-            $('#default_length_edit').prop('checked', status[modal_id]);
-            $('#default_length_edit').bootstrapSwitch();
         });
     </script>
-<?php
+    <?php
 }
 // Подгружаем Ajax Добавить, Редактировать, Удалить
 \eMarket\Ajax::action('');
