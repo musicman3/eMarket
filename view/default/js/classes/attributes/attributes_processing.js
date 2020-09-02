b15b51dd08e894e09f3ff1fbc2cec0d36b57b7bb
musicman3 <micromail@mail.ru>
2020-09-02 19:39:58 +0300

    переименовал

--- view/default/js/classes/attributes/attributes_processing.js
+++ view/default/js/classes/attributes/attributes_processing.js
@@ -24,17 +24,17 @@ class AttributesProcessing {
                 );
 
         if (attributes !== undefined && attributes !== null) {
-            for (var y = 0; y < attributes.length; y++) {
-                if (attributes[y][0]['data'] !== undefined && attributes[y][0]['data'] !== null) {
+            for (var x = 0; x < attributes.length; x++) {
+                if (attributes[x][0]['data'] !== undefined && attributes[x][0]['data'] !== null) {
                     $('#table_' + group_number).prepend(
-                            '<tr><td><span class="product-attribute-specification">' + attributes[y][0]['value'] + '</span></td>' +
-                            '<td><span class="product-attribute-specification pull-right"><select id="selectattr_' + group_number + '_' + y + '"></select></span></td></tr>'
+                            '<tr><td><span class="product-attribute-specification">' + attributes[x][0]['value'] + '</span></td>' +
+                            '<td><span class="product-attribute-specification pull-right"><select id="selectattr_' + group_number + '_' + x + '"></select></span></td></tr>'
                             );
 
-                    $('#selectattr_' + group_number + '_' + y).empty();
-                    attributes[y][0]['data'].reverse();
-                    $.each(attributes[y][0]['data'], function (i, p) {
-                        $('#selectattr_' + group_number + '_' + y).append($('<option></option>').val(group_number + '_' + y + '_' + i).html(p));
+                    $('#selectattr_' + group_number + '_' + x).empty();
+                    attributes[x][0]['data'].reverse();
+                    $.each(attributes[x][0]['data'], function (i, p) {
+                        $('#selectattr_' + group_number + '_' + x).append($('<option></option>').val(group_number + '_' + x + '_' + i).html(p));
                     });
                 }
             }
