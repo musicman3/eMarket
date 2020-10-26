e03531f6d846052d7a44b7656b5086adde00d7ef
Prizraki <lw12@yandex.ru>
2020-10-26 09:17:43 +0300

    Еще

--- controller/catalog/layouts/categories_listing.php
+++ controller/catalog/layouts/categories_listing.php
@@ -4,6 +4,6 @@
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 
-$categories = \eMarket\Pdo::getColRow("SELECT id, name, logo_general FROM " . TABLE_CATEGORIES . " WHERE language=? AND parent_id=? AND status=1 ORDER BY sort_category DESC", [lang('#lang_all')[0], \eMarket\Valid::inGET('category_id')]);
+$categories = \eMarket\Pdo::getColRow("SELECT id, name, logo_general, status FROM " . TABLE_CATEGORIES . " WHERE language=? AND parent_id=? AND status=1 ORDER BY sort_category DESC", [lang('#lang_all')[0], \eMarket\Valid::inGET('category_id')]);
 
 ?>
