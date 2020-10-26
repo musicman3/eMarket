e712d5834e22fba39621527d66f3e9c75b0ac6be
Prizraki <lw12@yandex.ru>
2020-10-26 16:50:36 +0300

    Правка

--- controller/catalog/pages/listing/index.php
+++ controller/catalog/pages/listing/index.php
@@ -9,7 +9,7 @@
 if (\eMarket\Valid::inGET('search')) {
     $products = \eMarket\Pdo::getColAssoc("SELECT * FROM " . TABLE_PRODUCTS . " WHERE name LIKE? AND language=? AND status=? ORDER BY id DESC", [\eMarket\Valid::inGET('search') . '%', lang('#lang_all')[0], 1]);
 } else {
-    $products = \eMarket\Pdo::getColAssoc("SELECT * FROM " . TABLE_PRODUCTS . " WHERE language=? AND parent_id=? AND status=1 ORDER BY id DESC", [lang('#lang_all')[0], \eMarket\Valid::inGET('category_id')]);
+    $products = \eMarket\Pdo::getColAssoc("SELECT * FROM " . TABLE_PRODUCTS . " WHERE language=? AND parent_id=? AND status=? ORDER BY id DESC", [lang('#lang_all')[0], \eMarket\Valid::inGET('category_id'), 1]);
 }
 
 $categories_name = \eMarket\Pdo::getCell("SELECT name FROM " . TABLE_CATEGORIES . " WHERE language=? AND id=?", [lang('#lang_all')[0], \eMarket\Valid::inGET('category_id')]);
