05797bbbb9342e457ec1e0d81aeadc457399f2fd
Prizraki <lw12@yandex.ru>
2020-10-28 09:52:17 +0300

    Правка

--- view/default/catalog/constructor.php
+++ view/default/catalog/constructor.php
@@ -8,10 +8,10 @@
 <!DOCTYPE html>
 <html dir="ltr" lang="<?php echo lang('meta-language') ?>">
     <head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=Edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="robots" content="noindex,nofollow" />
+        <meta name="robots" content="index,follow" />
         <meta name="generator" content="HippoEDIT, Netbeans, Notepad++" />
         <meta name="classification" content="software" />
         <meta name="author" content="eMarket" />
