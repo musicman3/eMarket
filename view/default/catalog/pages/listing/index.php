a2b76cf760b35376c3867727eedc1713f25530cc
musicman3 <micromail@mail.ru>
2020-11-12 14:56:38 +0200

    лишнее

--- view/default/catalog/pages/listing/index.php
+++ view/default/catalog/pages/listing/index.php
@@ -17,8 +17,7 @@ foreach (\eMarket\View::layoutRouting('content') as $path) {
         <div class="well well-sm">
             <div class="btn-group">
                 <button type="button" class="btn btn-primary"><span class="glyphicon glyphicon-sort"></span> &nbsp;<?php echo $sort_name ?></button>
-                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="caret"></span>
-    <span class="sr-only">Toggle Dropdown</span></button>
+                <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><span class="caret"></span></button>
                 <ul class="dropdown-menu text-right">
                     <li><a id="default" class="sorting">По умолчанию</a></li>
                     <li><a id="name" class="sorting">По названию</a></li>
@@ -28,7 +27,7 @@ foreach (\eMarket\View::layoutRouting('content') as $path) {
 
                 &nbsp;&nbsp;<input class="check-box" hidden type="checkbox" data-off-color="danger" data-size="normal" data-label-text="Отобразить" data-label-width='auto' data-on-text="Все" data-off-text="В наличии" data-handle-width="80" name="show_in_stock" id="show_in_stock"<?php echo $checked_stock ?>>
             </div>
-            
+
             <div class="btn-group pull-right">
                 <a id="grid" class="btn btn-default item-grid active"><span class="glyphicon glyphicon-th"></span></a>
                 <a id="list" class="btn btn-default item-list"><span class="glyphicon glyphicon-th-list"></span></a>
