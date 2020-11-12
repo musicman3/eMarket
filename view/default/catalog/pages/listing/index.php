e4116b424914589726596c02da900cedc8ea6401
Prizraki <lw12@yandex.ru>
2020-11-12 12:14:28 +0300

    ошибочка

--- view/default/catalog/pages/listing/index.php
+++ view/default/catalog/pages/listing/index.php
@@ -74,7 +74,7 @@ foreach (\eMarket\View::layoutRouting('content') as $path) {
 
             <div class="btn-group  pull-right" role="group">
                 <?php if ($navigate[0] > 0) { ?> <button id="prev" type="button" class="btn btn-default navigation"><span aria-hidden="true">&larr;</span> Назад</button> <?php } else { ?> <a id="prev" class="btn btn-default disabled" role="button"><span aria-hidden="true">&larr;</span> Назад</a> <?php } ?>
-                <?php if ($navigate[1] != $count_lines) { ?> <button id="next" type="button" class="btn btn-default navigation">Вперед <span aria-hidden="true">&rarr;</span></button> <?php } else { ?> <a id="next" class="btn btn-default disabled" role="button"><span aria-hidden="true">&larr;</span>Вперед </a> <?php } ?>
+                <?php if ($navigate[1] != $count_lines) { ?> <button id="next" type="button" class="btn btn-default navigation">Вперед <span aria-hidden="true">&rarr;</span></button> <?php } else { ?> <a id="next" class="btn btn-default disabled" role="button">Вперед <span aria-hidden="true">&rarr;</span></a> <?php } ?>
             </div>
         </div>
     </div>
