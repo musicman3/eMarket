8fdc0184825981828cda6bfa3c93b68ee6c19cd4
Prizraki <lw12@yandex.ru>
2020-11-13 12:00:44 +0300

    Поправочка

--- view/default/admin/footer.php
+++ view/default/admin/footer.php
@@ -10,17 +10,13 @@
 
     <div id="footerwrap">
         <footer class="clearfix"></footer>
-
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
-                <div class="col-md-12">
-                    <p><img src="/view/<?php echo \eMarket\Set::template() ?>/admin/images/emarket.png" width="57" alt="" class="img-responsive center-block"></p>
-
-                    <p>Copyright © 2018-<?php echo date('Y') ?>, <a target=_blank href="https://github.com/musicman3/eMarket">eMarket Team</a>. All rights reserved.</p>
-                </div>
-            </div><!-- /row -->
-        </div><!-- /container -->
-    </div><!-- /footerwrap -->
+                <p><img src="/view/<?php echo \eMarket\Set::template() ?>/admin/images/emarket.png" width="57" alt="" class="img-responsive center-block"></p>
+                <p>Copyright © 2018-<?php echo date('Y') ?>, <a target=_blank href="https://github.com/musicman3/eMarket">eMarket Team</a>. All rights reserved.</p>
+            </div>
+        </div>
+    </div>
 
 <script type="text/javascript">
 $(document).ready(function() {
