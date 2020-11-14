9f6fe0c6be155b405fe992e3be631c60fe9b5bf6
Prizraki <lw12@yandex.ru>
2020-11-14 23:34:22 +0300

    Рыба модала

--- view/default/catalog/pages/listing/modal/message_cart.php
+++ view/default/catalog/pages/listing/modal/message_cart.php
@@ -7,12 +7,39 @@
 ?>
 <!-- Модальное окно "Добавить категорию" -->
 <div id="message_cart" class="modal fade" tabindex="-1">
-    <div class="modal-dialog modal-sm">
+    <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header"><div class="pull-right">&nbsp;&nbsp;<button class="close" type="button" data-dismiss="modal">×</button></div>
                 <h4 id="confirm_title" class="modal-title">eMarket</h4>
             </div>
-                <div id="confirm_body" class="panel-body"><?php echo lang('listing_product_added_to_cart') ?></div>
+	    <div class="modal-body">
+		<div class="row">
+		    <div class="col-md-5">
+			<div class="row">
+			    <div class="col-md-6">
+				<img src="">
+			    </div>
+			    <div class="col-md-6">
+				<h6>Hummingbird printed sweater</h6>
+				<p>€34.46</p>
+				<span>Size:<strong> S</strong></span><br>
+				<span>Quantity:&nbsp;<strong>1</strong></span>
+			    </div>
+			</div>
+		    </div>
+		    <div class="col-md-7">
+			<div class="cart-content">
+			    <p>There is 1 item in your cart.</p>
+			    <p><span>Subtotal:</span>&nbsp;<span>€34.46</span></p>
+			    <p><span>Shipping:</span>&nbsp;<span>Free </span></p>
+			    <p><span>Total&nbsp;(tax incl.)</span>&nbsp;<span>€34.46</span></p>
+			</div>
+		    </div>
+		</div>
+	    </div>
+            <div class="modal-footer">
+		<button type="submit" class="btn btn-primary">Продолжить</button>
+	    </div>
         </div>
     </div>
 </div>
