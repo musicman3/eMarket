7a27c27bfdb6ef24b8a9ec39940ad1a209c4e419
musicman3 <micromail@mail.ru>
2020-11-15 18:43:01 +0200

    языковые

--- view/default/catalog/pages/listing/modal/cart_message.php
+++ view/default/catalog/pages/listing/modal/cart_message.php
@@ -10,20 +10,20 @@
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 id="confirm_title" class="modal-title">Product successfully added to your shopping cart</h4>
+                <h4 id="confirm_title" class="modal-title"><?php echo lang('listing_product_added_to_cart') ?></h4>
             </div>
 	    <div class="modal-body">
 		<div class="row">
 		    <div id="product_image" class="col-xs-5"></div>
 		    <div class="col-xs-7">
-			<p><strong>Товар:</strong>&nbsp;<span id="product_name"></span></p>
-			<p><strong>Цена:</strong>&nbsp;<span id="product_price_formated"></span></p>
-			<p><strong>Количество:</strong>&nbsp;<span id="product_quantity"></span></p>
+			<p><strong><?php echo lang('listing_product') ?></strong>&nbsp;<span id="product_name"></span></p>
+			<p><strong><?php echo lang('listing_price') ?></strong>&nbsp;<span id="product_price_formated"></span></p>
+			<p><strong><?php echo lang('listing_quantity') ?></strong>&nbsp;<span id="product_quantity"></span></p>
 		    </div>
 		</div>
 	    </div>
             <div class="modal-footer">
-		<button type="submit" class="btn btn-primary" data-dismiss="modal">Продолжить покупки</button> <button type="submit" class="btn btn-success" onClick='location.href="/?route=cart"'>Перейти к оформлению</button>
+		<button type="submit" class="btn btn-primary" data-dismiss="modal"><?php echo lang('button_continue_shopping') ?></button> <button type="submit" class="btn btn-success" onClick='location.href="/?route=cart"'><?php echo lang('button_proceed_to_checkout') ?></button>
 	    </div>
         </div>
     </div>
