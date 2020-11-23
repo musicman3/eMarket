0099e79e0b5cff283cf3a5fbc2158d0e5dba2861
musicman3 <micromail@mail.ru>
2020-11-23 14:42:32 +0200

    в новые

--- view/default/catalog/layouts/new_products.php
+++ view/default/catalog/layouts/new_products.php
@@ -13,6 +13,7 @@ if ($products_new == true) {
                 ?>
                 <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item">
                     <div class="productHolder">
+                        <?php echo \eMarket\Ecb::saleStiker($value) ?>
                         <a href="/?route=products&category_id=<?php echo $value['parent_id'] ?>&id=<?php echo $value['id'] ?>"><img src="/uploads/images/products/resize_1/<?php echo $value['logo_general'] ?>" alt="<?php echo $value['name']; ?>" class="img-responsive center-block"></a>
                         <h5 class="text-center item-heading"><a href="/?route=products&category_id=<?php echo $value['parent_id'] ?>&id=<?php echo $value['id'] ?>"><?php echo $value['name'] ?></a></h5>
                         <div class="text-center item-price"><?php echo \eMarket\Ecb::priceInterface($value, 1) ?></div>
