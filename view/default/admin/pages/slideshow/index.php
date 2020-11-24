533ee06dd1dbc3a53ba80f24d9760e2eca2fb3fe
Prizraki <lw12@yandex.ru>
2020-11-24 21:37:37 +0300

    Еще

--- view/default/admin/pages/slideshow/index.php
+++ view/default/admin/pages/slideshow/index.php
@@ -46,7 +46,7 @@
                                             <div class="b-left"><button type="submit" class="btn btn-primary btn-xs" formmethod="get"><span class="glyphicon glyphicon-chevron-left"></span></button></div>
                                     </form>
 
-				    <div class="b-left"><button type="button" class="btn btn-primary btn-xs" data-toggle="modal" data-target="#index"><span class="glyphicon glyphicon-cog"></span></button></div>
+				    <div class="b-left"><a href="#settings" class="btn btn-primary btn-xs" data-toggle="modal"><span class="glyphicon glyphicon-cog"></span></a></div>
 
                                     <div class="b-left"><a href="#add" class="btn btn-primary btn-xs" data-toggle="modal"><span class="glyphicon glyphicon-plus"></span></a></div>
 
