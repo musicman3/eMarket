5d049a377d9f5e113df501dc1cb1fd1b45e4b4b5
Prizraki <lw12@yandex.ru>
2020-12-03 14:40:40 +0300

    Правка

--- view/default/catalog/layouts/navbar.php
+++ view/default/catalog/layouts/navbar.php
@@ -18,20 +18,20 @@
 
         <div class="collapse navbar-collapse" id="bs-navbar-collapse-core-nav">
             <ul class="nav navbar-nav navbar-left">
-                <li><a href="/"><i class="glyphicon glyphicon-home"></i><span class="hidden-sm"> <?php echo lang('breadcrumb_home') ?></span></a></li>
-                <!--<li><a href="#"><i class="glyphicon glyphicon-certificate"></i><span class="hidden-sm">  <?php echo lang('sale') ?></span></a></li>
-                <li><a href="#"><i class="glyphicon glyphicon-fire"></i><span class="hidden-sm"> <?php echo lang('recommended') ?></span></a></li>
-                <li><a href="#"><i class="glyphicon glyphicon-comment"></i><span class="hidden-sm"> <?php echo lang('articles') ?></span></a></li>-->
+                <li><a href="/"><span class="glyphicon glyphicon-home"></span><span class="hidden-sm"> <?php echo lang('breadcrumb_home') ?></span></a></li>
+                <!--<li><a href="#"><span class="glyphicon glyphicon-certificate"></span><span class="hidden-sm">  <?php echo lang('sale') ?></span></a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-fire"></span><span class="hidden-sm"> <?php echo lang('recommended') ?></span></a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-comment"></span><span class="hidden-sm"> <?php echo lang('articles') ?></span></a></li>-->
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li class="dropdown"><a data-toggle="dropdown" href="#"><i class="glyphicon glyphicon-globe"></i><span class="hidden-sm"> <?php echo lang('navbar_languages') ?></span> <span class="caret"></span></a>
+                <li class="dropdown"><a data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-globe"></span><span class="hidden-sm"> <?php echo lang('navbar_languages') ?></span> <span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <?php foreach (lang('#lang_all') as $value) { ?>
                             <li><a href="<?php echo $url_request . '&language=' . $value ?>"><img src="/view/<?php echo \eMarket\Set::template() ?>/admin/images/langflags/<?php echo $value ?>.png"> <?php echo lang('language_name', $value) ?></a></li>
                         <?php } ?>
                     </ul>
                 </li>
-                <li class="dropdown"><a data-toggle="dropdown" href="#"><i class="glyphicon glyphicon-credit-card"></i><span class="hidden-sm"> <?php echo lang('navbar_currencies') ?></span> <span class="caret"></span></a>
+                <li class="dropdown"><a data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-credit-card"></span><span class="hidden-sm"> <?php echo lang('navbar_currencies') ?></span> <span class="caret"></span></a>
                     <ul class="dropdown-menu">
                         <?php foreach (\eMarket\Set::currenciesData() as $value) { ?>
                             <li><a href="<?php echo $url_request . '&currency_default=' . $value[1] ?>"><?php echo $value[0] ?></a></li>
@@ -39,9 +39,9 @@
                     </ul>
                 </li>
                 <?php if ($CUSTOMER == FALSE) { ?>
-                    <li><a href="/?route=login"><i class="glyphicon glyphicon-user"></i><span class="hidden-sm"> <?php echo lang('login_to_account') ?></span></a></li>
+                    <li><a href="/?route=login"><span class="glyphicon glyphicon-user"></span><span class="hidden-sm"> <?php echo lang('login_to_account') ?></span></a></li>
                 <?php } else { ?>
-                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="glyphicon glyphicon-user"></i><span class="hidden-sm"> <?php echo lang('my_account') ?></span> <span class="caret"></span></a>
+                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-user"></span><span class="hidden-sm"> <?php echo lang('my_account') ?></span> <span class="caret"></span></a>
                         <ul class="dropdown-menu">
                             <li><a href="/?route=login&logout=ok"><?php echo lang('navbar_logout') ?></a></li>
                             <li class="divider"></li>
@@ -53,10 +53,10 @@
                 <?php } ?>
 
                 <?php if (\eMarket\Cart::totalQuantity() == 0) { ?>
-                    <li id="cart_bar" class="nav"><a href="#" class="disabled"><i class="glyphicon glyphicon-shopping-cart"></i> <?php echo \eMarket\Cart::totalQuantity() . ' ' . lang('navbar_pcs') ?></a></li>
+                    <li id="cart_bar" class="nav"><a href="#" class="disabled"><span class="glyphicon glyphicon-shopping-cart"></span> <?php echo \eMarket\Cart::totalQuantity() . ' ' . lang('navbar_pcs') ?></a></li>
                 <?php } else { ?>
                     <li id="cart_bar" class="dropdown">
-                        <a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="glyphicon glyphicon-shopping-cart"></i> <?php echo \eMarket\Cart::totalQuantity() . ' ' . lang('navbar_pcs') ?> <span class="caret"></span></a>
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#"><span class="glyphicon glyphicon-shopping-cart"></span> <?php echo \eMarket\Cart::totalQuantity() . ' ' . lang('navbar_pcs') ?> <span class="caret"></span></a>
                         <ul class="dropdown-menu">
                             <li><a href="#" class="disabled"><?php echo \eMarket\Ecb::totalPriceCartInterface(1) ?></a></li>
                             <li class="divider"></li>
