e7da3bfb5f4e92ed7f008107ce0c89174b170c7e
musicman3 <micromail@mail.ru>
2020-12-05 00:45:14 +0200

    выводим слайды (пока не все сделано)

--- controller/catalog/layouts/slide_show.php
+++ controller/catalog/layouts/slide_show.php
@@ -1,9 +1,54 @@
 <?php
+
 /* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
-  |    GNU GENERAL PUBLIC LICENSE v.3.0    |    
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 
-$slide_show = 0;
+$slideshow = \eMarket\Pdo::getColAssoc("SELECT * FROM " . TABLE_SLIDESHOW . " WHERE language=? ORDER BY id DESC", [lang('#lang_all')[0]]);
+$slideshow_pref = \eMarket\Pdo::getColAssoc("SELECT * FROM " . TABLE_SLIDESHOW_PREF . " WHERE id=?", [1])[0];
 
+$slide_interval = $slideshow_pref['show_interval'];
+
+if ($slideshow_pref['mouse_stop'] == 1) {
+    $slide_pause = 'hover';
+} else {
+    $slide_pause = 'false';
+}
+
+if ($slideshow_pref['autostart'] == 1) {
+    $autostart = 'carousel';
+} else {
+    $autostart = 'false';
+}
+
+if ($slideshow_pref['cicles'] == 1) {
+    $cicles = 'true';
+} else {
+    $cicles = 'false';
+}
+
+if ($slideshow_pref['indicators'] == 1) {
+    $indicators = 'true';
+} else {
+    $indicators = 'false';
+}
+
+if ($slideshow_pref['navigation'] == 1) {
+    $navigation_key = 'true';
+} else {
+    $navigation_key = 'false';
+}
+
+//\eMarket\Debug::trace($slideshow);
+$slideshow_array = [];
+foreach ($slideshow as $images_data) {
+    foreach (json_decode($images_data['logo'], 1) as $logo) {
+        if ($images_data['status'] == '1') {
+            array_push($slideshow_array, $logo);
+        }
+    }
+}
+$active_class = ' active';
+//\eMarket\Debug::trace($slideshow_array);
 ?>
--- view/default/catalog/layouts/slide_show.php
+++ view/default/catalog/layouts/slide_show.php
@@ -1,44 +1,52 @@
 <?php
 /* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
-  |    GNU GENERAL PUBLIC LICENSE v.3.0    |    
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
-
 ?>
 
-<?php if ($slide_show == true) { ?>
-<div class="container-fluid">
-    <div id="Carousel" class="carousel slide hidden-xs" data-interval="5000" data-pause="hover" data-ride="carousel" data-wrap="true">
-        <ol class="carousel-indicators">
-            <li data-target="#Carousel" data-slide-to="0" class="active"></li>
-            <li data-target="#Carousel" data-slide-to="1" class=""></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="item active">
-		<a href="#">
-		    <img src="" class="center-block" >
-		</a>
-		<div class="carousel-caption">
-		    <h3> </h3>
-		    <p> </p>
-		</div>
-	    </div>
-            <div class="item">
-		<a href="#">
-		    <img src="" class="center-block">
-		</a>
-		<div class="carousel-caption">
-		    <h3> </h3>
-		    <p> </p>
-		</div>
-	    </div>
+<?php if (count($slideshow) > 0) { ?>
+    <div class="container-fluid">
+        <div id="Carousel" class="carousel slide hidden-xs" data-interval="<?php echo $slide_interval ?>" data-pause="<?php echo $slide_pause ?>" data-ride="<?php echo $autostart ?>" data-wrap="<?php echo $cicles ?>">
+            <?php if ($indicators == 'true') { ?>
+                <ol class="carousel-indicators">
+                    <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+                    <?php for ($x = 1; $x < count($slideshow_array); $x++) { ?>
+                        <li data-target="#Carousel" data-slide-to="<?php echo $x ?>"></li>
+                    <?php } ?>
+                </ol>
+            <?php } ?>
+            <div class="carousel-inner">
+                <?php
+                foreach ($slideshow as $images_data) {
+                    foreach (json_decode($images_data['logo'], 1) as $logo) {
+                        if ($images_data['status'] == 1) {
+                            ?>
+                            <div class="item<?php echo $active_class ?>">
+                                <a href="<?php echo $images_data['url'] ?>">
+                                    <img src="/uploads/images/slideshow/resize_4/<?php echo $logo ?>" class="center-block" >
+                                </a>
+                                <div class="carousel-caption">
+                                    <h3><?php echo $images_data['name'] ?></h3>
+                                    <p><?php echo $images_data['heading'] ?></p>
+                                </div>
+                            </div>
+
+                            <?php
+                            $active_class = '';
+                        }
+                    }
+                }
+                ?>
+            </div>
+            <?php if ($navigation_key == 'true') { ?>
+                <a class="carousel-control left" href="#Carousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                </a>
+                <a class="carousel-control right" href="#Carousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                </a>
+            <?php } ?>
         </div>
-        <a class="carousel-control left" href="#Carousel" data-slide="prev">
-            <span class="glyphicon glyphicon-chevron-left"></span>
-        </a>
-        <a class="carousel-control right" href="#Carousel" data-slide="next">
-            <span class="glyphicon glyphicon-chevron-right"></span>
-        </a>
     </div>
-</div>
 <?php } ?>
