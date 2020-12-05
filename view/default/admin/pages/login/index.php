179b461a6490a72709172f8e8a6bc5c7f7c16264
Prizraki <lw12@yandex.ru>
2020-12-05 09:58:43 +0300

    Правка

--- view/default/admin/pages/login/index.php
+++ view/default/admin/pages/login/index.php
@@ -5,29 +5,31 @@
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 ?>
 
-<div class="lbox-horz"></div>
-<div id="login" class="lbox-vert">
-    <?php if ($login_error == TRUE) { ?>
-        <div id="alert" class="alert alert-danger"><span class="glyphicon glyphicon-exclamation-sign"></span> <?php echo $login_error ?><button type="button" class="close" data-dismiss="alert">×</button>
-        </div>
-    <?php } ?>
-</div>
+<div id="login" class="row">
+    <div class="lbox-horz"></div>
+    <div id="login" class="lbox-vert">
+	<?php if ($login_error == TRUE) { ?>
+	    <div id="alert" class="alert alert-danger"><span class="glyphicon glyphicon-exclamation-sign"></span> <?php echo $login_error ?><button type="button" class="close" data-dismiss="alert">×</button>
+	    </div>
+	<?php } ?>
+    </div>
 
-<div class="login_logo">eMarket</div>
+    <div class="login_logo">eMarket</div>
 
 
-<div class="login-box side-form">
-    <form action='?route=login' method='post'>
+    <div class="login-box side-form">
+	<form action='?route=login' method='post'>
 
-        <input hidden name="autorize" value="ok">
+	    <input hidden name="autorize" value="ok">
 
-        <div class="form-group">
-            <input type="text" name="login" class="input-sm form-control" placeholder="<?php echo lang('email') ?>">
-        </div>
-        <div class="form-group">
-            <input type="password" name="pass" class="input-sm form-control" placeholder="<?php echo lang('password') ?>">
-        </div>
+	    <div class="form-group">
+		<input type="text" name="login" class="input-sm form-control" placeholder="<?php echo lang('email') ?>">
+	    </div>
+	    <div class="form-group">
+		<input type="password" name="pass" class="input-sm form-control" placeholder="<?php echo lang('password') ?>">
+	    </div>
 
-        <input type="submit" class="btn btn-block btn-xs" value="<?php echo lang('entrance') ?>">
-    </form>
+	    <input type="submit" class="btn btn-block btn-xs" value="<?php echo lang('entrance') ?>">
+	</form>
+    </div>
 </div>
