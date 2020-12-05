1ada2766e76bc1a32bc1b732ee5560810c3eea61
Prizraki <lw12@yandex.ru>
2020-12-05 10:46:48 +0300

    Правка

--- view/default/admin/pages/dashboard/index.php
+++ view/default/admin/pages/dashboard/index.php
@@ -6,9 +6,11 @@
 ?>
 
 <?php if (isset($_SESSION['login']) && isset($_SESSION['pass'])) { // Выводим если авторизованы   ?>
-    <div id="dashboard" class="welcome text-center">
+<div id="dashboard">
+    <div class="welcome text-center">
 	<?php echo lang('index-title') ?>
         <div class="welcome_logo">eMarket <small>V0.4.0 Alpha 1</small></div>
         <div class="welcome_description"><?php echo lang('index-text') ?></div>
     </div>
+</div>
 <?php } ?>
