faff4d611f51ebf2b6ab8cda416bc90d07691c44
musicman3 <micromail@mail.ru>
2020-12-09 01:35:13 +0200

    сохранение

--- controller/admin/pages/settings/taxes/index.php
+++ controller/admin/pages/settings/taxes/index.php
@@ -5,9 +5,20 @@
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 
-// 
 // Если нажали на кнопку Добавить
 if (\eMarket\Valid::inPOST('add')) {
+
+    if (\eMarket\Valid::inPOST('tax_type')) {
+        $tax_type = 1;
+    } else {
+        $tax_type = 0;
+    }
+
+    if (\eMarket\Valid::inPOST('fixed')) {
+        $fixed = 1;
+    } else {
+        $fixed = 0;
+    }
 
     // Получаем последний id и увеличиваем его на 1
     $id_max = \eMarket\Pdo::selectPrepare("SELECT id FROM " . TABLE_TAXES . " WHERE language=? ORDER BY id DESC", [lang('#lang_all')[0]]);
@@ -15,7 +26,7 @@ if (\eMarket\Valid::inPOST('add')) {
 
     // добавляем запись для всех вкладок
     for ($x = 0; $x < $LANG_COUNT; $x++) {
-        \eMarket\Pdo::inPrepare("INSERT INTO " . TABLE_TAXES . " SET id=?, name=?, language=?, rate=?", [$id, \eMarket\Valid::inPOST('name_taxes_' . $x), lang('#lang_all')[$x], \eMarket\Valid::inPOST('rate_taxes')]);
+        \eMarket\Pdo::inPrepare("INSERT INTO " . TABLE_TAXES . " SET id=?, name=?, language=?, rate=?, tax_type=?, zones_id=?, fixed=?", [$id, \eMarket\Valid::inPOST('name_taxes_' . $x), lang('#lang_all')[$x], \eMarket\Valid::inPOST('rate_taxes'), $tax_type, \eMarket\Valid::inPOST('zones_id'), $fixed]);
     }
 
     // Выводим сообщение об успехе
@@ -25,9 +36,21 @@ if (\eMarket\Valid::inPOST('add')) {
 // Если нажали на кнопку Редактировать
 if (\eMarket\Valid::inPOST('edit')) {
 
+    if (\eMarket\Valid::inPOST('tax_type')) {
+        $tax_type = 1;
+    } else {
+        $tax_type = 0;
+    }
+
+    if (\eMarket\Valid::inPOST('fixed')) {
+        $fixed = 1;
+    } else {
+        $fixed = 0;
+    }
+
     for ($x = 0; $x < $LANG_COUNT; $x++) {
         // обновляем запись
-        \eMarket\Pdo::inPrepare("UPDATE " . TABLE_TAXES . " SET name=?, rate=? WHERE id=? AND language=?", [\eMarket\Valid::inPOST('name_taxes_' . $x), \eMarket\Valid::inPOST('rate_taxes'), \eMarket\Valid::inPOST('edit'), lang('#lang_all')[$x]]);
+        \eMarket\Pdo::inPrepare("UPDATE " . TABLE_TAXES . " SET name=?, rate=?, tax_type=?, zones_id=?, fixed=? WHERE id=? AND language=?", [\eMarket\Valid::inPOST('name_taxes_' . $x), \eMarket\Valid::inPOST('rate_taxes'), $tax_type, \eMarket\Valid::inPOST('zones_id'), $fixed, \eMarket\Valid::inPOST('edit'), lang('#lang_all')[$x]]);
     }
 
     // Выводим сообщение об успехе
@@ -56,5 +79,4 @@ require_once('modal/index.php');
 
 //Создаем маркер для подгрузки JS/JS.PHP в конце перед </body>
 $JS_END = __DIR__;
-
 ?>
