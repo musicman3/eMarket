cdada4ae833df862c3a353813350ef1e3af2cabd
musicman3 <micromail@mail.ru>
2020-12-12 00:58:25 +0200

    Вес

--- controller/admin/pages/settings/weight/index.php
+++ controller/admin/pages/settings/weight/index.php
@@ -91,7 +91,8 @@ if (\eMarket\Valid::inPOST('delete')) {
 }
 
 //КНОПКИ НАВИГАЦИИ НАЗАД-ВПЕРЕД И ПОСТРОЧНЫЙ ВЫВОД ТАБЛИЦЫ
-$lines = \eMarket\Pdo::getColRow("SELECT id, name, code, value_weight, default_weight FROM " . TABLE_WEIGHT . " WHERE language=? ORDER BY id DESC", [lang('#lang_all')[0]]);
+$sql_data = \eMarket\Pdo::getColAssoc("SELECT * FROM " . TABLE_WEIGHT . " ORDER BY id DESC", []);
+$lines = \eMarket\Func::filterData($sql_data, 'language', lang('#lang_all')[0]);
 $lines_on_page = \eMarket\Set::linesOnPage();
 $count_lines = count($lines);
 $navigate = \eMarket\Navigation::getLink($count_lines, $lines_on_page);
--- controller/admin/pages/settings/weight/modal/index.php
+++ controller/admin/pages/settings/weight/modal/index.php
@@ -7,20 +7,23 @@
 // собираем данные для отображения в Редактировании
 $json_data = json_encode([]);
 for ($i = $start; $i < $finish; $i++) {
-    if (isset($lines[$i][0]) == TRUE) {
+    if (isset($lines[$i]['id']) == TRUE) {
 
-        $modal_id = $lines[$i][0]; // ID
-        $count_lang = $LANG_COUNT;
+        $modal_id = $lines[$i]['id']; // ID
 
-        for ($x = 0; $x < $count_lang; $x++) {
-            $query_lang = \eMarket\Pdo::getRow("SELECT name, code FROM " . TABLE_WEIGHT . " WHERE id=? and language=?", [$modal_id, lang('#lang_all')[$x]]);
-            $name[$x][$modal_id] = $query_lang[0];
-            $code[$x][$modal_id] = $query_lang[1];
+        foreach ($sql_data as $sql_modal) {
+            //Языковые
+            if ($sql_modal['id'] == $modal_id) {
+                $name[array_search($sql_modal['language'], lang('#lang_all'))][$modal_id] = $sql_modal['name'];
+                $code[array_search($sql_modal['language'], lang('#lang_all'))][$modal_id] = $sql_modal['code'];
+            }
+            if ($sql_modal['language'] == lang('#lang_all')[0] && $sql_modal['id'] == $modal_id) {
+                $value[$modal_id] = (float) $sql_modal['value_weight'];
+                $status[$modal_id] = (int) $sql_modal['default_weight'];
+            }
         }
-
-        $query = \eMarket\Pdo::getRow("SELECT value_weight, default_weight FROM " . TABLE_WEIGHT . " WHERE id=?", [$modal_id]);
-        $value[$modal_id] = (float) $query[0];
-        $status[$modal_id] = (int) $query[1];
+        //Сортируем языковые
+        ksort($name);
 
         $json_data = json_encode([
             'name' => $name,
