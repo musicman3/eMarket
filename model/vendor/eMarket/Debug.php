b0ab1f87890070aea7386746a837f2c642dfeabb
musicman3 <micromail@mail.ru>
2020-12-16 14:20:33 +0200

    еще

--- model/vendor/eMarket/Debug.php
+++ model/vendor/eMarket/Debug.php
@@ -7,7 +7,7 @@
 namespace eMarket;
 
 /**
- * Класс для отладочной информации
+ * Класс для отладочной информации / Debug class
  *
  * @package Debug
  * @author eMarket
@@ -16,9 +16,9 @@ namespace eMarket;
 class Debug {
 
     /**
-     * Удобное отображение массива при отладке
+     * Отображение массива при отладке / Array displaying when debugging
      *
-     * @param array $var (массив для отображения)
+     * @param array $var (массив / array)
      */
     public static function trace($var) {
         static $int = 0;
@@ -29,18 +29,18 @@ class Debug {
     }
 
     /**
-     * Отображение отладочной информации
+     * Отображение отладочной информации / Displaying debug information
      *
-     * @param string $time_start (начальное время)
+     * @param string $start_time (начальное время / start time)
      */
-    public static function info($time_start) {
+    public static function info($start_time) {
         
         $val = \eMarket\Pdo::getCell("SELECT debug FROM " . TABLE_BASIC_SETTINGS . "", []);
         if ($val == 1) {
-            $tend = microtime(1); // Засекаем конечное время
-            // Округляем до двух знаков после запятой
-            $totaltime = round(($tend - $time_start), 2);
-            // Результат на экран
+            $tend = microtime(1);
+
+            $totaltime = round(($tend - $start_time), 2);
+
             echo lang('debug_page_generation_time') . " " . $totaltime . " " . lang('debug_sec') . "<br>";
             echo lang('debug_db_queries') . " " . \eMarket\Pdo::$query_count . " " . lang('debug_pcs') . "<br><br>";
         }
