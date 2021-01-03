4e198806b26922b500f9bd3dd054816be73505b7
musicman3 <micromail@mail.ru>
2021-01-04 01:57:34 +0200

    производители на натив

--- controller/admin/pages/manufacturers/js/js.php
+++ controller/admin/pages/manufacturers/js/js.php
@@ -17,35 +17,33 @@ $lang_js = json_encode([
 <script type="text/javascript" src="/model/js/classes/ajax/ajax.js"></script>
 
 <script type="text/javascript">
-    var resize_max = $.parseJSON('<?php echo $resize_max ?>');
-    var lang = $.parseJSON('<?php echo $lang_js ?>');
+    var resize_max = JSON.parse('<?php echo $resize_max ?>');
+    var lang = JSON.parse('<?php echo $lang_js ?>');
     new Fileupload(resize_max, lang);
     new Ajax();
-    
+
     $('#index').on('show.bs.modal', function (event) {
 
-        var button = $(event.relatedTarget);
-        var modal_id = button.data('edit'); // Получаем ID из data-edit при клике на кнопку редактирования
+        var button = event.relatedTarget;
+        var modal_id = Number(button.dataset.edit); // Получаем ID из data-edit при клике на кнопку редактирования
+
         if (Number.isInteger(modal_id)) {
 
-            var json_data = $('div#ajax_data').data('jsondata');
-
-            $('#edit').val(modal_id);
-            $('#add').val('');
+            var json_data = JSON.parse(document.getElementById('ajax_data').dataset.jsondata);
+            document.getElementById('edit').value = modal_id;
+            document.getElementById('add').value = '';
 
             // Ищем id и добавляем данные
-            for (x = 0; x < json_data['name'].length; x++) {
-                $('#name_manufacturers_' + x).val(json_data['name'][x][modal_id]);
+            for (var x = 0; x < json_data.name.length; x++) {
+                document.getElementById('name_manufacturers_' + x).value = json_data.name[x][modal_id];
             }
-            $('#site_manufacturers').val(json_data['site'][modal_id]);
+            document.getElementById('site_manufacturers').value = json_data.site[modal_id];
 
             // Подгружаем изображения
-            Fileupload.getImageToEdit(json_data['logo_general'], json_data['logo'], modal_id, 'manufacturers');
+            Fileupload.getImageToEdit(json_data.logo_general, json_data.logo, modal_id, 'manufacturers');
         } else {
-            $('#edit').val('');
-            $('#add').val('ok');
-            //Очищаем поля
-            $(this).find('form').trigger('reset');
+            document.getElementById('edit').value = '';
+            document.getElementById('add').value = 'ok';
         }
     });
 </script>
