2e4060b694b6b5c0b809091ccaf95c784ec625e7
musicman3 <micromail@mail.ru>
2021-01-06 00:32:42 +0200

    вес на натив

--- controller/admin/pages/settings/weight/js/js.php
+++ controller/admin/pages/settings/weight/js/js.php
@@ -13,33 +13,31 @@
 <!-- Загрузка данных в модальное окно -->
 <script type="text/javascript">
     $('#index').on('show.bs.modal', function (event) {
-        var button = $(event.relatedTarget);
-        var modal_id = button.data('edit'); // Получаем ID из data-edit при клике на кнопку редактирования
+        var button = event.relatedTarget;
+        var modal_id = Number(button.dataset.edit); // Получаем ID из data-edit при клике на кнопку редактирования
         if (Number.isInteger(modal_id)) {
             $('#default_weight').bootstrapSwitch('destroy', true);
             // Получаем массивы данных
-            var json_data = $('div#ajax_data').data('jsondata');
+            var json_data = JSON.parse(document.querySelector('#ajax_data').dataset.jsondata);
 
-            $('#edit').val(modal_id);
-            $('#add').val('');
+            document.querySelector('#edit').value = modal_id;
+            document.querySelector('#add').value = '';
 
             // Ищем id и добавляем данные
-            for (x = 0; x < json_data['name'].length; x++) {
-                $('#name_weight_' + x).val(json_data['name'][x][modal_id]);
-                $('#code_weight_' + x).val(json_data['code'][x][modal_id]);
+            for (var x = 0; x < json_data.name.length; x++) {
+                document.querySelector('#name_weight_' + x).value = json_data.name[x][modal_id];
+                document.querySelector('#code_weight_' + x).value = json_data.code[x][modal_id];
             }
 
-            $('#value_weight').val(json_data['value'][modal_id]);
+            document.querySelector('#value_weight').value = json_data.value[modal_id];
             // Меняем значение чекбокса
-            $('#default_weight').prop('checked', json_data['status'][modal_id]);
+            document.querySelector('#default_weight').checked = json_data.status[modal_id];
             $('#default_weight').bootstrapSwitch();
         } else {
-            $('#edit').val('');
-            $('#add').val('ok');
+            document.querySelector('#edit').value = '';
+            document.querySelector('#add').value = 'ok';
             //Очищаем поля
-            $(this).find('form').trigger('reset');
-            // Меняем значение чекбокса
-            $('#default_weight').prop('checked', '1');
+            document.querySelector('form').reset();
         }
     });
 </script>
