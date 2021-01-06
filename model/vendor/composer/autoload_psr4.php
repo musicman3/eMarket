2ce2052d7c4f590887ea5ce5801cbad883b2ca82
musicman3 <micromail@mail.ru>
2021-01-06 16:14:56 +0200

    переделка

--- model/vendor/composer/autoload_psr4.php
+++ model/vendor/composer/autoload_psr4.php
@@ -6,7 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'eMarket\\' => array($vendorDir . '/eMarket'),
+    'eMarket\\' => array($baseDir . '/eMarket'),
     'PHPMailer\\PHPMailer\\' => array($vendorDir . '/phpmailer/phpmailer/src'),
     '' => array($vendorDir . '/league/color-extractor/src'),
 );
