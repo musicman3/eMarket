48ce5391e12cb3545023a88b04eff95b77aab8f6
musicman3 <micromail@mail.ru>
2021-01-12 01:39:22 +0200

    Клиенты на классы

--- controller/admin/pages/customers/index.php
+++ controller/admin/pages/customers/index.php
@@ -5,33 +5,4 @@
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 
-if (\eMarket\Valid::inPOST('status')) {
-    
-    $status_data = \eMarket\Pdo::getCell("SELECT status FROM " . TABLE_CUSTOMERS . " WHERE id=?", [\eMarket\Valid::inPOST('status')]);
-
-    if ($status_data == 0) {
-        $status = 1;
-    } else {
-        $status = 0;
-    }
-    
-    \eMarket\Pdo::action("UPDATE " . TABLE_CUSTOMERS . " SET status=? WHERE id=?", [$status, \eMarket\Valid::inPOST('status')]);
-
-    \eMarket\Messages::alert('success', lang('action_completed_successfully'));
-}
-
-if (\eMarket\Valid::inPOST('delete')) {
-    
-    \eMarket\Pdo::action("DELETE FROM " . TABLE_CUSTOMERS . " WHERE id=?", [\eMarket\Valid::inPOST('delete')]);
-    
-    \eMarket\Messages::alert('success', lang('action_completed_successfully'));
-}
-
-$search = '%' . \eMarket\Valid::inGET('search') . '%';
-if (\eMarket\Valid::inGET('search')) {
-    $lines = \eMarket\Pdo::getColRow("SELECT * FROM " . TABLE_CUSTOMERS . " WHERE firstname LIKE? OR lastname LIKE? OR middle_name LIKE? OR email LIKE? ORDER BY id DESC", [$search, $search, $search, $search]);
-} else {
-    $lines = \eMarket\Pdo::getColRow("SELECT * FROM " . TABLE_CUSTOMERS . " ORDER BY id DESC", []);
-}
-
-\eMarket\Pages::table($lines);
+$eMarketCustomers = new eMarket\Admin\Customers();
