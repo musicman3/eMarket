6144cfa52aabf57c5c7f8789ae2b8607e35ef5f6
musicman3 <micromail@mail.ru>
2021-01-13 00:45:05 +0200

    Modules

--- controller/admin/pages/settings/modules/index.php
+++ controller/admin/pages/settings/modules/index.php
@@ -5,33 +5,4 @@
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 
-$installed = \eMarket\Pdo::getColAssoc("SELECT name, type FROM " . TABLE_MODULES . "", []);
-$installed_active = \eMarket\Pdo::getColAssoc("SELECT name, type FROM " . TABLE_MODULES . " WHERE active=?", [1]);
-
-if (\eMarket\Valid::inPOST('add')) {
-    $module = explode('_', \eMarket\Valid::inPOST('add'));
-    $namespace = '\eMarket\Modules\\' . ucfirst($module[0]) . '\\' . ucfirst($module[1]);
-    $namespace::install($module);
-
-    \eMarket\Messages::alert('success', lang('action_completed_successfully'));
-}
-
-if (\eMarket\Valid::inPOST('delete')) {
-    $module = explode('_', \eMarket\Valid::inPOST('delete'));
-    $namespace = '\eMarket\Modules\\' . ucfirst($module[0]) . '\\' . ucfirst($module[1]);
-    $namespace::uninstall($module);
-
-    \eMarket\Messages::alert('success', lang('action_completed_successfully'));
-}
-
-if (\eMarket\Valid::inPOST('edit_active')) {
-
-    if (\eMarket\Valid::inPOST('switch_active') == 'on') {
-        $active = 1;
-    }
-    if (!\eMarket\Valid::inPOST('switch_active')) {
-        $active = 0;
-    }
-    $module = explode('_', \eMarket\Valid::inPOST('edit_active'));
-    \eMarket\Pdo::action("UPDATE " . TABLE_MODULES . " SET active=? WHERE name=? AND type=?", [$active, $module[1], $module[0]]);
-}
+$eMarket = new eMarket\Admin\Modules();
