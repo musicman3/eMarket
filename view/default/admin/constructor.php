be8d82d253ab75e78b519bded506a976a2a50fb3
musicman3 <micromail@mail.ru>
2021-01-19 01:06:29 +0200

    лишнее

--- view/default/admin/constructor.php
+++ view/default/admin/constructor.php
@@ -12,7 +12,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=Edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="robots" content="noindex,nofollow" />
-        <meta name="generator" content="HippoEDIT, Netbeans, Notepad++" />
+        <meta name="generator" content="Netbeans" />
         <meta name="classification" content="software" />
         <meta name="author" content="eMarket" />
         <meta name="owner" content="eMarket" />
--- view/default/catalog/constructor.php
+++ view/default/catalog/constructor.php
@@ -12,7 +12,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=Edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="robots" content="index,follow" />
-        <meta name="generator" content="HippoEDIT, Netbeans, Notepad++" />
+        <meta name="generator" content="Netbeans" />
         <meta name="classification" content="software" />
         <meta name="author" content="eMarket" />
         <meta name="owner" content="eMarket" />
