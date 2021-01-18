64207630126a7afaaf74f2cfda24aa484297c576
musicman3 <micromail@mail.ru>
2021-01-19 01:46:13 +0200

    стикеры

--- model/eMarket/Admin/Stikers.php
+++ model/eMarket/Admin/Stikers.php
@@ -7,6 +7,17 @@
 
 namespace eMarket\Admin;
 
+use \eMarket\Core\{
+    Eac,
+    Func,
+    Lang,
+    Messages,
+    Pages,
+    Pdo,
+    Valid
+};
+use \eMarket\Admin\HeaderMenu;
+
 /**
  * Stikers
  *
@@ -34,41 +45,43 @@ class Stikers {
         $this->data();
         $this->modal();
     }
-    
+
     /**
      * Menu config
      * [0] - url, [1] - icon, [2] - name, [3] - target="_blank", [4] - submenu (true/false)
      * 
      */
     public static function menu() {
-        \eMarket\Admin\HeaderMenu::$menu[\eMarket\Admin\HeaderMenu::$menu_marketing][] = ['?route=stikers', 'glyphicon glyphicon-bookmark', lang('title_stikers_index'), '', 'false'];
-    }    
+        HeaderMenu::$menu[HeaderMenu::$menu_marketing][] = ['?route=stikers', 'glyphicon glyphicon-bookmark', lang('title_stikers_index'), '', 'false'];
+    }
 
     /**
      * Add
      *
      */
     public function add() {
-        if (\eMarket\Core\Valid::inPOST('add')) {
+        if (Valid::inPOST('add')) {
 
-            if (\eMarket\Core\Valid::inPOST('default_stikers')) {
+            if (Valid::inPOST('default_stikers')) {
                 $default_stikers = 1;
             } else {
                 $default_stikers = 0;
             }
 
-            $id_max = \eMarket\Core\Pdo::selectPrepare("SELECT id FROM " . TABLE_STIKERS . " WHERE language=? ORDER BY id DESC", [lang('#lang_all')[0]]);
+            $id_max = Pdo::selectPrepare("SELECT id FROM " . TABLE_STIKERS . " WHERE language=? ORDER BY id DESC", [lang('#lang_all')[0]]);
             $id = intval($id_max) + 1;
 
             if ($id > 1 && $default_stikers != 0) {
-                \eMarket\Core\Pdo::action("UPDATE " . TABLE_STIKERS . " SET default_stikers=?", [0]);
+                Pdo::action("UPDATE " . TABLE_STIKERS . " SET default_stikers=?", [0]);
             }
 
-            for ($x = 0; $x < \eMarket\Core\Lang::$COUNT; $x++) {
-                \eMarket\Core\Pdo::action("INSERT INTO " . TABLE_STIKERS . " SET id=?, name=?, language=?, default_stikers=?", [$id, \eMarket\Core\Valid::inPOST('name_stikers_' . $x), lang('#lang_all')[$x], $default_stikers]);
+            for ($x = 0; $x < Lang::$COUNT; $x++) {
+                Pdo::action("INSERT INTO " . TABLE_STIKERS . " SET id=?, name=?, language=?, default_stikers=?", [
+                    $id, Valid::inPOST('name_stikers_' . $x), lang('#lang_all')[$x], $default_stikers
+                ]);
             }
 
-            \eMarket\Core\Messages::alert('success', lang('action_completed_successfully'));
+            Messages::alert('success', lang('action_completed_successfully'));
         }
     }
 
@@ -77,23 +90,25 @@ class Stikers {
      *
      */
     public function edit() {
-        if (\eMarket\Core\Valid::inPOST('edit')) {
+        if (Valid::inPOST('edit')) {
 
-            if (\eMarket\Core\Valid::inPOST('default_stikers')) {
+            if (Valid::inPOST('default_stikers')) {
                 $default_stikers = 1;
             } else {
                 $default_stikers = 0;
             }
 
             if ($default_stikers != 0) {
-                \eMarket\Core\Pdo::action("UPDATE " . TABLE_STIKERS . " SET default_stikers=?", [0]);
+                Pdo::action("UPDATE " . TABLE_STIKERS . " SET default_stikers=?", [0]);
             }
 
-            for ($x = 0; $x < \eMarket\Core\Lang::$COUNT; $x++) {
-                \eMarket\Core\Pdo::action("UPDATE " . TABLE_STIKERS . " SET name=?, default_stikers=? WHERE id=? AND language=?", [\eMarket\Core\Valid::inPOST('name_stikers_' . $x), $default_stikers, \eMarket\Core\Valid::inPOST('edit'), lang('#lang_all')[$x]]);
+            for ($x = 0; $x < Lang::$COUNT; $x++) {
+                Pdo::action("UPDATE " . TABLE_STIKERS . " SET name=?, default_stikers=? WHERE id=? AND language=?", [
+                    Valid::inPOST('name_stikers_' . $x), $default_stikers, Valid::inPOST('edit'), lang('#lang_all')[$x]
+                ]);
             }
 
-            \eMarket\Core\Messages::alert('success', lang('action_completed_successfully'));
+            Messages::alert('success', lang('action_completed_successfully'));
         }
     }
 
@@ -102,10 +117,10 @@ class Stikers {
      *
      */
     public function delete() {
-        if (\eMarket\Core\Valid::inPOST('delete')) {
-            \eMarket\Core\Pdo::action("DELETE FROM " . TABLE_STIKERS . " WHERE id=?", [\eMarket\Core\Valid::inPOST('delete')]);
+        if (Valid::inPOST('delete')) {
+            Pdo::action("DELETE FROM " . TABLE_STIKERS . " WHERE id=?", [Valid::inPOST('delete')]);
 
-            \eMarket\Core\Messages::alert('success', lang('action_completed_successfully'));
+            Messages::alert('success', lang('action_completed_successfully'));
         }
     }
 
@@ -114,9 +129,9 @@ class Stikers {
      *
      */
     public function data() {
-        self::$sql_data = \eMarket\Core\Pdo::getColAssoc("SELECT * FROM " . TABLE_STIKERS . " ORDER BY id DESC", []);
-        $lines = \eMarket\Core\Func::filterData(self::$sql_data, 'language', lang('#lang_all')[0]);
-        \eMarket\Core\Pages::table($lines);
+        self::$sql_data = Pdo::getColAssoc("SELECT * FROM " . TABLE_STIKERS . " ORDER BY id DESC", []);
+        $lines = Func::filterData(self::$sql_data, 'language', lang('#lang_all')[0]);
+        Pages::table($lines);
     }
 
     /**
@@ -127,7 +142,7 @@ class Stikers {
         self::$stikers = '';
         self::$stikers_default = 0;
         self::$stikers_flag = 0;
-        $stikers_data = \eMarket\Core\Pdo::getColAssoc("SELECT * FROM " . TABLE_STIKERS . " WHERE language=?", [lang('#lang_all')[0]]);
+        $stikers_data = Pdo::getColAssoc("SELECT * FROM " . TABLE_STIKERS . " WHERE language=?", [lang('#lang_all')[0]]);
 
         foreach ($stikers_data as $val) {
             self::$stikers_flag = 1;
@@ -148,18 +163,18 @@ class Stikers {
      */
     public static function initEac() {
 
-        if ((\eMarket\Core\Valid::inPOST('idsx_stikerOn_key') == 'On')
-                or ( \eMarket\Core\Valid::inPOST('idsx_stikerOff_key') == 'Off')) {
+        if ((Valid::inPOST('idsx_stikerOn_key') == 'On')
+                or (Valid::inPOST('idsx_stikerOff_key') == 'Off')) {
 
-            $parent_id_real = (int) \eMarket\Core\Valid::inPOST('idsx_real_parent_id');
+            $parent_id_real = (int) Valid::inPOST('idsx_real_parent_id');
 
-            if (\eMarket\Core\Valid::inPOST('idsx_stikerOn_key') == 'On') {
-                $idx = \eMarket\Core\Func::deleteEmptyInArray(\eMarket\Core\Valid::inPOST('idsx_stiker_on_id'));
-                $stiker = \eMarket\Core\Valid::inPOST('stiker');
+            if (Valid::inPOST('idsx_stikerOn_key') == 'On') {
+                $idx = Func::deleteEmptyInArray(Valid::inPOST('idsx_stiker_on_id'));
+                $stiker = Valid::inPOST('stiker');
             }
 
-            if (\eMarket\Core\Valid::inPOST('idsx_stikerOff_key') == 'Off') {
-                $idx = \eMarket\Core\Func::deleteEmptyInArray(\eMarket\Core\Valid::inPOST('idsx_stiker_off_id'));
+            if (Valid::inPOST('idsx_stikerOff_key') == 'Off') {
+                $idx = Func::deleteEmptyInArray(Valid::inPOST('idsx_stiker_off_id'));
                 $stiker = '';
             }
 
@@ -169,38 +184,38 @@ class Stikers {
 
             for ($i = 0; $i < count($idx); $i++) {
                 if (strstr($idx[$i], '_', true) != 'product') {
-                    // Это категория / This is category
-                    \eMarket\Core\Eac::$parent_id = self::dataParentId($idx[$i]);
+                    Eac::$parent_id = self::dataParentId($idx[$i]);
                     $keys = self::dataKeys($idx[$i]);
 
                     $count_keys = count($keys);
                     for ($x = 0; $x < $count_keys; $x++) {
 
-                        if (\eMarket\Core\Valid::inPOST('idsx_stikerOn_key') == 'On' OR \eMarket\Core\Valid::inPOST('idsx_stikerOff_key') == 'Off') {
-                            // Это товар / This is product
-                            $stiker_id_array = \eMarket\Core\Pdo::getCol("SELECT id FROM " . TABLE_PRODUCTS . " WHERE language=? AND parent_id=?", [lang('#lang_all')[0], $keys[$x]]);
+                        if (Valid::inPOST('idsx_stikerOn_key') == 'On' OR Valid::inPOST('idsx_stikerOff_key') == 'Off') {
+
+                            $stiker_id_array = Pdo::getCol("SELECT id FROM " . TABLE_PRODUCTS . " WHERE language=? AND parent_id=?", [
+                                        lang('#lang_all')[0], $keys[$x]
+                            ]);
 
                             foreach ($stiker_id_array as $stiker_id_arr) {
-                                \eMarket\Core\Pdo::action("UPDATE " . TABLE_PRODUCTS . " SET stiker=? WHERE id=?", [$stiker, $stiker_id_arr]);
+                                Pdo::action("UPDATE " . TABLE_PRODUCTS . " SET stiker=? WHERE id=?", [$stiker, $stiker_id_arr]);
                             }
 
                             if ($parent_id_real > 0) {
-                                \eMarket\Core\Eac::$parent_id = $parent_id_real;
+                                Eac::$parent_id = $parent_id_real;
                             }
                         }
                     }
                 } else {
-                    // Это товар / This is product
-                    if (\eMarket\Core\Valid::inPOST('idsx_stikerOn_key') == 'On' OR \eMarket\Core\Valid::inPOST('idsx_stikerOff_key') == 'Off') {
+                    if (Valid::inPOST('idsx_stikerOn_key') == 'On' OR Valid::inPOST('idsx_stikerOff_key') == 'Off') {
                         $id_prod = explode('product_', $idx[$i]);
-                        \eMarket\Core\Pdo::action("UPDATE " . TABLE_PRODUCTS . " SET stiker=? WHERE id=?", [$stiker, $id_prod[1]]);
+                        Pdo::action("UPDATE " . TABLE_PRODUCTS . " SET stiker=? WHERE id=?", [$stiker, $id_prod[1]]);
                     }
                 }
             }
         }
 
-        if (is_array(\eMarket\Core\Eac::$parent_id) == TRUE) {
-            \eMarket\Core\Eac::$parent_id = 0;
+        if (is_array(Eac::$parent_id) == TRUE) {
+            Eac::$parent_id = 0;
         }
     }
 
@@ -211,10 +226,10 @@ class Stikers {
     public function modal() {
         self::$json_data = json_encode([]);
         $name = [];
-        for ($i = \eMarket\Core\Pages::$start; $i < \eMarket\Core\Pages::$finish; $i++) {
-            if (isset(\eMarket\Core\Pages::$table['lines'][$i]['id']) == TRUE) {
+        for ($i = Pages::$start; $i < Pages::$finish; $i++) {
+            if (isset(Pages::$table['lines'][$i]['id']) == TRUE) {
 
-                $modal_id = \eMarket\Core\Pages::$table['lines'][$i]['id'];
+                $modal_id = Pages::$table['lines'][$i]['id'];
 
                 foreach (self::$sql_data as $sql_modal) {
                     if ($sql_modal['id'] == $modal_id) {
