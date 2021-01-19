e33d15472427ea5e486756a4213f7b29fb5009ae
musicman3 <micromail@mail.ru>
2021-01-19 16:09:48 +0200

    листинг

--- model/eMarket/Catalog/Listing.php
+++ model/eMarket/Catalog/Listing.php
@@ -7,6 +7,14 @@
 
 namespace eMarket\Catalog;
 
+use \eMarket\Core\{
+    Ecb,
+    Pages,
+    Pdo,
+    Products,
+    Valid
+};
+
 /**
  * Listing
  *
@@ -39,32 +47,32 @@ class Listing {
      *
      */
     public function initData() {
-        if (\eMarket\Core\Valid::inGET('change') == 'on' OR!\eMarket\Core\Valid::inGET('change')) {
+        if (Valid::inGET('change') == 'on' OR!Valid::inGET('change')) {
             self::$checked_stock = ' checked';
             $qnt_flag = '';
         } else {
             self::$checked_stock = '';
             $qnt_flag = 'AND quantity>0 ';
         }
-        if (!\eMarket\Core\Valid::inGET('sort')) {
+        if (!Valid::inGET('sort')) {
             self::$sort_flag = 'on';
         } else {
             self::$sort_flag = 'off';
         }
 
-        if (!\eMarket\Core\Valid::inGET('sort') OR \eMarket\Core\Valid::inGET('sort') == 'default') {
+        if (!Valid::inGET('sort') OR Valid::inGET('sort') == 'default') {
             self::$sort_parameter = $qnt_flag . 'ORDER BY id DESC';
             self::$sort_name = lang('listing_sort_by_default');
         }
-        if (\eMarket\Core\Valid::inGET('sort') == 'name') {
+        if (Valid::inGET('sort') == 'name') {
             self::$sort_parameter = $qnt_flag . 'ORDER BY name ASC';
             self::$sort_name = lang('listing_sort_by_name');
         }
-        if (\eMarket\Core\Valid::inGET('sort') == 'up') {
+        if (Valid::inGET('sort') == 'up') {
             self::$sort_parameter = $qnt_flag . 'ORDER BY price ASC';
             self::$sort_name = lang('listing_sort_by_price_asc');
         }
-        if (\eMarket\Core\Valid::inGET('sort') == 'down') {
+        if (Valid::inGET('sort') == 'down') {
             self::$sort_parameter = $qnt_flag . 'ORDER BY price DESC';
             self::$sort_name = lang('listing_sort_by_price_desc');
         }
@@ -75,15 +83,19 @@ class Listing {
      *
      */
     public function data() {
-        if (\eMarket\Core\Valid::inGET('search')) {
-            $search = '%' . \eMarket\Core\Valid::inGET('search') . '%';
-            self::$lines = \eMarket\Core\Pdo::getColAssoc("SELECT * FROM " . TABLE_PRODUCTS . " WHERE (name LIKE? OR description LIKE?) AND language=? AND status=? " . self::$sort_parameter, [$search, $search, lang('#lang_all')[0], 1]);
+        if (Valid::inGET('search')) {
+            $search = '%' . Valid::inGET('search') . '%';
+            self::$lines = Pdo::getColAssoc("SELECT * FROM " . TABLE_PRODUCTS . " WHERE (name LIKE? OR description LIKE?) AND language=? AND status=? " . self::$sort_parameter, [
+                        $search, $search, lang('#lang_all')[0], 1
+            ]);
         } else {
-            self::$lines = \eMarket\Core\Pdo::getColAssoc("SELECT * FROM " . TABLE_PRODUCTS . " WHERE language=? AND parent_id=? AND status=? " . self::$sort_parameter, [lang('#lang_all')[0], \eMarket\Core\Valid::inGET('category_id'), 1]);
+            self::$lines = Pdo::getColAssoc("SELECT * FROM " . TABLE_PRODUCTS . " WHERE language=? AND parent_id=? AND status=? " . self::$sort_parameter, [
+                        lang('#lang_all')[0], Valid::inGET('category_id'), 1
+            ]);
         }
-        \eMarket\Core\Pages::table(self::$lines);
+        Pages::table(self::$lines);
 
-        self::$categories_name = \eMarket\Core\Products::categoryData(\eMarket\Core\Valid::inGET('category_id'))['name'];
+        self::$categories_name = Products::categoryData(Valid::inGET('category_id'))['name'];
     }
 
     /**
@@ -92,14 +104,14 @@ class Listing {
      */
     public function modal() {
         self::$product_edit = json_encode([]);
-        for ($i = \eMarket\Core\Pages::$start; $i < \eMarket\Core\Pages::$finish; $i++) {
+        for ($i = Pages::$start; $i < Pages::$finish; $i++) {
             if (isset(self::$lines[$i]['id']) == TRUE) {
 
                 $modal_id = self::$lines[$i]['id'];
 
-                $query = \eMarket\Core\Pdo::getColAssoc("SELECT * FROM " . TABLE_PRODUCTS . " WHERE id=? and language=?", [$modal_id, lang('#lang_all')[0]])[0];
+                $query = Pdo::getColAssoc("SELECT * FROM " . TABLE_PRODUCTS . " WHERE id=? and language=?", [$modal_id, lang('#lang_all')[0]])[0];
                 $product_temp[$modal_id] = $query;
-                $product_temp[$modal_id]['price_formated'] = \eMarket\Core\Ecb::priceInterface($query, 1);
+                $product_temp[$modal_id]['price_formated'] = Ecb::priceInterface($query, 1);
 
                 self::$product_edit = json_encode($product_temp);
             }
