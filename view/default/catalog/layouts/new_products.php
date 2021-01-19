332e2f83a500a2d37e2954c4e1cf4d75472ace16
musicman3 <micromail@mail.ru>
2021-01-19 17:30:36 +0200

    новые

--- view/default/catalog/layouts/new_products.php
+++ view/default/catalog/layouts/new_products.php
@@ -4,21 +4,26 @@
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 
-\eMarket\Core\Products::newProducts(8);
+use \eMarket\Core\{
+    Ecb,
+    Products,
+};
 
-if (\eMarket\Core\Products::$new_products == true) {
+Products::newProducts(8);
+
+if (Products::$new_products == true) {
     ?>
     <div id="new_products" class="contentText">
         <h3><?php echo lang('new_products_name') ?></h3>
         <div class="row row-flex">
-            <?php foreach (\eMarket\Core\Products::$new_products as $value) {
+            <?php foreach (Products::$new_products as $value) {
                 ?>
                 <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item">
                     <div class="productHolder">
-                        <?php echo \eMarket\Core\Products::stikers($value, 'label-danger', 'label-success') ?>
+                        <?php echo Products::stikers($value, 'label-danger', 'label-success') ?>
                         <a href="/?route=products&category_id=<?php echo $value['parent_id'] ?>&id=<?php echo $value['id'] ?>"><img src="/uploads/images/products/resize_1/<?php echo $value['logo_general'] ?>" alt="<?php echo $value['name']; ?>" class="img-responsive center-block"></a>
                         <h5 class="text-center item-heading"><a href="/?route=products&category_id=<?php echo $value['parent_id'] ?>&id=<?php echo $value['id'] ?>"><?php echo $value['name'] ?></a></h5>
-                        <div class="text-center item-price"><?php echo \eMarket\Core\Ecb::priceInterface($value, 1) ?></div>
+                        <div class="text-center item-price"><?php echo Ecb::priceInterface($value, 1) ?></div>
 
                     </div>
                 </div>
