98170de42d389467e3c13c9a56b410b323dd9812
musicman3 <micromail@mail.ru>
2021-01-19 17:29:36 +0200

    слайд

--- view/default/catalog/layouts/slide_show.php
+++ view/default/catalog/layouts/slide_show.php
@@ -4,31 +4,34 @@
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 
-\eMarket\Admin\Slideshow::view();
+use \eMarket\Admin\Slideshow;
+use \eMarket\Core\Settings;
 
-if (\eMarket\Admin\Slideshow::$slideshow == true) {
+Slideshow::view();
+
+if (Slideshow::$slideshow == true) {
     ?>
     <div class="container-fluid">
         <div id="Carousel" class="carousel slide hidden-xs" 
-             data-interval="<?php echo \eMarket\Admin\Slideshow::$slide_interval ?>" 
-             data-pause="<?php echo \eMarket\Admin\Slideshow::$slide_pause ?>" 
-             data-ride="<?php echo \eMarket\Admin\Slideshow::$autostart ?>" 
-             data-wrap="<?php echo \eMarket\Admin\Slideshow::$cicles ?>">
-                 <?php if (\eMarket\Admin\Slideshow::$indicators == 'true') { ?>
+             data-interval="<?php echo Slideshow::$slide_interval ?>" 
+             data-pause="<?php echo Slideshow::$slide_pause ?>" 
+             data-ride="<?php echo Slideshow::$autostart ?>" 
+             data-wrap="<?php echo Slideshow::$cicles ?>">
+                 <?php if (Slideshow::$indicators == 'true') { ?>
                 <ol class="carousel-indicators">
                     <li data-target="#Carousel" data-slide-to="0" class="active"></li>
-                    <?php for ($x = 1; $x < count(\eMarket\Admin\Slideshow::$slideshow_array); $x++) { ?>
+                    <?php for ($x = 1; $x < count(Slideshow::$slideshow_array); $x++) { ?>
                         <li data-target="#Carousel" data-slide-to="<?php echo $x ?>"></li>
                     <?php } ?>
                 </ol>
             <?php } ?>
             <div class="carousel-inner">
                 <?php
-                foreach (\eMarket\Admin\Slideshow::$slideshow as $images_data) {
+                foreach (Slideshow::$slideshow as $images_data) {
                     foreach (json_decode($images_data['logo'], 1) as $logo) {
-                        if ($images_data['status'] == 1 && strtotime($images_data['date_start']) <= \eMarket\Admin\Slideshow::$this_time && strtotime($images_data['date_finish']) >= \eMarket\Admin\Slideshow::$this_time) {
+                        if ($images_data['status'] == 1 && strtotime($images_data['date_start']) <= Slideshow::$this_time && strtotime($images_data['date_finish']) >= Slideshow::$this_time) {
                             ?>
-                            <div class="item <?php echo \eMarket\Core\Settings::activeTab(0, 0) ?>">
+                            <div class="item <?php echo Settings::activeTab(0, 0) ?>">
                                 <a href="<?php echo $images_data['url'] ?>">
                                     <img src="/uploads/images/slideshow/resize_4/<?php echo $logo ?>" class="center-block" >
                                     <?php if ($images_data['animation'] == 1) { ?>
@@ -51,7 +54,7 @@ if (\eMarket\Admin\Slideshow::$slideshow == true) {
                 }
                 ?>
             </div>
-            <?php if (\eMarket\Admin\Slideshow::$navigation_key == 'true') { ?>
+            <?php if (Slideshow::$navigation_key == 'true') { ?>
                 <a class="carousel-control left" href="#Carousel" data-slide="prev">
                     <span class="glyphicon glyphicon-chevron-left"></span>
                 </a>
