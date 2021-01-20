4c4c27c386669a3eccfc03518d7cfbe4aea002b9
musicman3 <micromail@mail.ru>
2021-01-20 17:29:27 +0200

    еще

--- model/eMarket/Core/Ecb.php
+++ model/eMarket/Core/Ecb.php
@@ -263,7 +263,7 @@ final class Ecb {
     /**
      * Price with region format
      *
-     * @param string $price Price
+     * @param int $price Price
      * @param int $format Format flag (Example: 0 - Dollar USA, 1 - doll., 2 - $, 3 - USD)
      * @param string $language Language
      * @return string Format price data
