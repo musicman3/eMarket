74f9ae3beb1c82e1cca4f13be1f8458e284badd0
Prizraki <lw12@yandex.ru>
2021-01-25 21:15:05 +0300

    Поправил с новыми классами

--- view/default/admin/pages/settings/templates/index.php
+++ view/default/admin/pages/settings/templates/index.php
@@ -19,66 +19,69 @@ use \eMarket\Admin\Templates;
         </div>
         <div class="modal-body">
 
-            <div class="form-inline">
-                <div class="input-group has-error">
-                    <span class="input-group-text"><span class="glyphicon glyphicon-th-large"></span></span>
-                    <form method="get" name="select_template" action="index.php">
-                        <input hidden name="route" value="settings/templates">
-                        <select name="name_templates" id="name_templates" class="form-select"  onchange="selectTemplate()">
-                            <option><?php echo Settings::template() ?></option>
-                            <?php
-                            foreach (Templates::$name_template as $path) {
-                                if ($path != '.' && $path != '..' && $path != Settings::template()) {
-                                    if ($path == Templates::$select_template) {
-                                        ?>
-                                        <option selected><?php echo $path ?></option>
-                                    <?php } else {
-                                        ?>
-                                        <option><?php echo $path ?></option>
-                                        <?php
-                                    }
-                                }
-                            }
-                            ?>
-                        </select>
-                    </form>
-                </div>
-
-                <div class="input-group has-error">
-                    <span class="input-group-text"><span class="bi-file-text"></span></span>
-                    <form method="get" name="select_page">
-                        <input hidden name="route" value="settings/templates">
-                        <input type="hidden" name="name_templates" value="<?php echo Templates::$select_template ?>" />
-                        <select name="layout_pages_templates" id="layout_pages_templates" class="form-select" onchange="selectPage()">
-                            <option value="all"><?php echo lang('all_pages_template') ?></option>
-                            <?php
-                            foreach (Templates::$layout_pages as $path) {
-                                if ($path != '.' && $path != '..') {
-                                    if ($path == Templates::$select_page) {
-                                        ?>
-                                        <option value="<?php echo $path ?>" selected><?php echo $path ?></option>
-                                    <?php } else {
-                                        ?>
-                                        <option><?php echo $path ?></option>
-                                        <?php
-                                    }
-                                }
-                            }
-                            ?>
-                        </select>
-                    </form>
-                </div>
+            <div class="row">
+		<div class="col-2">
+		    <div class="input-group has-error">
+			<span class="input-group-text"><span class="glyphicon glyphicon-th-large"></span></span>
+			<form method="get" name="select_template" action="index.php">
+			    <input hidden name="route" value="settings/templates">
+			    <select name="name_templates" id="name_templates" class="form-select"  onchange="selectTemplate()">
+				<option><?php echo Settings::template() ?></option>
+				<?php
+				foreach (Templates::$name_template as $path) {
+				    if ($path != '.' && $path != '..' && $path != Settings::template()) {
+					if ($path == Templates::$select_template) {
+					    ?>
+					    <option selected><?php echo $path ?></option>
+					<?php } else {
+					    ?>
+					    <option><?php echo $path ?></option>
+					    <?php
+					}
+				    }
+				}
+				?>
+			    </select>
+			</form>
+		    </div>
+		</div>
+		<div class="col">
+		    <div class="input-group has-error">
+			<span class="input-group-text"><span class="bi-file-text"></span></span>
+			<form method="get" name="select_page">
+			    <input hidden name="route" value="settings/templates">
+			    <input type="hidden" name="name_templates" value="<?php echo Templates::$select_template ?>" />
+			    <select name="layout_pages_templates" id="layout_pages_templates" class="form-select" onchange="selectPage()">
+				<option value="all"><?php echo lang('all_pages_template') ?></option>
+				<?php
+				foreach (Templates::$layout_pages as $path) {
+				    if ($path != '.' && $path != '..') {
+					if ($path == Templates::$select_page) {
+					    ?>
+					    <option value="<?php echo $path ?>" selected><?php echo $path ?></option>
+					<?php } else {
+					    ?>
+					    <option><?php echo $path ?></option>
+					    <?php
+					}
+				    }
+				}
+				?>
+			    </select>
+			</form>
+		    </div>
+		</div>
             </div>
 
             <div class="clearfix"></br></div>
             <div class="center-block">
-                <ul id="sortable1" class="connectedSortable block-ul" style="width:66%">
+                <ul id="sortable1" class="connectedSortable block-ul" style="width:66.3%">
                     <li class="sortno border bg-primary">header &nbsp;<span class="glyphicon glyphicon-resize-horizontal"></span></li>
                     <?php foreach (Templates::$layout_header as $path) { ?>
                         <li id="<?php echo basename($path, '.php') ?>" class="sortyes"><?php echo basename($path, '.php') ?></li>
                     <?php } ?>
                 </ul>
-                <ul id="sortable2" class="connectedSortable block-ul" style="width:33%">
+                <ul id="sortable2" class="connectedSortable block-ul" style="width:33.3%">
                     <li class="sortno border bg-primary"><span class="glyphicon glyphicon-resize-horizontal"></span><span class="bi-trash"></span></li>
                     <?php foreach (Templates::$layout_header_basket as $path) { ?>
                         <li id="<?php echo basename($path, '.php') ?>" class="sortyes"><?php echo basename($path, '.php') ?></li>
@@ -86,13 +89,13 @@ use \eMarket\Admin\Templates;
                 </ul>
             </div>
             <div class="center-block">
-                <ul id="sortable3" class="connectedSortable2 block-ul" style="width:66%">
+                <ul id="sortable3" class="connectedSortable2 block-ul" style="width:66.3%">
                     <li class="sortno border bg-primary">content &nbsp;<span class="glyphicon glyphicon-resize-horizontal"></span></li>
                     <?php foreach (Templates::$layout_content as $path) { ?>
                         <li id="<?php echo basename($path, '.php') ?>" class="sortyes"><?php echo basename($path, '.php') ?></li>
                     <?php } ?>
                 </ul>
-                <ul id="sortable4" class="connectedSortable2 block-ul" style="width:33%">
+                <ul id="sortable4" class="connectedSortable2 block-ul" style="width:33.3%">
                     <li class="sortno border bg-primary"><span class="glyphicon glyphicon-resize-horizontal"></span><span class="bi-trash"></span></li>
                     <?php foreach (Templates::$layout_content_basket as $path) { ?>
                         <li id="<?php echo basename($path, '.php') ?>" class="sortyes"><?php echo basename($path, '.php') ?></li>
@@ -100,19 +103,19 @@ use \eMarket\Admin\Templates;
                 </ul>
             </div>
             <div class="center-block">
-                <ul id="sortable5" class="connectedSortable3 block-l" style="width:33%;">
+                <ul id="sortable5" class="connectedSortable3 block-l" style="width:33.2%;">
                     <li class="sortno border-l bg-primary">boxes-left &nbsp;<span class="glyphicon glyphicon-resize-horizontal"></span></li>
                     <?php foreach (Templates::$layout_boxes_left as $path) { ?>
                         <li id="<?php echo basename($path, '.php') ?>" class="sortyes"><?php echo basename($path, '.php') ?></li>
                     <?php } ?>
                 </ul>
-                <ul id="sortable6" class="connectedSortable3 block-m block-r" style="width:33%;">
+                <ul id="sortable6" class="connectedSortable3 block-m block-r" style="width:33.2%;">
                     <li class="sortno border-r bg-primary">boxes-right &nbsp;<span class="glyphicon glyphicon-resize-horizontal"></span></li>
                     <?php foreach (Templates::$layout_boxes_right as $path) { ?>
                         <li id="<?php echo basename($path, '.php') ?>" class="sortyes"><?php echo basename($path, '.php') ?></li>
                     <?php } ?>
                 </ul>
-                <ul id="sortable7" class="connectedSortable3 block-ul" style="width:33%">
+                <ul id="sortable7" class="connectedSortable3 block-ul" style="width:33.2%">
                     <li class="sortno border bg-primary"><span class="glyphicon glyphicon-resize-horizontal"></span><span class="bi-trash"></span></li>
                     <?php foreach (Templates::$layout_boxes_basket as $path) { ?>
                         <li id="<?php echo basename($path, '.php') ?>" class="sortyes"><?php echo basename($path, '.php') ?></li>
@@ -120,13 +123,13 @@ use \eMarket\Admin\Templates;
                 </ul>
             </div>
             <div class="center-block">
-                <ul id="sortable8" class="connectedSortable4 block-ul" style="width:66%">
+                <ul id="sortable8" class="connectedSortable4 block-ul" style="width:66.3%">
                     <li class="sortno border bg-primary">footer &nbsp;<span class="glyphicon glyphicon-resize-horizontal"></span></li>
                     <?php foreach (Templates::$layout_footer as $path) { ?>
                         <li id="<?php echo basename($path, '.php') ?>" class="sortyes"><?php echo basename($path, '.php') ?></li>
                     <?php } ?>
                 </ul>
-                <ul id="sortable9" class="connectedSortable4 block-ul" style="width:33%">
+                <ul id="sortable9" class="connectedSortable4 block-ul" style="width:33.3%">
                     <li class="sortno border bg-primary"><span class="glyphicon glyphicon-resize-horizontal"></span><span class="bi-trash"></span></li>
                     <?php foreach (Templates::$layout_footer_basket as $path) { ?>
                         <li id="<?php echo basename($path, '.php') ?>" class="sortyes"><?php echo basename($path, '.php') ?></li>
