5367bdf2cb1293fa94fd9102d57bbf6bc2462d79
musicman3 <micromail@mail.ru>
2021-01-26 12:32:16 +0200

    правка

--- view/default/admin/pages/stock/modal/confirm.php
+++ view/default/admin/pages/stock/modal/confirm.php
@@ -8,13 +8,14 @@
 <div id="confirm" class="modal fade" tabindex="-1">
     <div class="modal-dialog modal-sm">
         <div class="modal-content">
-            <div class="modal-header"><div class="float-end">&nbsp;&nbsp;<button class="close" type="button" data-bs-dismiss="modal">×</button></div>
-                <h4 id="confirm_title" class="modal-title"></h4>
+            <div class="modal-header">
+                <h5 id="confirm_title" class="modal-title"></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
             <div id="confirm_body" class="modal-body"></div>
             <div class="modal-footer">
-                <button class="btn btn-success btn-sm" type="button" data-bs-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> <?php echo lang('confirm-no') ?></button>
-                <button type="button" id="confirmation" class="btn btn-danger btn-sm"><span class="glyphicon glyphicon-ok"></span> <?php echo lang('confirm-yes') ?></button>
+                <button class="btn btn-success btn-sm" type="button" data-bs-dismiss="modal"><span class="bi-x-circle"></span> <?php echo lang('confirm-no') ?></button>
+                <button type="button" id="confirmation" class="btn btn-danger btn-sm"><span class="bi-check-circle"></span> <?php echo lang('confirm-yes') ?></button>
             </div>
         </div>
     </div>
