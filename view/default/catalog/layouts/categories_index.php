8d89f03eaa5352bd7db77c8e0e745f543662fb72
Prizraki <lw12@yandex.ru>
2021-02-03 21:29:56 +0300

    Еще категории

--- view/default/catalog/layouts/categories_index.php
+++ view/default/catalog/layouts/categories_index.php
@@ -12,12 +12,12 @@ Categories::indexData();
 <?php if (Categories::$index_data == true) { ?>
     <div id="categories_index" class="contentText">
         <h3><?php echo lang('categories_name') ?></h3>
-        <div class="row grid-group">
+        <div class="row">
             <?php foreach (Categories::$index_data as $value) { ?>
-                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item">
+                <div class="col-xl-3 col-lg-4 col-md-6 col-12 grid-group-item">
                     <div class="categoriesHolder">
                         <?php if ($value[2] == true) { ?>
-                            <a href="/?route=listing&category_id=<?php echo $value[0] ?>"><img src="/uploads/images/categories/resize_0/<?php echo $value[2] ?>" alt="<?php echo $value[1] ?>" class="img-responsive img-rounded center-block"></a>
+                            <a href="/?route=listing&category_id=<?php echo $value[0] ?>"><img src="/uploads/images/categories/resize_0/<?php echo $value[2] ?>" alt="<?php echo $value[1] ?>" class="img-fluid rounded mx-auto d-block"></a>
                         <?php } ?>
                         <h5 class="text-center"><a href="/?route=listing&category_id=<?php echo $value[0] ?>"><?php echo $value[1] ?></a></h5>
                     </div>
