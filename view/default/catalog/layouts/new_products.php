37dea61229555eadf73b00f083415afc60ec6706
Prizraki <lw12@yandex.ru>
2021-02-03 23:19:36 +0300

    Новые товары

--- view/default/catalog/layouts/new_products.php
+++ view/default/catalog/layouts/new_products.php
@@ -15,19 +15,19 @@ if (Products::$new_products == true) {
     ?>
     <div id="new_products" class="contentText">
         <h3><?php echo lang('new_products_name') ?></h3>
-        <div class="row row-flex">
+        <div class="row">
             <?php foreach (Products::$new_products as $value) {
                 ?>
-                <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 grid-group-item">
+                <div class="col-xl-3 col-lg-4 col-md-6 col-12 grid-group-item">
                     <div class="productHolder">
 
                         <div class="labelsblock">
-                            <?php foreach (Products::stikers($value, 'label-danger', 'label-success') as $stiker) { ?>
+                            <?php foreach (Products::stikers($value, 'bg-danger', 'bg-success') as $stiker) { ?>
                                 <div class="<?php echo $stiker[0] ?>"><?php echo $stiker[1] ?></div>
                             <?php } ?>
                         </div>
 
-                        <a href="/?route=products&category_id=<?php echo $value['parent_id'] ?>&id=<?php echo $value['id'] ?>"><img src="/uploads/images/products/resize_1/<?php echo $value['logo_general'] ?>" alt="<?php echo $value['name']; ?>" class="img-responsive center-block"></a>
+                        <a href="/?route=products&category_id=<?php echo $value['parent_id'] ?>&id=<?php echo $value['id'] ?>"><img src="/uploads/images/products/resize_1/<?php echo $value['logo_general'] ?>" alt="<?php echo $value['name']; ?>" class="img-fluid mx-auto d-block"></a>
                         <h5 class="text-center item-heading"><a href="/?route=products&category_id=<?php echo $value['parent_id'] ?>&id=<?php echo $value['id'] ?>"><?php echo $value['name'] ?></a></h5>
                         <div class="text-center item-price"><?php echo Ecb::priceInterface($value, 2) ?></div>
 
