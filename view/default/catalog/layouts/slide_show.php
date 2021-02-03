b925badef26ddb08d82880bb3055be65dd212cc5
Prizraki <lw12@yandex.ru>
2021-02-03 18:04:36 +0300

    Слайдшоу

--- view/default/catalog/layouts/slide_show.php
+++ view/default/catalog/layouts/slide_show.php
@@ -12,16 +12,19 @@ Slideshow::view();
 if (Slideshow::$slideshow == true) {
     ?>
     <div class="container-fluid">
-        <div id="Carousel" class="carousel slide hidden-xs" 
-             data-interval="<?php echo Slideshow::$slide_interval ?>" 
-             data-pause="<?php echo Slideshow::$slide_pause ?>" 
-             data-ride="<?php echo Slideshow::$autostart ?>" 
-             data-wrap="<?php echo Slideshow::$cicles ?>">
+        <div id="Carousel" class="carousel slide d-none d-md-block" 
+             data-bs-interval="<?php echo Slideshow::$slide_interval ?>" 
+             data-bs-pause="<?php echo Slideshow::$slide_pause ?>" 
+             data-bs-ride="<?php echo Slideshow::$autostart ?>" 
+             data-bs-wrap="<?php echo Slideshow::$cicles ?>" 
+             data-bs-slide="true"
+             data-bs-touch="true" 
+             data-bs-keyboard="true">
                  <?php if (Slideshow::$indicators == 'true') { ?>
                 <ol class="carousel-indicators">
-                    <li data-target="#Carousel" data-slide-to="0" class="active"></li>
+                    <li data-bs-target="#Carousel" data-bs-slide-to="0" class="active"></li>
                     <?php for ($x = 1; $x < count(Slideshow::$slideshow_array); $x++) { ?>
-                        <li data-target="#Carousel" data-slide-to="<?php echo $x ?>"></li>
+                        <li data-bs-target="#Carousel" data-bs-slide-to="<?php echo $x ?>"></li>
                     <?php } ?>
                 </ol>
             <?php } ?>
@@ -31,9 +34,9 @@ if (Slideshow::$slideshow == true) {
                     foreach (json_decode($images_data['logo'], 1) as $logo) {
                         if ($images_data['status'] == 1 && strtotime($images_data['date_start']) <= Slideshow::$this_time && strtotime($images_data['date_finish']) >= Slideshow::$this_time) {
                             ?>
-                            <div class="item <?php echo Settings::activeTab(0, 0) ?>">
+                            <div class="carousel-item <?php echo Settings::activeTab(0, 0) ?>">
                                 <a href="<?php echo $images_data['url'] ?>">
-                                    <img src="/uploads/images/slideshow/resize_4/<?php echo $logo ?>" class="center-block" >
+                                    <img src="/uploads/images/slideshow/resize_4/<?php echo $logo ?>" class="d-block w-100" >
                                     <?php if ($images_data['animation'] == 1) { ?>
                                         <div class="carousel-caption slide-text-anime" style="color: <?php echo $images_data['color'] ?>;">
                                             <h3><?php echo $images_data['name'] ?></h3>
@@ -45,7 +48,7 @@ if (Slideshow::$slideshow == true) {
                                             <p><?php echo $images_data['heading'] ?></p>
                                         </div>
                                     <?php } ?>
-                                </a>
+				</a>
                             </div>
 
                             <?php
@@ -55,11 +58,11 @@ if (Slideshow::$slideshow == true) {
                 ?>
             </div>
             <?php if (Slideshow::$navigation_key == 'true') { ?>
-                <a class="carousel-control left" href="#Carousel" data-slide="prev">
-                    <span class="glyphicon glyphicon-chevron-left"></span>
+                <a class="carousel-control-prev" href="#Carousel" data-bs-slide="prev">
+                    <span class="bi-chevron-left"></span>
                 </a>
-                <a class="carousel-control right" href="#Carousel" data-slide="next">
-                    <span class="glyphicon glyphicon-chevron-right"></span>
+                <a class="carousel-control-next" href="#Carousel" data-bs-slide="next">
+                    <span class="bi-chevron-right"></span>
                 </a>
             <?php } ?>
         </div>
