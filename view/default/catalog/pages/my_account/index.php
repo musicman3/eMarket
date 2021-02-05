bebe04e6652b3dd8a6d52a7b4e6a9eecd70f3918
Prizraki <lw12@yandex.ru>
2021-02-05 22:38:04 +0300

    Аккаунт

--- view/default/catalog/pages/my_account/index.php
+++ view/default/catalog/pages/my_account/index.php
@@ -19,60 +19,60 @@ foreach (View::tlpc('content') as $path) {
 <h1><?php echo lang('my_account_name') ?></h1>
 
 <div id="my_account" class="contentText">
-    <form name="form_add" id="form_add" action="javascript:void(null);" onchange="validate()" onsubmit="Ajax.callAdd()">
+    <form class="was-validated" name="form_add" id="form_add" action="javascript:void(null);" onchange="validate()" onsubmit="Ajax.callAdd()">
         <input type="hidden" id="edit" name="edit" value="ok" />
         <div class="row">
 
-            <div class="col-sm-6">
-                <div class="form-group">
+            <div class="col-md-6">
+                <div class="mb-3">
                     <legend><?php echo lang('my_account_personal_details') ?></legend>
                     <small class="form-text text-muted"><?php echo lang('my_account_firstname') ?></small>
-                    <div class="input-group has-success">
-                        <span class="input-group-addon"><span class="glyphicon glyphicon-pencil"></span></span>
-                        <input class="input-sm form-control" placeholder="<?php echo lang('my_account_enter_your_firstname') ?>" type="text" name="firstname" id="firstname" value="<?php echo Autorize::$customer['firstname'] ?>" />
+                    <div class="input-group">
+                        <span class="input-group-text"><span class="bi-pencil"></span></span>
+                        <input class="form-control" placeholder="<?php echo lang('my_account_enter_your_firstname') ?>" type="text" name="firstname" id="firstname" value="<?php echo Autorize::$customer['firstname'] ?>" />
                     </div>
 
                     <small class="form-text text-muted"><?php echo lang('my_account_lastname') ?></small>
-                    <div class="input-group has-success">
-                        <span class="input-group-addon"><span class="glyphicon glyphicon-pencil"></span></span>
-                        <input class="input-sm form-control" placeholder="<?php echo lang('my_account_enter_your_lastname') ?>" type="text" name="lastname" id="lastname" value="<?php echo Autorize::$customer['lastname'] ?>" />
+                    <div class="input-group">
+                        <span class="input-group-text"><span class="bi-pencil"></span></span>
+                        <input class="form-control" placeholder="<?php echo lang('my_account_enter_your_lastname') ?>" type="text" name="lastname" id="lastname" value="<?php echo Autorize::$customer['lastname'] ?>" />
                     </div>
 
                     <small class="form-text text-muted"><?php echo lang('my_account_middlename') ?></small>
-                    <div class="input-group has-success">
-                        <span class="input-group-addon"><span class="glyphicon glyphicon-pencil"></span></span>
-                        <input class="input-sm form-control" placeholder="<?php echo lang('my_account_enter_your_middlename') ?>" type="text" name="middle_name" id="middle_name" value="<?php echo Autorize::$customer['middle_name'] ?>" />
+                    <div class="input-group">
+                        <span class="input-group-text"><span class="bi-pencil"></span></span>
+                        <input class="form-control" placeholder="<?php echo lang('my_account_enter_your_middlename') ?>" type="text" name="middle_name" id="middle_name" value="<?php echo Autorize::$customer['middle_name'] ?>" />
                     </div>
 
                     <small class="form-text text-muted"><?php echo lang('my_account_telephone') ?></small>
-                    <div class="input-group has-success">
-                        <span class="input-group-addon"><span class="glyphicon glyphicon-pencil"></span></span>
-                        <input class="input-sm form-control" placeholder="<?php echo lang('my_account_enter_your_phone') ?>" type="text" name="telephone" id="lastname" value="<?php echo \eMarket\Core\Autorize::$customer['telephone'] ?>" />
+                    <div class="input-group">
+                        <span class="input-group-text"><span class="bi-pencil"></span></span>
+                        <input class="form-control" placeholder="<?php echo lang('my_account_enter_your_phone') ?>" type="text" name="telephone" id="lastname" value="<?php echo \eMarket\Core\Autorize::$customer['telephone'] ?>" />
                     </div>
 
                 </div>
             </div>
 
-            <div class="col-sm-6">
-                <div class="form-group">
+            <div class="col-md-6">
+                <div class="mb-3">
                     <legend><?php echo lang('my_account_details_password') ?></legend>
                     <small class="form-text text-muted"><?php echo lang('my_account_password') ?></small>
-                    <div class="input-group has-success password">
-                        <span class="input-group-addon"><span class="glyphicon glyphicon-pencil"></span></span>
-                        <input class="input-sm form-control password-data" minlength="7" maxlength="40" placeholder="<?php echo lang('my_account_enter_your_password') ?>" type="password" name="password" id="password" />
+                    <div class="input-group password">
+                        <span class="input-group-text"><span class="bi-pencil"></span></span>
+                        <input class="form-control password-data" minlength="7" maxlength="40" placeholder="<?php echo lang('my_account_enter_your_password') ?>" type="password" name="password" id="password" />
                     </div>
 
                     <small class="form-text text-muted"><?php echo lang('my_account_confirm_password') ?></small>
-                    <div class="input-group has-success confirm">
-                        <span class="input-group-addon"><span class="glyphicon glyphicon-pencil"></span></span>
-                        <input class="input-sm form-control password-data" minlength="7" maxlength="40" placeholder="<?php echo lang('my_account_confirm_your_password') ?>" type="password" name="confirm_password" id="confirm_password" />
+                    <div class="input-group confirm">
+                        <span class="input-group-text"><span class="bi-pencil"></span></span>
+                        <input class="form-control password-data" minlength="7" maxlength="40" placeholder="<?php echo lang('my_account_confirm_your_password') ?>" type="password" name="confirm_password" id="confirm_password" />
                     </div>
                 </div>
             </div>
 
         </div>
 
-        <div class="text-right form-group">
+        <div class="text-end mb-3">
             <input id="submit_btn" class="btn btn-primary" type="submit" value="<?php echo lang('save') ?>">
         </div>
 
