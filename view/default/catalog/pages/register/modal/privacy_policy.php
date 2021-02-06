ddaa05382110afd34bbfc9a0287752a659a4a9f8
Prizraki <lw12@yandex.ru>
2021-02-06 11:31:18 +0300

    Регистрация

--- view/default/catalog/pages/register/modal/privacy_policy.php
+++ view/default/catalog/pages/register/modal/privacy_policy.php
@@ -6,15 +6,17 @@
 ?>
 
 <div id="privacy_policy" class="modal fade" tabindex="-1">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
         <div class="modal-content">
-            <div class="modal-header"><div class="pull-right"><button class="close" type="button" data-dismiss="modal">×</button></div>
-                <h4 class="modal-title"><?php echo lang('register_privacy_policy_tittle') ?></h4>
+            <div class="modal-header">
+                <h3 class="modal-title"><?php echo lang('register_privacy_policy_tittle') ?></h3>
+                <button type="button" class="btn-close btn-sm" data-bs-dismiss="modal"></button>
             </div>
 
-            <div class="panel-body">
-                <div class="form-group"><?php echo lang('register_privacy_policy_text') ?></div> 
+            <div class="modal-body">
+                <p><?php echo lang('register_privacy_policy_text') ?></p> 
             </div>
+
         </div>
     </div>
 </div>
