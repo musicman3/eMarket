2e584457b0657da045ca474c73766d8914e729b9
Prizraki <lw12@yandex.ru>
2021-02-15 21:22:53 +0300

    Update new_products.php

--- view/default/catalog/layouts/new_products.php
+++ view/default/catalog/layouts/new_products.php
@@ -27,7 +27,7 @@ if (Products::$new_products == true) {
                             <?php } ?>
                         </div>
 
-                        <a href="/?route=products&category_id=<?php echo $value['parent_id'] ?>&id=<?php echo $value['id'] ?>"><img src="/uploads/images/products/resize_1/<?php echo $value['logo_general'] ?>" alt="<?php echo $value['name']; ?>" class="mb-2 img-fluid rounded mx-auto d-block"></a>
+                        <a href="/?route=products&category_id=<?php echo $value['parent_id'] ?>&id=<?php echo $value['id'] ?>"><img src="/uploads/images/products/resize_1/<?php echo $value['logo_general'] ?>" alt="<?php echo $value['name']; ?>" class="img-fluid rounded mx-auto d-block mb-2"></a>
                         <h5 class="text-center"><a href="/?route=products&category_id=<?php echo $value['parent_id'] ?>&id=<?php echo $value['id'] ?>"><?php echo $value['name'] ?></a></h5>
                         <div class="text-center item-price"><?php echo Ecb::priceInterface($value, 2) ?></div>
 
