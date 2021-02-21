f5ae293432faec522581b7faecab4290ce1db6a8
musicman3 <micromail@mail.ru>
2021-02-21 16:28:43 +0200

    еще

--- view/default/admin/pages/stock/modal/index_product.php
+++ view/default/admin/pages/stock/modal/index_product.php
@@ -182,7 +182,7 @@ use \eMarket\Admin\Stock;
                                 <div class="col-md-6 mb-3">
                                     <div><small class="form-text text-muted"><?php echo lang('stock_product_receipt_date') ?></small></div>
                                     <div class="input-group input-group-sm">
-                                        <span class="input-group-text bi-calendar3"><span class="bi-calendar3"></span></span>
+                                        <span class="input-group-text bi-calendar3"></span>
                                         <input class="form-control" placeholder="<?php echo lang('stock_product_receipt_date') ?>" type="text" name="date_available_product_stock" id="date_available_product_stock" autocomplete="off" />
                                     </div>
                                 </div>
