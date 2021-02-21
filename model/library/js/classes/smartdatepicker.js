685333f4cc87d1f578dd48b58f18a20211f0a47c
musicman3 <micromail@mail.ru>
2021-02-21 16:11:39 +0200

    локализация через момент

--- model/library/js/classes/smartdatepicker.js
+++ model/library/js/classes/smartdatepicker.js
@@ -51,6 +51,11 @@ class SmartDatepicker {
                     onSelect: function () {
                         startDate = this.getDate();
                         updateStartDate();
+                    },
+                    i18n: {
+                        months: [moment.months()[0], moment.months()[1], moment.months()[2], moment.months()[3], moment.months()[4], moment.months()[5], moment.months()[6], moment.months()[7], moment.months()[8], moment.months()[9], moment.months()[10], moment.months()[11]],
+                        weekdays: [moment.weekdays()[0], moment.weekdays()[1], moment.weekdays()[2], moment.weekdays()[3], moment.weekdays()[4], moment.weekdays()[5], moment.weekdays()[6]],
+                        weekdaysShort: [moment.weekdaysMin()[0], moment.weekdaysMin()[1], moment.weekdaysMin()[2], moment.weekdaysMin()[3], moment.weekdaysMin()[4], moment.weekdaysMin()[5], moment.weekdaysMin()[6]]
                     }
                 }),
                 endPicker = new Pikaday({
@@ -64,6 +69,11 @@ class SmartDatepicker {
                     onSelect: function () {
                         endDate = this.getDate();
                         updateEndDate();
+                    },
+                    i18n: {
+                        months: [moment.months()[0], moment.months()[1], moment.months()[2], moment.months()[3], moment.months()[4], moment.months()[5], moment.months()[6], moment.months()[7], moment.months()[8], moment.months()[9], moment.months()[10], moment.months()[11]],
+                        weekdays: [moment.weekdays()[0], moment.weekdays()[1], moment.weekdays()[2], moment.weekdays()[3], moment.weekdays()[4], moment.weekdays()[5], moment.weekdays()[6]],
+                        weekdaysShort: [moment.weekdaysMin()[0], moment.weekdaysMin()[1], moment.weekdaysMin()[2], moment.weekdaysMin()[3], moment.weekdaysMin()[4], moment.weekdaysMin()[5], moment.weekdaysMin()[6]]
                     }
                 }),
                 _startDate = startPicker.setDate(new Date(start)),
