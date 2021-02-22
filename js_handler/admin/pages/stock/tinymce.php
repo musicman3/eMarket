8e6df9901a3893093ea1ad800ef30151a95eb1fc
musicman3 <micromail@mail.ru>
2021-02-22 03:59:10 +0200

    фикс для 5 бута и новые настройки в тини

--- js_handler/admin/pages/stock/tinymce.php
+++ js_handler/admin/pages/stock/tinymce.php
@@ -9,6 +9,23 @@
 <script>
     tinymce.init({
         selector: '.wysiwyg',
+        plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media table charmap hr toc advlist lists imagetools textpattern noneditable help charmap quickbars emoticons',
+        imagetools_cors_hosts: ['picsum.photos'],
+        menubar: 'file edit view insert format tools table help',
+        toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+        toolbar_sticky: true,
+        autosave_ask_before_unload: true,
+        autosave_interval: '30s',
+        autosave_prefix: '{path}{query}-{id}-',
+        autosave_restore_when_empty: false,
+        autosave_retention: '2m',
+        image_advtab: true,
         language: document.documentElement.lang
+    });
+
+    document.addEventListener('focusin', function (e) {
+        if ($(e.target).closest(".tox-tinymce-aux, .moxman-window, .tam-assetmanager-root").length) {
+            e.stopImmediatePropagation();
+        }
     });
 </script>
