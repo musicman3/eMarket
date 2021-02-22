5b60270600916eb6bc7376a29bbce8414f2e3991
musicman3 <micromail@mail.ru>
2021-02-22 23:54:23 +0200

    и это

--- js_handler/admin/pages/stock/tinymce.php
+++ js_handler/admin/pages/stock/tinymce.php
@@ -9,7 +9,7 @@
 <script>
     tinymce.init({
         selector: '.wysiwyg',
-        plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media table charmap hr toc advlist lists imagetools textpattern noneditable help charmap quickbars emoticons',
+        plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media table charmap hr toc advlist lists imagetools wordcount textpattern noneditable help charmap quickbars emoticons',
         menubar: 'file edit view insert format tools table help',
         toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
         toolbar_sticky: true,
