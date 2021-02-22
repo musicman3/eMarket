a2c1d668672c11d7bf347b408d16e5b33662e377
musicman3 <micromail@mail.ru>
2021-02-22 22:56:13 +0200

    немного настроек

--- js_handler/admin/pages/stock/tinymce.php
+++ js_handler/admin/pages/stock/tinymce.php
@@ -10,7 +10,6 @@
     tinymce.init({
         selector: '.wysiwyg',
         plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media table charmap hr toc advlist lists imagetools textpattern noneditable help charmap quickbars emoticons',
-        imagetools_cors_hosts: ['picsum.photos'],
         menubar: 'file edit view insert format tools table help',
         toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
         toolbar_sticky: true,
@@ -21,6 +20,8 @@
         autosave_retention: '2m',
         image_advtab: true,
         toolbar_mode: 'sliding',
+        image_caption: true,
+        quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
         language: document.documentElement.lang
     });
     // tinymce and modal
