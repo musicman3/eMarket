93f9cb7ea6150f7469b169ede7d9eddd629c0e60
musicman3 <micromail@mail.ru>
2021-05-22 02:21:27 +0300

    еще

--- js_handler/admin/pages/login/js.php
+++ js_handler/admin/pages/login/js.php
@@ -1,15 +1,11 @@
 <?php
-
 /* =-=-=-= Copyright © 2018 eMarket =-=-=-= 
   |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
-
 ?>
 <script type="text/javascript">
-    document.addEventListener("DOMContentLoaded", function () {
-        window.setTimeout(function () {
-            new bootstrap.Alert(document.querySelector('#alert')).close();
-        }, 3000);
-    });
+    window.setTimeout(function () {
+        new bootstrap.Alert(document.querySelector('#alert')).close();
+    }, 3000);
 </script>
--- view/default/layouts/alert.php
+++ view/default/layouts/alert.php
@@ -3,17 +3,14 @@
   |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
-
 ?>
 <!--Выводим уведомление об успешном действии-->
 <div id="alert" class="alert alert-<?php echo $_SESSION['message'][0] ?> fade show bi-exclamation-triangle"> <?php echo $_SESSION['message'][1] ?></div>
 
 <!--Автозакрытие уведомлений-->
 <script>
-    $(function () {
-        var time = <?php echo $_SESSION['message'][2] ?>;
-        window.setTimeout(function () {
-            $('#alert').alert('close');
-        }, time);
-    });
+    var time = <?php echo $_SESSION['message'][2] ?>;
+    window.setTimeout(function () {
+        new bootstrap.Alert(document.querySelector('#alert')).close();
+    }, time);
 </script>
