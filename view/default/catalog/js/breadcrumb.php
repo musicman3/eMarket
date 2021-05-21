b6f37c7a1d4809abf062e6038e9c63b21f2bb300
musicman3 <micromail@mail.ru>
2021-05-22 01:45:45 +0300

    еще

--- view/default/catalog/js/breadcrumb.php
+++ view/default/catalog/js/breadcrumb.php
@@ -15,41 +15,41 @@ if (Valid::inGET('search') OR !Valid::inGET('category_id')) {
 ?>
 
 <script type="text/javascript" language="javascript">
-    $('#breadcrumb').append('<li class="breadcrumb-item"><a href="/"><?php echo lang('breadcrumb_home') ?></a></li>');
+    document.querySelector('#breadcrumb').insertAdjacentHTML('beforeend', '<li class="breadcrumb-item"><a href="/"><?php echo lang('breadcrumb_home') ?></a></li>');
 
 <?php if (Valid::inGET('route') == 'products') { ?>
         function breadcrumb() {
-            var breadcrumbid = $('div#data_breadcrumb').data('breadcrumbid');
-            var breadcrumbname = $('div#data_breadcrumb').data('breadcrumbname');
+            var breadcrumbid = JSON.parse(document.querySelector('#data_breadcrumb').dataset.breadcrumbid);
+            var breadcrumbname = JSON.parse(document.querySelector('div#data_breadcrumb').dataset.breadcrumbname);
 
             if (breadcrumbid.length > 0) {
-                for (x = 0; x < breadcrumbname.length; x++) {
-                    $('#breadcrumb').append('<li class="breadcrumb-item"><a href="/?route=listing&category_id=' + breadcrumbid[x] + '">' + breadcrumbname[x] + '</a></li>');
+                for (var x = 0; x < breadcrumbname.length; x++) {
+                    document.querySelector('#breadcrumb').insertAdjacentHTML('beforeend', '<li class="breadcrumb-item"><a href="/?route=listing&category_id=' + breadcrumbid[x] + '">' + breadcrumbname[x] + '</a></li>');
                 }
             }
-            $('#breadcrumb').append('<li class="breadcrumb-item"><a href="/?route=listing&category_id=<?php echo Valid::inGET('category_id') ?>"><?php echo Products::$category_data['name'] ?></a></li>');
-            $('#breadcrumb').append('<li class="breadcrumb-item"><?php echo Products::$product_data['name'] ?></li>');
+            document.querySelector('#breadcrumb').insertAdjacentHTML('beforeend', '<li class="breadcrumb-item"><a href="/?route=listing&category_id=<?php echo Valid::inGET('category_id') ?>"><?php echo Products::$category_data['name'] ?></a></li>');
+            document.querySelector('#breadcrumb').insertAdjacentHTML('beforeend', '<li class="breadcrumb-item"><?php echo Products::$product_data['name'] ?></li>');
         }
 
-        $(document).ready(function () {
+        document.addEventListener("DOMContentLoaded", function () {
             breadcrumb();
         });
 
 <?php } elseif (Valid::inGET('route') == 'listing') {
     ?>
         function breadcrumb() {
-            var breadcrumbid = $('div#data_breadcrumb').data('breadcrumbid');
-            var breadcrumbname = $('div#data_breadcrumb').data('breadcrumbname');
+            var breadcrumbid = JSON.parse(document.querySelector('#data_breadcrumb').dataset.breadcrumbid);
+            var breadcrumbname = JSON.parse(document.querySelector('div#data_breadcrumb').dataset.breadcrumbname);
 
             if (breadcrumbid.length > 0) {
-                for (x = 0; x < breadcrumbname.length; x++) {
-                    $('#breadcrumb').append('<li class="breadcrumb-item"><a href="/?route=listing&category_id=' + breadcrumbid[x] + '">' + breadcrumbname[x] + '</a></li>');
+                for (var x = 0; x < breadcrumbname.length; x++) {
+                    document.querySelector('#breadcrumb').insertAdjacentHTML('beforeend', '<li class="breadcrumb-item"><a href="/?route=listing&category_id=' + breadcrumbid[x] + '">' + breadcrumbname[x] + '</a></li>');
                 }
             }
-            $('#breadcrumb').append('<li class="breadcrumb-item"><?php echo Products::$category_data['name'] ?></li>');
+            document.querySelector('#breadcrumb').insertAdjacentHTML('beforeend', '<li class="breadcrumb-item"><?php echo Products::$category_data['name'] ?></li>');
         }
 
-        $(document).ready(function () {
+        document.addEventListener("DOMContentLoaded", function () {
             breadcrumb();
         });
 <?php } ?>
