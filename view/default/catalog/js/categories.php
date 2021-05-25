0a74af74d6dde6aee3360528350cb4c3287da8ca
musicman3 <micromail@mail.ru>
2021-05-25 20:55:15 +0300

    переименовал

--- view/default/catalog/js/categories.php
+++ view/default/catalog/js/categories.php
@@ -9,7 +9,7 @@
         let params = (new URL(document.location)).searchParams;
         if (params.get('route') === 'listing') {
             document.querySelector('#cat_' + params.get('category_id')).closest('ul').setAttribute('aria-expand', 'true');
-            document.querySelector('#namecat_' + params.get('category_id')).classList.add('toggle-bold');
+            document.querySelector('#namecat_' + params.get('category_id')).classList.add('menu-bold');
             var json_data = JSON.parse(document.querySelector('#data_breadcrumb').dataset.breadcrumbid);
             json_data.forEach(e => document.querySelector('#cat_' + e).closest('ul').setAttribute('aria-expand', 'true'));
         }
@@ -17,7 +17,7 @@
         var toggle = new Toggle({
             buttonClassExpanded: 'toggle-btn-active',
             buttonSelector: 'span',
-            parentClass: 'toggle-parent',
+            parentClass: 'menu-parent',
             singleSibling: true,
             targetSelector: '.menu ul ul'
         });
