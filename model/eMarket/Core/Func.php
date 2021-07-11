bd4b8076204089423367ac30ceaafab2184e58af
musicman3 <micromail@mail.ru>
2021-07-12 01:08:57 +0300

    update

--- model/eMarket/Core/Func.php
+++ model/eMarket/Core/Func.php
@@ -398,8 +398,8 @@ class Func {
      */
     public static function escapeSign($string) {
         // symbol and replacement
-        $symbols = ["'", "<script", "</script>", "javascript:"];
-        $escape = ["&#8216;", "!script!", "!/script!", "!script!"];
+        $symbols = ["'", "<script", "/script", "javascript:"];
+        $escape = ["&#8216;", "!s-c-r-i-p-t!", "!/s-c-r-i-p-t!", "!s-c-r-i-p-t!"];
 
         if (is_array($string)) {
             $output = [];
