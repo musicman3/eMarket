4f027965c2d08385420fc2a001e4368925fcf086
musicman3 <micromail@mail.ru>
2021-07-27 17:38:46 +0300

    js files

--- js_handler/admin/pages/dashboard/js.php
+++ js_handler/admin/pages/dashboard/js.php
@@ -4,11 +4,10 @@
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
 ?>
-<script src="https://cdn.jsdelivr.net/npm/echarts@5.1.2/dist/echarts.min.js"></script>
+<script src="/ext/echarts/echarts.min.js"></script>
 <script type="text/javascript" src="/model/library/js/classes/ajax/ajax.js"></script>
 <script type="text/javascript" src="/model/library/js/classes/dashboard/dashboard.js"></script>
 <script type="text/javascript">
     new Ajax();
     new Dashboard();
-
 </script>
