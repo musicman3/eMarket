b61fdd1356524d31d79a9f57605fddce15040e48
musicman3 <micromail@mail.ru>
2021-07-28 21:24:27 +0300

    update file

--- view/default/catalog/layouts/new_products.php
+++ view/default/catalog/layouts/new_products.php
@@ -15,22 +15,22 @@ if (Products::$new_products == true) {
     ?>
     <div id="new_products" class="contentText">
         <h3><?php echo lang('new_products_name') ?></h3>
-        <div class="row row-cols-1 row-cols-md-4 g-0">
+        <div class="row">
             <?php foreach (Products::$new_products as $value) {
                 ?>
-                <div class="col g-3">
-                    <div class="card h-100 border rounded p-2">
+                <div class="mb-3 col-xl-3 col-lg-4 col-md-6 col-12">
+                    <div class="card border rounded p-2 h-100">
                         <div class="labelsblock">
                             <?php foreach (Products::stikers($value, 'bg-danger', 'bg-success') as $stiker) { ?>
                                 <div class="<?php echo $stiker[0] ?>"><?php echo $stiker[1] ?></div>
                             <?php } ?>
                         </div>
-                        <div class="h-100">
+                        <div id="image" class="h-100">
                             <a href="/?route=products&category_id=<?php echo $value['parent_id'] ?>&id=<?php echo $value['id'] ?>"><img src="/uploads/images/products/resize_1/<?php echo $value['logo_general'] ?>" alt="<?php echo $value['name']; ?>" class="img-fluid rounded mx-auto d-block mb-2"></a>
                         </div>
-                        <div class="align-bottom text-center">
-                            <h5><a href="/?route=products&category_id=<?php echo $value['parent_id'] ?>&id=<?php echo $value['id'] ?>"><?php echo $value['name'] ?></a></h5>
-                            <div class="item-price"><?php echo Ecb::priceInterface($value, 2) ?></div>
+                        <div class="align-bottom">
+                            <h5 class="text-center"><a href="/?route=products&category_id=<?php echo $value['parent_id'] ?>&id=<?php echo $value['id'] ?>"><?php echo $value['name'] ?></a></h5>
+                            <div class="text-center item-price"><?php echo Ecb::priceInterface($value, 2) ?></div>
                         </div>
                     </div>
                 </div>
