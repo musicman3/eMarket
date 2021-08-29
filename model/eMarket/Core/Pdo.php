0a961f01707d556a14507b2079aadb93c359ba29
musicman3 <micromail@mail.ru>
2021-08-29 22:53:11 +0300

    update

--- model/eMarket/Core/Pdo.php
+++ model/eMarket/Core/Pdo.php
@@ -149,7 +149,7 @@ final class Pdo {
         if ($exec && $exec->execute($param)) {
             $result = $exec->fetchAll(\PDO :: FETCH_NUM);
         }
-        return Func::outputDataFiltering($result);
+        return Func::outputDataFiltering($result)[0];
     }
 
     /**
