e3d16729db12e3c57d9dc50f90370e3561b93768
Prizraki <lw12@yandex.ru>
2021-08-31 22:17:50 +0300

    Update index.php

--- view/default/admin/pages/settings/index.php
+++ view/default/admin/pages/settings/index.php
@@ -21,15 +21,9 @@ use eMarket\Core\{
                     <div class="card">
                         <div class="card-header text-white bg-primary text-center"><span class="bi-circle-fill"></span></div>
                         <div class="card-body">
-                            <div class="mb-2">
-                                <div><a href="?route=settings/basic_settings" class="btn btn-primary btn-sm bi-gear-fill"></a> <?php echo lang('title_settings_basic_settings_index') ?></div>
-                            </div>
-                            <div class="mb-2">
-                                <div><a href="?route=settings/currencies" class="btn btn-primary btn-sm bi-cash"></a> <?php echo lang('title_settings_currencies_index') ?></div>
-                            </div>
-                            <div class="mb-2">
-                                <div><a href="?route=settings/order_status" class="btn btn-primary btn-sm bi-pin-angle-fill"></a> <?php echo lang('title_settings_order_status_index') ?></div>
-                            </div>
+                            <div class="mb-2"><a href="?route=settings/basic_settings" class="btn btn-primary btn-sm bi-gear-fill"></a> <?php echo lang('title_settings_basic_settings_index') ?></div>
+                            <div class="mb-2"><a href="?route=settings/currencies" class="btn btn-primary btn-sm bi-cash"></a> <?php echo lang('title_settings_currencies_index') ?></div>
+                            <div class="mb-2"><a href="?route=settings/order_status" class="btn btn-primary btn-sm bi-pin-angle-fill"></a> <?php echo lang('title_settings_order_status_index') ?></div>
                         </div>
                     </div>
                 </div>
@@ -38,15 +32,9 @@ use eMarket\Core\{
                     <div class="card">
                         <div class="card-header text-white bg-primary text-center"><span class="bi-circle-fill"></span></div>
                         <div class="card-body">
-                            <div class="mb-2">
-                                <div><a href="?route=settings/length" class="btn btn-primary btn-sm bi-rulers"></a> <?php echo lang('title_settings_length_index') ?></div>
-                            </div>
-                            <div class="mb-2">
-                                <div><a href="?route=settings/weight" class="btn btn-primary btn-sm bi-minecart-loaded"></a> <?php echo lang('title_settings_weight_index') ?></div>
-                            </div>
-                            <div class="mb-2">
-                                <div><a href="?route=settings/vendor_codes" class="btn btn-primary btn-sm bi-tag-fill"></a> <?php echo lang('title_settings_vendor_codes_index') ?></div>
-                            </div>
+                            <div class="mb-2"><a href="?route=settings/length" class="btn btn-primary btn-sm bi-rulers"></a> <?php echo lang('title_settings_length_index') ?></div>
+                            <div class="mb-2"><a href="?route=settings/weight" class="btn btn-primary btn-sm bi-minecart-loaded"></a> <?php echo lang('title_settings_weight_index') ?></div>
+                            <div class="mb-2"><a href="?route=settings/vendor_codes" class="btn btn-primary btn-sm bi-tag-fill"></a> <?php echo lang('title_settings_vendor_codes_index') ?></div>
                         </div>
                     </div>
                 </div>
@@ -55,15 +43,9 @@ use eMarket\Core\{
                     <div class="card">
                         <div class="card-header text-white bg-primary text-center"><span class="bi-circle-fill"></span></div>
                         <div class="card-body">
-                            <div class="mb-2">
-                                <div><a href="?route=settings/units" class="btn btn-primary btn-sm bi-flag-fill"></a> <?php echo lang('title_settings_units_index') ?></div>
-                            </div>
-                            <div class="mb-2">
-                                <div><a href="?route=settings/templates" class="btn btn-primary btn-sm bi-intersect"></a> <?php echo lang('title_settings_templates_index') ?></div>
-                            </div>
-                            <div class="mb-2">
-                                <div><a href="?route=settings/modules" class="btn btn-primary btn-sm bi-cpu-fill"></a> <?php echo lang('title_settings_modules_index') ?></div>
-                            </div>
+                            <div class="mb-2"><a href="?route=settings/units" class="btn btn-primary btn-sm bi-flag-fill"></a> <?php echo lang('title_settings_units_index') ?></div>
+                            <div class="mb-2"><a href="?route=settings/templates" class="btn btn-primary btn-sm bi-intersect"></a> <?php echo lang('title_settings_templates_index') ?></div>
+                            <div class="mb-2"><a href="?route=settings/modules" class="btn btn-primary btn-sm bi-cpu-fill"></a> <?php echo lang('title_settings_modules_index') ?></div>
                         </div>
                     </div>
                 </div>
@@ -72,15 +54,9 @@ use eMarket\Core\{
                     <div class="card">
                         <div class="card-header text-white bg-primary text-center"><span class="bi-circle-fill"></span></div>
                         <div class="card-body">
-                            <div class="mb-2">
-                                <div><a href="?route=settings/countries" class="btn btn-primary btn-sm bi-globe"></a> <?php echo lang('title_settings_countries_index') ?></div>
-                            </div>
-                            <div class="mb-2">
-                                <div><a href="?route=settings/zones" class="btn btn-primary btn-sm bi-geo-alt-fill"></a> <?php echo lang('title_settings_zones_index') ?></div>
-                            </div>
-                            <div class="mb-2">
-                                <div><a href="?route=settings/taxes" class="btn btn-primary btn-sm bi-percent"></a> <?php echo lang('title_settings_taxes_index') ?></div>
-                            </div>
+                            <div class="mb-2"><a href="?route=settings/countries" class="btn btn-primary btn-sm bi-globe"></a> <?php echo lang('title_settings_countries_index') ?></div>
+                            <div class="mb-2"><a href="?route=settings/zones" class="btn btn-primary btn-sm bi-geo-alt-fill"></a> <?php echo lang('title_settings_zones_index') ?></div>
+                            <div class="mb-2"><a href="?route=settings/taxes" class="btn btn-primary btn-sm bi-percent"></a> <?php echo lang('title_settings_taxes_index') ?></div>
                         </div>
                     </div>
                 </div>
