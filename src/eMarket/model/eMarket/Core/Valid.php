b38c81c78dff4928cfa1bbc23bc3e995e6766782
musicman3 <micromail@mail.ru>
2021-10-12 16:10:28 +0300

    test

--- src/eMarket/model/eMarket/Core/Valid.php
+++ src/eMarket/model/eMarket/Core/Valid.php
@@ -21,6 +21,11 @@ namespace eMarket\Core;
 class Valid {
 
     public static $demo_mode = FALSE;
+    public static $test_post_json = FALSE;
+    public static $test_post = FALSE;
+    public static $test_get = FALSE;
+    public static $test_server = FALSE;
+    public static $test_cookie = FALSE;
 
     /**
      * POST validation
@@ -29,6 +34,9 @@ class Valid {
      * @return mixed
      */
     public static function inPOST(?string $input): mixed {
+        if (self::$test_post != FALSE && isset(self::$test_post[$input])) {
+            return self::$test_post[$input];
+        }
         if (filter_input(INPUT_POST, $input, FILTER_SANITIZE_SPECIAL_CHARS, FILTER_FORCE_ARRAY) == TRUE && self::$demo_mode == FALSE) {
             if (isset($_POST[$input])) {
                 return $_POST[$input];
@@ -56,6 +64,9 @@ class Valid {
      * @return mixed
      */
     public static function inGET(?string $input): mixed {
+        if (self::$test_get != FALSE && isset(self::$test_get[$input])) {
+            return self::$test_get[$input];
+        }
         if (filter_input(INPUT_GET, $input, FILTER_SANITIZE_SPECIAL_CHARS, FILTER_FORCE_ARRAY) == TRUE) {
             if (isset($_GET[$input])) {
                 return $_GET[$input];
@@ -71,6 +82,9 @@ class Valid {
      * @return mixed
      */
     public static function inSERVER(?string $input): mixed {
+        if (self::$test_server != FALSE && isset(self::$test_server[$input])) {
+            return self::$test_server[$input];
+        }
         if (filter_input(INPUT_SERVER, $input, FILTER_SANITIZE_SPECIAL_CHARS, FILTER_FORCE_ARRAY) == TRUE) {
             if (isset($_SERVER[$input])) {
                 return $_SERVER[$input];
@@ -86,6 +100,9 @@ class Valid {
      * @return mixed
      */
     public static function inCOOKIE(?string $input): mixed {
+        if (self::$test_cookie != FALSE && isset(self::$test_cookie[$input])) {
+            return self::$test_cookie[$input];
+        }
         if (filter_input(INPUT_COOKIE, $input, FILTER_SANITIZE_SPECIAL_CHARS, FILTER_FORCE_ARRAY) == TRUE) {
             if (isset($_COOKIE[$input])) {
                 return $_COOKIE[$input];
@@ -101,6 +118,9 @@ class Valid {
      * @return mixed
      */
     public static function inPostJson(?string $input): mixed {
+        if (self::$test_post_json != FALSE && isset(self::$test_post_json[$input])) {
+            return self::$test_post_json[$input];
+        }
         $postData = htmlspecialchars(file_get_contents('php://input'), ENT_NOQUOTES);
         $data = json_decode($postData, true);
         if (is_string($postData) && is_array($data) && (json_last_error() == JSON_ERROR_NONE) && self::$demo_mode == FALSE) {
--- tests/eMarket/Core/JsonRpcTest.php
+++ tests/eMarket/Core/JsonRpcTest.php
@@ -10,9 +10,9 @@ declare(strict_types=1);
 namespace eMarket\Core;
 
 use PHPUnit\Framework\TestCase;
-
 use eMarket\Core\{
-    JsonRpc
+    JsonRpc,
+    Valid
 };
 
 class JsonRpcTest extends TestCase {
@@ -20,6 +20,24 @@ class JsonRpcTest extends TestCase {
     public function testEncodeGetData() {
         $result = JsonRpc::encodeGetData('1', 'get', []);
         $this->assertIsString($result);
+    }
+
+    public function testDecodeGetData() {
+        
+        Valid::$test_get = [
+            'request' => urlencode(json_encode([
+                'jsonrpc' => '2.0',
+                'method' => 'POST',
+                'param' => [],
+                'id' => '1',
+        ]))];
+        
+        $eMarket = new JsonRpc();
+        $this->assertIsString($eMarket->decodeGetData('jsonrpc'));
+        $this->assertIsString($eMarket->decodeGetData('method'));
+        $this->assertIsArray($eMarket->decodeGetData('param'));
+        $this->assertIsString($eMarket->decodeGetData('id'));
+        $this->assertIsArray($eMarket->decodeGetData(null));
     }
 
 }
