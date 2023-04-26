2b7a329c535f899b86e3496c37f88150cf4cc3d0
musicman3 <micromail@mail.ru>
2023-04-26 14:54:49 +0300

    Cruder for RecoveryPass

--- src/eMarket/model/eMarket/Catalog/RecoveryPass.php
+++ src/eMarket/model/eMarket/Catalog/RecoveryPass.php
@@ -13,9 +13,9 @@ use eMarket\Core\{
     Cryptography,
     Clock\SystemClock,
     Messages,
-    Pdo,
     Valid
 };
+use Cruder\Cruder;
 
 /**
  * Recovery Password
@@ -30,6 +30,7 @@ class RecoveryPass {
 
     public static $routing_parameter = 'recoverypass';
     public $title = 'title_recoverypass_index';
+    public $db;
     public static $customer_id;
 
     /**
@@ -37,6 +38,7 @@ class RecoveryPass {
      *
      */
     function __construct() {
+        $this->db = new Cruder();
         $this->recovery();
     }
 
@@ -46,21 +48,44 @@ class RecoveryPass {
      */
     private function recovery(): void {
         if (Valid::inGET('recovery_code')) {
-            self::$customer_id = Pdo::getValue("SELECT customer_id FROM " . TABLE_PASSWORD_RECOVERY . " WHERE recovery_code=?", [
-                        Valid::inGET('recovery_code')]
-            );
+
+            self::$customer_id = $this->db
+                    ->read(TABLE_PASSWORD_RECOVERY)
+                    ->selectValue('customer_id')
+                    ->where('recovery_code=', Valid::inGET('recovery_code'))
+                    ->save();
+
             if (self::$customer_id != FALSE && Valid::inPOST('password')) {
-                $recovery_code_created = Pdo::getValue("SELECT UNIX_TIMESTAMP (recovery_code_created) FROM " . TABLE_PASSWORD_RECOVERY . " WHERE customer_id=?", [
-                            self::$customer_id
-                ]);
+
+                $recovery_code_created = $this->db
+                        ->read(TABLE_PASSWORD_RECOVERY)
+                        ->selectValue('{{UNIX_TIMESTAMP->recovery_code_created}}')
+                        ->where('customer_id=', self::$customer_id)
+                        ->save();
+
                 if ($recovery_code_created + (3 * 24 * 60 * 60) > SystemClock::nowUnixTime()) {
-                    Pdo::action("DELETE FROM " . TABLE_PASSWORD_RECOVERY . " WHERE customer_id=?", [self::$customer_id]);
+
+                    $this->db
+                            ->delete(TABLE_PASSWORD_RECOVERY)
+                            ->where('customer_id=', self::$customer_id)
+                            ->save();
 
                     $password_hash = Cryptography::passwordHash(Valid::inPOST('password'));
-                    Pdo::action("UPDATE " . TABLE_CUSTOMERS . " SET password=? WHERE id=?", [$password_hash, self::$customer_id]);
+
+                    $this->db
+                            ->update(TABLE_CUSTOMERS)
+                            ->set('password', $password_hash)
+                            ->where('id=', self::$customer_id)
+                            ->save();
+
                     Messages::alert('messages_recovery_password_complete', 'success', lang('messages_recovery_password_complete'), 7000, true);
                 } else {
-                    Pdo::action("DELETE FROM " . TABLE_PASSWORD_RECOVERY . " WHERE customer_id=?", [self::$customer_id]);
+
+                    $this->db
+                            ->delete(TABLE_PASSWORD_RECOVERY)
+                            ->where('customer_id=', self::$customer_id)
+                            ->save();
+
                     Messages::alert('messages_recovery_password_failed', 'danger', lang('messages_recovery_password_failed'), 7000, true);
                 }
             }
