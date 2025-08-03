98321cb828b456767a5949b684fba68a9c8b127b
musicman3 <micromail@mail.ru>
2025-08-03 22:05:41 +0300

    sql revision (probe)

--- src/eMarket/storage/updater/sql/mysql.sql
+++ src/eMarket/storage/updater/sql/mysql.sql
@@ -1,11 +1,2 @@
-delimiter $$ DROP
-    PROCEDURE IF EXISTS foo $$ CREATE
-        PROCEDURE foo() BEGIN DECLARE CONTINUE handler FOR 1060 BEGIN
-    END;
-
-ALTER TABLE
-    emkt_basic_settings ADD logo_general VARCHAR(128);
-
-ALTER TABLE
-    emkt_basic_settings ADD logo json;
-END $$ CALL foo() $$
+ALTER TABLE emkt_basic_settings ADD logo_general VARCHAR(128);
+ALTER TABLE emkt_basic_settings ADD logo json;
