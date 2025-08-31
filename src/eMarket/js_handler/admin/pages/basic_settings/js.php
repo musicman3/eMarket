c22bca9ffe48e92d39d8cb6197c27bc993fc5a04
musicman3 <micromail@mail.ru>
2025-08-31 12:58:20 +0300

    revision

--- src/eMarket/js_handler/admin/pages/basic_settings/js.php
+++ src/eMarket/js_handler/admin/pages/basic_settings/js.php
@@ -6,12 +6,8 @@
 ?>
 <script type="text/javascript" src="/ext/lpology/SimpleAjaxUploader.min.js"></script>
 <script type="text/javascript" src="/js_handler/admin/pages/basic_settings/main.js"></script>
-<?php if (Valid::$demo_mode == FALSE) { ?>
-    <script type="text/javascript" src="/model/library/js/classes/update/update.js"></script>
-<?php } ?>
+<script type="text/javascript" src="/model/library/js/classes/update/update.js"></script>
 <script type="text/javascript">
     new BasicSettings();
-<?php if (Valid::$demo_mode == FALSE) { ?>
-        new Update();
-<?php } ?>
+    new Update();
 </script>
