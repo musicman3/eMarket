2733272bf23dfea09729a99052956a6b6abf9615
musicman3 <micromail@mail.ru>
2025-08-31 12:59:12 +0300

    revision

--- src/eMarket/js_handler/admin/pages/basic_settings/js.php
+++ src/eMarket/js_handler/admin/pages/basic_settings/js.php
@@ -3,11 +3,19 @@
   |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
+use eMarket\Core\{
+    Valid
+};
 ?>
 <script type="text/javascript" src="/ext/lpology/SimpleAjaxUploader.min.js"></script>
 <script type="text/javascript" src="/js_handler/admin/pages/basic_settings/main.js"></script>
-<script type="text/javascript" src="/model/library/js/classes/update/update.js"></script>
+<?php if (Valid::$demo_mode == FALSE) { ?>
+    <script type="text/javascript" src="/model/library/js/classes/update/update.js"></script>
+<?php } ?>
 <script type="text/javascript">
     new BasicSettings();
-    new Update();
+<?php if (Valid::$demo_mode == FALSE) { ?>
+        new Update();
+<?php } ?>
 </script>
