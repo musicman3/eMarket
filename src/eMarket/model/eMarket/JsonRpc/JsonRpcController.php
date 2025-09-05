b02ffa01e370ff524674bc33d78dd72660e0f4b7
musicman3 <micromail@mail.ru>
2025-09-05 20:18:08 +0300

    jsonrpc revision

---
+++ src/eMarket/model/eMarket/JsonRpc/JsonRpcController.php
@@ -0,0 +1,52 @@
+<?php
+
+/* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
+  |  https://github.com/musicman3/eMarket  |
+  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
+declare(strict_types=1);
+
+namespace eMarket\JsonRpc;
+
+use eMarket\Core\{
+    JsonRpc
+};
+
+/**
+ * Update class
+ *
+ * @package JsonRpc
+ * @author eMarket Team
+ * @copyright © 2018 eMarket
+ * @license GNU GPL v.3.0
+ * 
+ */
+class JsonRpcController {
+
+    public static $routing_parameter = 'JsonRpcController';
+
+    /**
+     * Constructor
+     *
+     */
+    public function __construct() {
+        $this->controller();
+    }
+
+    /**
+     * JsonRpc Controller
+     * 
+     */
+    public function controller(): void {
+        
+        $jsonRpc = new JsonRpc();
+        $methods_available = $jsonRpc->routing();
+
+        foreach ($methods_available as $method) {
+            new $method['method']();
+        }
+
+        $jsonRpc->response();
+    }
+}
