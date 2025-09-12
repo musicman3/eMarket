bd533d18ac8c5a9448410560122fbbaa9cde84a1
musicman3 <micromail@mail.ru>
2025-09-12 13:42:40 +0300

    tinymce 8.02

--- src/eMarket/js/structure/admin/pages/stock/main.js
+++ src/eMarket/js/structure/admin/pages/stock/main.js
@@ -107,7 +107,7 @@ class Stock {
             selector: '.wysiwyg',
             plugins: 'print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media table charmap hr toc advlist lists imagetools wordcount textpattern noneditable help charmap quickbars emoticons',
             menubar: 'file edit view insert format tools table help',
-            toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
+            toolbar: 'undo redo | bold italic underline strikethrough | blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
             toolbar_sticky: true,
             autosave_ask_before_unload: true,
             autosave_interval: '30s',
@@ -119,7 +119,9 @@ class Stock {
             contextmenu: 'link image imagetools table',
             image_caption: true,
             quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
-            language: document.documentElement.lang
+            language: document.documentElement.lang,
+            promotion: false,
+            license_key: 'gpl'
         });
     }
 
