773434e85f2f0ff99ac4ef1192014903327e16c8
musicman3 <micromail@mail.ru>
2025-09-14 15:18:37 +0300

    revision

--- src/eMarket/view/default/catalog/constructor.php
+++ src/eMarket/view/default/catalog/constructor.php
@@ -20,7 +20,7 @@ use eMarket\Catalog\{
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=Edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="robots" content="index,follow" />
         <meta name="generator" content="Netbeans" />
         <meta name="classification" content="software" />
