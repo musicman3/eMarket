2304d5e4687cb8c8438adf0d81c3f0b04fd97b0b
musicman3 <micromail@mail.ru>
2025-09-15 00:28:36 +0300

    mobile adaptation

--- src/eMarket/view/default/catalog/pages/listing/index.php
+++ src/eMarket/view/default/catalog/pages/listing/index.php
@@ -79,36 +79,40 @@ if (Pages::$count > 0) {
                 <div class="item mb-3 col-xl-3 col-lg-4 col-md-6 col-12 grid-group-item">
                     <div id="card" class="card border rounded p-2 h-100">
 
-                        <div class="labelsblock">
-                            <?php foreach (Products::stickers(Pages::$table['line'], 'bg-danger', 'bg-success') as $sticker) { ?>
-                                <div class="<?php echo $sticker[0] ?>"><?php echo $sticker[1] ?></div>
-                            <?php } ?>
-                        </div>
-                        <div class="image h-100">
-                            <a href="/?route=products&id=<?php echo Pages::$table['line']['id'] ?>">
-                                <img src="/uploads/images/products/resize_1/<?php echo Pages::$table['line']['logo_general'] ?>" alt="<?php echo Pages::$table['line']['name'] ?>" class="img-fluid rounded mx-auto d-block mb-2">
-                            </a>
-                        </div>
-                        <h5 class="item-heading"><a href="/?route=products&id=<?php echo Pages::$table['line']['id'] ?>"><?php echo Pages::$table['line']['name'] ?></a></h5>
-                        <div class="item-price mb-2"><?php echo Ecb::priceInterface(Pages::$table['line'], 2) ?></div>
-                        <div class="item-text mb-2">
-                            <ul>
-                                <?php foreach (Listing::getCharData() as $val) { ?>
-                                    <li>
-                                        <label><?php echo $val['label'] ?> </label> 
-                                        <?php echo $val['text'] ?>
-                                    </li>
+                        <div class="d-flex h-75">
+                            <div class="labelsblock">
+                                <?php foreach (Products::stickers(Pages::$table['line'], 'bg-danger', 'bg-success') as $sticker) { ?>
+                                    <div class="<?php echo $sticker[0] ?>"><?php echo $sticker[1] ?></div>
                                 <?php } ?>
-                                <li>
-                                    <label><?php echo lang('product_availability') ?></label>
-                                    <?php foreach (Products::inStock(Pages::$table['line']['date_available'], Pages::$table['line']['quantity']) as $in_stock) { ?>
-                                        <span class="<?php echo $in_stock[0] ?>"><?php echo $in_stock[1] ?></span>
-                                    <?php } ?>
-                                </li>
-                            </ul>
+                            </div>
+                            <div class="image text-center w-100">
+                                <a href="/?route=products&id=<?php echo Pages::$table['line']['id'] ?>">
+                                    <img src="/uploads/images/products/resize_2/<?php echo Pages::$table['line']['logo_general'] ?>" alt="<?php echo Pages::$table['line']['name'] ?>" class="img-fluid rounded mx-auto d-block mb-2">
+                                </a>
+                            </div>
+                            <div class="ms-auto text-end w-100">
+                                <h5 class="item-heading"><a href="/?route=products&id=<?php echo Pages::$table['line']['id'] ?>"><?php echo Pages::$table['line']['name'] ?></a></h5>
+                                <div class="item-price mb-2"><?php echo Ecb::priceInterface(Pages::$table['line'], 2) ?></div>
+                                <div class="item-text mb-2">
+                                    <ul>
+                                        <?php foreach (Listing::getCharData() as $val) { ?>
+                                            <li>
+                                                <label><?php echo $val['label'] ?> </label> 
+                                                <?php echo $val['text'] ?>
+                                            </li>
+                                        <?php } ?>
+                                        <li>
+                                            <label><?php echo lang('product_availability') ?></label>
+                                            <?php foreach (Products::inStock(Pages::$table['line']['date_available'], Pages::$table['line']['quantity']) as $in_stock) { ?>
+                                                <span class="<?php echo $in_stock[0] ?>"><?php echo $in_stock[1] ?></span>
+                                            <?php } ?>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
-                        <div class="col-12 mb-2 item-button">
-                            <div>
+                        <div class="col-12 mb-2 d-flex align-items-center h-25">
+                            <div class="text-end w-100">
                                 <button class="btn btn-outline-primary bi-dash" type="button" onclick="ProductsListing.pcsProduct('minus', <?php echo Pages::$table['line']['id'] ?>)"></button>
                                 <input id="number_<?php echo Pages::$table['line']['id'] ?>" data-bs-toggle="popover" data-bs-placement="top" data-bs-content="<?php echo lang('listing_no_more_in_stock') ?>" type="number" min="1" value="<?php echo Cart::maxQuantityToOrder(Pages::$table['line']) ?>" class="quantity" disabled>
                                 <button class="btn btn-outline-primary button-plus bi-plus" type="button" onclick="ProductsListing.pcsProduct('plus', <?php echo Pages::$table['line']['id'] ?>, <?php echo Cart::maxQuantityToOrder(Pages::$table['line'], 'true') ?>)"></button>
