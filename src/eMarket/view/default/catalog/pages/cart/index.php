b503429309857a21b0f49c8cad7163859b136e5c
musicman3 <micromail@mail.ru>
2025-09-14 13:51:17 +0300

    revision

--- src/eMarket/view/default/catalog/pages/cart/index.php
+++ src/eMarket/view/default/catalog/pages/cart/index.php
@@ -26,7 +26,7 @@ require_once('modal/index.php')
 
             <?php foreach (Cart::$cart_info as $value) { ?>
                 <form id="quantity_product" name="quantity_product" action="javascript:void(null);">
-                    <div class="d-flex align-items-center">
+                    <div class="d-flex align-items-center border-bottom p-1">
                         <div class="text-center w-50">
                             <div class="p-1">
                                 <div><a href="/?route=products&id=<?php echo $value['id'] ?>"><img src="/uploads/images/products/resize_0/<?php echo $value['logo_general'] ?>" alt="<?php echo $value['name'] ?>" class="img-thumbnail"></a></div>
@@ -45,7 +45,6 @@ require_once('modal/index.php')
                         </div>
                         <div class="p-1 ms-auto text-end"><button class="btn btn-primary btn-sm bi-trash" type="button" onclick="Cart.deleteProduct(<?php echo $value['id'] ?>)"></button></div>
                     </div>
-                    <hr>
                 </form>
             <?php } ?>
         </div>
