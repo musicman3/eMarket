5e439df92151e0b4db77bf879c5ad6fa51f81b1a
musicman3 <micromail@mail.ru>
2025-09-16 12:13:35 +0300

    revision

--- src/eMarket/view/default/catalog/pages/listing/index.php
+++ src/eMarket/view/default/catalog/pages/listing/index.php
@@ -85,9 +85,9 @@ if (Pages::$count > 0) {
                                     <div class="<?php echo $sticker[0] ?>"><?php echo $sticker[1] ?></div>
                                 <?php } ?>
                             </div>
-                            <div class="image text-center w-100 mb-2">
+                            <div class="image text-center mb-2 p-3">
                                 <a href="/?route=products&id=<?php echo Pages::$table['line']['id'] ?>">
-                                    <img src="/uploads/images/products/resize_2/<?php echo Pages::$table['line']['logo_general'] ?>" alt="<?php echo Pages::$table['line']['name'] ?>" class="img-fluid rounded mx-auto d-block mb-2">
+                                    <img src="/uploads/images/products/resize_2/<?php echo Pages::$table['line']['logo_general'] ?>" alt="<?php echo Pages::$table['line']['name'] ?>" class="w-100 img-fluid rounded mx-auto d-block mb-2">
                                 </a>
                             </div>
                             <div class="ms-auto text-end w-100">
