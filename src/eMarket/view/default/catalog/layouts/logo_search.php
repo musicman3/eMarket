8533fb467392cfce867257e817b180fe97111d63
musicman3 <micromail@mail.ru>
2025-10-03 01:24:21 +0300

    revision

--- src/eMarket/view/default/catalog/layouts/logo_search.php
+++ src/eMarket/view/default/catalog/layouts/logo_search.php
@@ -7,10 +7,10 @@
 
 <div id="header" class="container-fluid mt-3 mb-3">
     <div class="row align-items-center">
-        <div class="col-4">
+        <div class="col-5">
             <a href="/"><img src="/uploads/images/emarket_logo/catalog_logo.png" alt="" class="logo img-fluid float-start"></a>
         </div>
-        <div class="col-8">
+        <div class="col-7">
             <form>
                 <input hidden name="route" value="listing">
                 <div class="input-group">
