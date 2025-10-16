9a21077f7f2a6697e5799a9f0c4210e403215e34
musicman3 <micromail@mail.ru>
2025-10-17 00:21:59 +0300

    revision

--- src/eMarket/controller/index.php
+++ src/eMarket/controller/index.php
@@ -54,7 +54,7 @@ if (Settings::path() != 'install') {
 
     Db::set([
         'db_type' => DB_TYPE,
-        'db_transaction' => 'true',
+        'db_transactions' => 'true',
         'db_server' => DB_SERVER,
         'db_name' => DB_NAME,
         'db_username' => DB_USERNAME,
