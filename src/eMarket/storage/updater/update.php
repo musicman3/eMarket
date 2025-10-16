20c33e6bff4edeccb15c48014f8ca9bd03f95051
musicman3 <micromail@mail.ru>
2025-10-16 12:59:11 +0300

    revision

--- src/eMarket/storage/updater/update.php
+++ src/eMarket/storage/updater/update.php
@@ -205,7 +205,6 @@ function copyingFiles(string $repo, string $target_folder): void {
 
     filesRemoving($source_dir);
     if (file_exists(getenv('DOCUMENT_ROOT') . '/update.php') && file_exists(getenv('DOCUMENT_ROOT') . '/storage/updater/update.php')) {
-        unlink(getenv('DOCUMENT_ROOT') . '/update.php');
         copy(getenv('DOCUMENT_ROOT') . '/storage/updater/update.php', getenv('DOCUMENT_ROOT') . '/update.php');
     }
 
