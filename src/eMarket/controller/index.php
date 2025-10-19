8d2cbf76f6995327b40134d341a380dcb324856c
musicman3 <micromail@mail.ru>
2025-10-20 00:07:52 +0300

    revision

--- src/eMarket/controller/index.php
+++ src/eMarket/controller/index.php
@@ -51,26 +51,7 @@ Messages::monologErrorHandler();
 if (Settings::path() != 'install') {
 
     require_once(getenv('DOCUMENT_ROOT') . '/storage/configure/configure.php');
-
-    Db::config(
-            [
-                DB_TYPE =>
-                [
-                    'db_driver' => DB_TYPE,
-                    'db_server' => DB_SERVER,
-                    'db_name' => DB_NAME,
-                    'db_username' => DB_USERNAME,
-                    'db_password' => DB_PASSWORD,
-                    'db_prefix' => DB_PREFIX,
-                    'db_port' => DB_PORT,
-                    'db_family' => DB_FAMILY,
-                    'db_charset' => 'utf8mb4',
-                    'db_collate' => 'utf8mb4_unicode_ci',
-                    'db_path' => ROOT . '/storage/databases/sqlite.db3'
-                ]
-            ]
-    );
-    Db::use(DB_TYPE)->transactions('on');
+    require_once(getenv('DOCUMENT_ROOT') . '/storage/configure/cruder_config.php');
 
     // Load BasicSettings
     new BasicSettings();
---
+++ src/eMarket/storage/configure/cruder_config.php
@@ -0,0 +1,28 @@
+<?php
+
+/* =-=-=-= Copyright © 2018 eMarket =-=-=-=  
+  |    GNU GENERAL PUBLIC LICENSE v.3.0    |
+  |  https://github.com/musicman3/eMarket  |
+  =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
+
+use Cruder\Db;
+
+Db::config(
+        [
+            DB_TYPE =>
+            [
+                'db_driver' => DB_TYPE,
+                'db_server' => DB_SERVER,
+                'db_name' => DB_NAME,
+                'db_username' => DB_USERNAME,
+                'db_password' => DB_PASSWORD,
+                'db_prefix' => DB_PREFIX,
+                'db_port' => DB_PORT,
+                'db_family' => DB_FAMILY,
+                'db_charset' => 'utf8mb4',
+                'db_collate' => 'utf8mb4_unicode_ci',
+                'db_path' => ROOT . '/storage/databases/sqlite.db3'
+            ]
+        ]
+);
+Db::use(DB_TYPE)->transactions('on');
