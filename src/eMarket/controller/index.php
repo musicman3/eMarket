d818aabdb2ec4c14def40f2f45dd42b6133e834e
musicman3 <micromail@mail.ru>
2025-11-10 15:14:03 +0200

    revision

--- src/eMarket/controller/index.php
+++ src/eMarket/controller/index.php
@@ -1,6 +1,6 @@
 <?php
 
-/* =-=-=-= Copyright © 2018 eMarket =-=-=-= 
+/* =-=-=-= Copyright © 2018 eMarket =-=-=-=
   |    GNU GENERAL PUBLIC LICENSE v.3.0    |
   |  https://github.com/musicman3/eMarket  |
   =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-= */
@@ -56,11 +56,13 @@ if (Settings::path() != 'install') {
     new BasicSettings();
 }
 
+// Routing
+$Routing = new Routing();
+
 // Load Languages
 new Lang();
 
-// Routing
-$Routing = new Routing();
+// Load Template
 if ($Routing->constructor()) {
     require_once($Routing->constructor());
 }
