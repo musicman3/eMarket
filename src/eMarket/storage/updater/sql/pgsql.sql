15ecc2a14eb3e2df8afeb434fbddba1d77df60f7
musicman3 <micromail@mail.ru>
2026-03-22 00:57:24 +0200

    revision

--- src/eMarket/storage/updater/sql/pgsql.sql
+++ src/eMarket/storage/updater/sql/pgsql.sql
@@ -2,7 +2,6 @@ ALTER TABLE emkt_basic_settings ADD COLUMN logo jsonb;
 ALTER TABLE emkt_basic_settings ADD COLUMN logo_general varchar(128);
 ALTER TABLE emkt_basic_settings DROP COLUMN logo_general;
 ALTER TABLE emkt_basic_settings DROP COLUMN logo;
-
 DROP TABLE IF EXISTS emkt_contacts;
 CREATE TABLE emkt_contacts (
 	language varchar(64),
