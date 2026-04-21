e8aa2d530e8e75cc26ce5fb45421e8472400a8db
musicman3 <micromail@mail.ru>
2026-04-21 12:31:36 +0300

    revision

--- src/eMarket/js/structure/catalog/pages/listing/js.php
+++ src/eMarket/js/structure/catalog/pages/listing/js.php
@@ -6,8 +6,12 @@
 ?>
 <script type="text/javascript" src="/js/structure/catalog/pages/listing/main.js"></script>
 <script type="text/javascript" src="/js/library/classes/ajax/ajax.js"></script>
+<link rel="stylesheet" type="text/css" href="/js/ext/baguettebox/baguetteBox.min.css" />
+<script type="text/javascript" src="/js/ext/baguettebox/baguetteBox.min.js"></script>
 
 <script type="text/javascript" language="javascript">
     new ProductsListing();
     new Ajax();
+    // Litebox
+    baguetteBox.run('.gallery');
 </script>
